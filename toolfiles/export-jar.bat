d90fdea9fb8f6692465aa237f854f3f37efef1ad
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2006-11-13 23:10:05 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DoorzienDWO@256 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export-jar.bat
@@ -0,0 +1,2 @@
+copy ..\output\jar\doorziendwo.jar N:\home\www-dev\InfoGroups\javaclasses\jars\doorziendwo.jar
+copy ..\output\jar\doorziendwo.jar N:\home\www-dev\InfoGroups\dwo\jars\doorziendwo.jar
