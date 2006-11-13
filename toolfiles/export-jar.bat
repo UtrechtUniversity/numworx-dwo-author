8e6eebecff05edde651bc72effd49b4ca1c8d2d2
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2006-11-13 20:35:00 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenOpdr@250 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export-jar.bat
@@ -0,0 +1,2 @@
+copy ..\output\jar\algebrapijlenopdr.jar N:\home\www-dev\InfoGroups\javaclasses\jars\algebrapijlenopdr.jar
+copy ..\output\jar\algebrapijlenopdr.jar N:\home\www-dev\InfoGroups\dwo\jars\algebrapijlenopdr.jar
