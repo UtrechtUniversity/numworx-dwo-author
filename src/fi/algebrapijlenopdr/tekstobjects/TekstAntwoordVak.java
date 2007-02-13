825bd6c85ec7b889d4e9da1e2b7866fc7f987667
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2007-02-13 08:52:58 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenOpdr@577 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/algebrapijlenopdr/tekstobjects/TekstAntwoordVak.java
@@ -0,0 +1,57 @@
+package fi.algebrapijlenopdr.tekstobjects;
+
+import java.awt.*;
+import java.awt.event.*;
+import fi.algebrapijlenopdr.formuleobjects.*;
+import fi.algebrapijlenopdr.tekstobjects.*;
+
+public class TekstAntwoordVak extends TekstDeelVak implements ActionListener
+{
+	private TekstArea antwoordVak;
+	private Font font = new Font("TimesRoman",Font.PLAIN,13);
+	private FontMetrics fm;
+	private boolean selected = false;
+	
+	public TekstAntwoordVak(TekstVak tv)
+	{	super(tv);
+	
+		antwoordVak = new TekstArea();
+		antwoordVak.setBounds(0,0,tv.getSize().width-10,20);
+		antwoordVak.setEditable(true);
+		setFont(font);
+		add(antwoordVak);
+				
+		
+				
+		setSize(antwoordVak.getSize().width,antwoordVak.getSize().height);
+		//ashoogte = antwoordVak.getSize().height;
+	}
+	
+	public void vulVak(String s)
+	{	antwoordVak.setText(s);
+	}
+	
+	public TekstArea geefAntwoordVak()
+	{	return antwoordVak;
+	}
+	
+	public void setEditable(boolean b)
+	{	antwoordVak.setEditable(true);
+	}
+	
+	public void zetMaat()
+	{	setSize(antwoordVak.getSize().width, antwoordVak.getSize().height);
+		antwoordVak.setLocation(0,0);
+		//ashoogte = antwoordVak.getSize().height;
+		if(getParent()instanceof TekstElement)((TekstElement)getParent()).zetMaat();
+	}
+	
+	public String toString()
+	{	return "$A" + antwoordVak.getText() + "@";
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{	if(e.getActionCommand().equals("focus"))requestFocus();//tekstVak.zetAntwoordVak(antwoordVak);
+	}
+	
+}
