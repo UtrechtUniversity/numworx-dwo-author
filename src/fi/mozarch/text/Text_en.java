6ecb8316c777dd20feed44daa183b468fab85be5
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2007-03-20 10:35:59 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/MozArch@653 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/mozarch/text/Text_en.java
@@ -0,0 +1,13 @@
+package fi.mozarch.text;
+
+public class Text_en extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {		{ "wisKnopLabel" , "Clear" },
+                { "opstuurKnopLabel", "Send"},
+				{ "BestandenKnopLabel", "Show/open files"},
+				{ "infoKnopLabel", "Info"},
+        };
+}
+
---
+++ src/fi/mozarch/text/Text_nl.java
@@ -0,0 +1,15 @@
+package fi.mozarch.text;
+
+public class Text_nl extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {       { "wisKnopLabel" , "Wis" },
+                { "opstuurKnopLabel", "Opsturen"},
+				{ "bestandenKnopLabel", "Bestanden opvragen/bekijken"},
+				{ "infoKnopLabel", "Info"},
+        };
+}
+
+
+
