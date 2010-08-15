dc41a91905f049b697d2d64ae833b172e379dd01
Drijv103 <Drijv103@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2010-08-15 12:45:04 +0000

    15-8-10 14:44 Manu Drijvers
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/BinomVerdeling@3308 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/binomverdeling/BinomVerdeling.java
+++ src/fi/binomverdeling/BinomVerdeling.java
@@ -9,151 +9,149 @@ import fi.beans.scorm.*;
 import fi.beans.base64code.*;
 import fi.beans.wiskopdrbeans.*;
 
-public class BinomVerdeling extends Applet implements ScormAppletIF, WiskOpdrApplet
-{
+public class BinomVerdeling extends Applet implements ScormAppletIF, WiskOpdrApplet {
 	protected static ResourceBundle rb;
 	private String langArg;
 	protected SCORM12APIInterface api;
 	private TextField textField;
-	
-	public static void main(String[] args)    
-	{	int width = 800;
-        int height = 600;
-		ScormMainFrame mf = new ScormMainFrame(new BinomVerdeling(),width, height);
+
+	public static void main(String[] args) {
+		int width = 800;
+		int height = 600;
+		ScormMainFrame mf = new ScormMainFrame(new BinomVerdeling(), width,	height);
 		mf.setTitle("BinomVerdeling");
 		mf.pack();
 		mf.show();
 		mf.setSize(width, height);
 	}
-	
-	public BinomVerdeling(Locale language)
-    {   langArg = language.getLanguage();
-		rb = ResourceBundle.getBundle("fi.binomverdeling.text.Text", language);
-    }
-    
-   
-	public BinomVerdeling()
-	{	langArg = "nl";
-		Locale language = new Locale (langArg, "");
+
+	public BinomVerdeling(Locale language) {
+		langArg = language.getLanguage();
 		rb = ResourceBundle.getBundle("fi.binomverdeling.text.Text", language);
 	}
-	
-	
-	public void init() 
-	{	try
-		{	api = Scorm.findAPI(this);
+
+	public BinomVerdeling() {
+		langArg = "nl";
+		Locale language = new Locale(langArg, "");
+		rb = ResourceBundle.getBundle("fi.binomverdeling.text.Text", language);
+	}
+
+	public void init() {
+		try {
+			api = Scorm.findAPI(this);
 		}
-		catch(Exception e){}
-		
+		catch (Exception e) {
+			
+		}
+
 		setLayout(null);
-		
-		//instelling taal
+
+		// instelling taal
 		String langArg = getParameter("language");
-		if ( langArg == null) langArg = "nl";
-		Locale language = new Locale (langArg, "");
-		rb = ResourceBundle.getBundle("fi.binomverdeling.text.Text",language);
-		
-		//instelling achtergrondkleur
-		Color bgcolor = new Color(255,255,255);
+		if (langArg == null) {
+			langArg = "nl";
+		}
+		Locale language = new Locale(langArg, "");
+		rb = ResourceBundle.getBundle("fi.binomverdeling.text.Text", language);
+
+		// instelling achtergrondkleur
+		Color bgcolor = new Color(255, 255, 255);
 		String kleurcode = getParameter("bgcolor");
-		if(kleurcode!=null)bgcolor = new Color(Integer.parseInt(kleurcode.substring(1),16));
+		if (kleurcode != null) {
+			bgcolor = new Color(Integer.parseInt(kleurcode.substring(1), 16));
+		}
 		setBackground(bgcolor);
-		
-		//Fi-logo, copyright
-		FIButton fiButton = new FIButton("BinomVerdeling",new String[]
-			{	"versie-info: ...",
-				"auteur: ...",
-				"programmeur: ...",
-				"Freudenthal Instituut",
-				"www.fi.uu.nl",
-				""
-			});
-		fiButton.setBounds(0,0,20,30);
+
+		// Fi-logo, copyright
+		FIButton fiButton = new FIButton("BinomVerdeling", new String[] {
+				"versie-info: ...", "auteur: ...", "programmeur: ...",
+				"Freudenthal Instituut", "www.fi.uu.nl", "" });
+		fiButton.setBounds(0, 0, 20, 30);
 		add(fiButton);
-		
-		//Test-textfield
+
+		// Test-textfield
 		textField = new TextField();
-		textField.setBounds(50,100,200,25);
+		textField.setBounds(50, 100, 200, 25);
 		add(textField);
 	}
 
-	public void start()
-	{	if(api!=null)
-		{	String s = api.LMSGetValue("cmi.suspend_data");
-			if(s!=null && !s.equals(""))setState(s);
+	public void start() {
+		if (api != null) {
+			String s = api.LMSGetValue("cmi.suspend_data");
+			if (s != null && !s.equals("")) {
+				setState(s);
+			}
 		}
-	
+
 	}
-	
-	public void stopSco()
-	{	stop();
+
+	public void stopSco() {
+		stop();
 		api = null;
 	}
-	
-	public void stop()
-	{	if(api!=null)
-		{	String s = getState();
+
+	public void stop() {
+		if (api != null) {
+			String s = getState();
 			String d = new Double(getScore()).toString();
-			api.LMSSetValue("cmi.core.score.raw",d);
-			api.LMSSetValue("cmi.suspend_data",s);
+			api.LMSSetValue("cmi.core.score.raw", d);
+			api.LMSSetValue("cmi.suspend_data", s);
 		}
 	}
-	
-	public void paint(Graphics g) 
-	{	g.drawString(BinomVerdeling.rb.getString("welkomTekst"), 50, 60 );
+
+	public void paint(Graphics g) {
+		g.drawString(BinomVerdeling.rb.getString("welkomTekst"), 50, 60);
 		super.paint(g);
 	}
-	
-	public void setState(String s)
-	{	//decodeer de string
+
+	public void setState(String s) { // decodeer de string
 		Object o = StringCodeObject.decodeStringToObject(s);
-		Hashtable h = (Hashtable)o;
-		
-		//haal de data uit de hashtabel
-		String text = (String)h.get("text");
-		
-	    //herstel de state van het applet
-	    textField.setText(text);
-	}
-	
-	public String getState()
-	{	String text = null;
-	
-		//vraag de gegevens op die de state bepalen
-	    text = textField.getText();
-	    
-	    Hashtable h = new Hashtable();
-	    
-	    //voeg de gegeven toe aan de hashtable
-	    h.put("text", text);
-	      
-	    //codeer de hashtable tot string
-	    String s = StringCodeObject.encodeObjectToString(h);
-	    return s;
-	}
-	
-	public double getScore()
-	{	return 0.5;
-	}
-	
-	public boolean hasEditMode()
-	{	return false;
+		Hashtable h = (Hashtable) o;
+
+		// haal de data uit de hashtabel
+		String text = (String) h.get("text");
+
+		// herstel de state van het applet
+		textField.setText(text);
 	}
 
-    public ScormEditComponentIF getEditComponent(Hashtable launchdata)
-    {	return null;
-    }
-    
-    public Parameter[] getEditableParameters()
-	{	return null;
-    }
+	public String getState() {
+		String text = null;
 
-    public Parameter[] getAllParameters()
-    {	return null;
-    }
-    
-    public InteractiePanel getInteractiePanel()
-    {
-    	return new BVInteractiePanel();
-    }
+		// vraag de gegevens op die de state bepalen
+		text = textField.getText();
+
+		Hashtable h = new Hashtable();
+
+		// voeg de gegeven toe aan de hashtable
+		h.put("text", text);
+
+		// codeer de hashtable tot string
+		String s = StringCodeObject.encodeObjectToString(h);
+		return s;
+	}
+
+	public double getScore() {
+		return 0.5;
+	}
+
+	public boolean hasEditMode() {
+		return false;
+	}
+
+	public ScormEditComponentIF getEditComponent(Hashtable launchdata) {
+		return null;
+	}
+
+	public Parameter[] getEditableParameters() {
+		return null;
+	}
+
+	public Parameter[] getAllParameters() {
+		return null;
+	}
+
+	public InteractiePanel getInteractiePanel() {
+		return new BVInteractiePanel();
+	}
 }
