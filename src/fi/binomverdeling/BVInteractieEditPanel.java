aa2163343b6a63ecf540fe5ebc0bfd1f10fbcdc3
Drijv103 <Drijv103@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2010-08-17 11:57:32 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/BinomVerdeling@3315 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/binomverdeling/BVInteractieEditPanel.java
+++ src/fi/binomverdeling/BVInteractieEditPanel.java
@@ -1,79 +1,64 @@
 package fi.binomverdeling;
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Hashtable;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import fi.beans.scorm.Parameter;
-import fi.beans.wiskopdrbeans.*;
+import fi.beans.wiskopdrbeans.InteractieEditPanel;
 
+public class BVInteractieEditPanel extends JPanel implements InteractieEditPanel, ActionListener {
 
-public class BVInteractieEditPanel extends JPanel implements InteractieEditPanel, ActionListener
-{
-	
-	
-	public BVInteractieEditPanel()
-	{
-		
-    }
-	
-	public void setEditState(Hashtable h)
-	{
-		
+	public BVInteractieEditPanel() {
+		super();
+		super.add(new JLabel("test BVInteractieEditPanel"));
 	}
-	
-	public Hashtable getEditState()
-	{
+
+	public void setEditState(Hashtable h) {
+
+	}
+
+	public Hashtable getEditState() {
 		Hashtable h = new Hashtable();
-		
+
 		return h;
 	}
-		
-	public void setBounds(int x, int y, int b, int h)
-	{
-		super.setBounds(x,y,b,h);
-		
+
+	public void setBounds(int x, int y, int b, int h) {
+		super.setBounds(x, y, b, h);
+
 	}
-	
-	public void zetBreedte(int b)
-	{	
+
+	public void zetBreedte(int b) {
 	}
-	
-	public void zetHoogte(int h)
-	{	
+
+	public void zetHoogte(int h) {
 	}
-	
-	public void wis()
-	{
-		
+
+	public void wis() {
+
 	}
-    
-	public void zetMode(int mode)
-	{
-		
+
+	public void zetMode(int mode) {
+
 	}
-	
-    public void stop()
-	{
-		
+
+	public void stop() {
+
 	}
-    
-    public void start()
-	{
-		
+
+	public void start() {
+
 	}
-    
-    public void addActionListener(ActionListener al)
-	{
-		
+
+	public void addActionListener(ActionListener al) {
+
 	}
-    
-	public void actionPerformed(ActionEvent e)
-	{
-		
+
+	public void actionPerformed(ActionEvent e) {
+
 	}
 
 }
