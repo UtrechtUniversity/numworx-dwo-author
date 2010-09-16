a277618a049fe67376df1d2cd89b3f115f33e8c5
Drijv103 <Drijv103@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2010-09-16 12:55:01 +0000

    Enum aangepast tot Typesafe Enum, nu dus ook JDK 1.4 compatible.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/BinomVerdeling@3377 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/binomverdeling/GrenzenOptie.java
+++ src/fi/binomverdeling/GrenzenOptie.java
@@ -1,11 +1,27 @@
 package fi.binomverdeling;
 
 /**
- * Enumeratie om aan te geven welke kans bekeken wordt
+ * Typesafe enum om aan te geven welke kans bekeken wordt
+ * JDK 1.4 compatible
+ * 
  * LINKS is alle gevallen kleiner dan de grens links (bij twee grenzen) of alle gevallen kleiner dan de grens (bij één grens)
  * RECHTS is alle gevallen groter dan de grens rechts (bij twee grenzen) of alle gevallen groter dan de grens (bij één grens)
  * GELIJK is alle gevallen >= grens links en <= grens rechts (bij twee grenzen) of het geval P(X=grens) (bij één grens)
  */
-public enum GrenzenOptie {
-	LINKS, RECHTS, GELIJK;
+
+public class GrenzenOptie {
+    private final String name;
+    
+    public static final GrenzenOptie LINKS =new GrenzenOptie("GrenzenOptie Links");
+    public static final GrenzenOptie GELIJK =new GrenzenOptie("GrenzenOptie Gelijk");
+    public static final GrenzenOptie RECHTS =new GrenzenOptie("GrenzenOpte Rechts");
+    
+    private GrenzenOptie(String name){
+        this.name =name;
+    }
+    
+    public String toString(){
+        return this.name;
+    }
+
 }
