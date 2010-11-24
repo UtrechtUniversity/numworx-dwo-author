fd31c7ec0d120a10d37bcdcda9d26e7f09941524
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2010-11-24 21:48:18 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/TekenVeelvlakOpdr@3563 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export-jar.bat
@@ -0,0 +1,2 @@
+copy ..\output\jar\tekenveelvlakopdr.jar N:\home\www-dev\InfoGroups\javaclasses\jars\tekenveelvlakopdr.jar
+copy ..\output\jar\tekenveelvlakopdr.jar N:\home\www-dev\InfoGroups\dwo\jars\tekenveelvlakopdr.jar
