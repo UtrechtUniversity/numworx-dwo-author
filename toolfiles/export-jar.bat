4f90232066230be4679870cb05552ba0459395c3
Boon0104 <Boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2011-04-17 14:19:16 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/JavaLogoWeb@4062 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export-jar.bat
@@ -0,0 +1,3 @@
+copy ..\output\jar\javalogoweb.jar N:\home\www-dev\InfoGroups\dwo\jars\javalogoweb.jar
+copy ..\output\jar\javalogoweb.jar N:\home\www-dev\InfoGroups\javaclasses\jars\javalogoweb.jar
+
