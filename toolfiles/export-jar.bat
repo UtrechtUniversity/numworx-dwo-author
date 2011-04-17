c670db63e7d4948e86bc7fb4cc0552d3274ce381
Boon0104 <Boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2011-04-17 10:40:13 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/BinomVerdeling@4028 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export-jar.bat
@@ -0,0 +1,2 @@
+copy ..\output\jar\binomverdeling.jar N:\home\www-dev\InfoGroups\dwo\jars\binomverdeling.jar
+copy ..\output\jar\binomverdeling.jar N:\home\www-dev\InfoGroups\javaclasses\jars\binomverdeling.jar
