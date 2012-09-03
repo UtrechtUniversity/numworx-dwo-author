ba63fb5164a01a098b2092c8675eb15bbcd5b636
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2012-09-03 12:25:35 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Kansbomen@5230 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/kansbomen/Kansbomen.java
@@ -0,0 +1,138 @@
+package fi.kansbomen;
+
+import java.awt.*;
+import java.applet.*;
+import java.util.*;
+import fi.beans.copyright.*;
+import fi.beans.scorm.*;
+import fi.beans.base64code.*;
+
+public class Kansbomen extends Applet implements ScormAppletIF 
+{
+	protected static ResourceBundle rb;
+	protected SCORM12APIInterface api;
+	private TextField textField;
+	
+	public static void main(String[] args)    
+	{	int width = 800;
+        int height = 600;
+		ScormMainFrame mf = new ScormMainFrame(new Kansbomen(),width, height);
+		mf.setTitle("Kansbomen");
+		mf.pack();
+		mf.show();
+		mf.setSize(width, height);
+	}
+	
+	public void init() 
+	{	try
+		{	api = Scorm.findAPI(this);
+		}
+		catch(Exception e){}
+		
+		setLayout(null);
+		
+		//instelling taal
+		String langArg = getParameter("language");
+		if ( langArg == null) langArg = "nl";
+		Locale language = new Locale (langArg, "");
+		rb = ResourceBundle.getBundle("fi.kansbomen.text.Text",language);
+		
+		//instelling achtergrondkleur
+		Color bgcolor = new Color(255,255,255);
+		String kleurcode = getParameter("bgcolor");
+		if(kleurcode!=null)bgcolor = new Color(Integer.parseInt(kleurcode.substring(1),16));
+		setBackground(bgcolor);
+		
+		//Fi-logo, copyright
+		FIButton fiButton = new FIButton("Kansbomen",new String[]
+			{	"versie-info: ...",
+				"auteur: ...",
+				"programmeur: ...",
+				"Freudenthal Instituut",
+				"www.fi.uu.nl",
+				""
+			});
+		fiButton.setBounds(0,0,20,30);
+		add(fiButton);
+		
+		//Test-textfield
+		textField = new TextField();
+		textField.setBounds(50,100,200,25);
+		add(textField);
+	}
+
+	public void start()
+	{	if(api!=null)
+		{	String s = api.LMSGetValue("cmi.suspend_data");
+			if(s!=null && !s.equals(""))setState(s);
+		}
+	
+	}
+	
+	public void stopSco()
+	{	stop();
+		api = null;
+	}
+	
+	public void stop()
+	{	if(api!=null)
+		{	String s = getState();
+			String d = new Double(getScore()).toString();
+			api.LMSSetValue("cmi.core.score.raw",d);
+			api.LMSSetValue("cmi.suspend_data",s);
+		}
+	}
+	
+	public void paint(Graphics g) 
+	{	g.drawString(Kansbomen.rb.getString("welkomTekst"), 50, 60 );
+		super.paint(g);
+	}
+	
+	public void setState(String s)
+	{	//decodeer de string
+		Object o = StringCodeObject.decodeStringToObject(s);
+		Hashtable h = (Hashtable)o;
+		
+		//haal de data uit de hashtabel
+		String text = (String)h.get("text");
+		
+	    //herstel de state van het applet
+	    textField.setText(text);
+	}
+	
+	public String getState()
+	{	String text = null;
+	
+		//vraag de gegevens op die de state bepalen
+	    text = textField.getText();
+	    
+	    Hashtable h = new Hashtable();
+	    
+	    //voeg de gegeven toe aan de hashtable
+	    h.put("text", text);
+	      
+	    //codeer de hashtable tot string
+	    String s = StringCodeObject.encodeObjectToString(h);
+	    return s;
+	}
+	
+	public double getScore()
+	{	return 0.5;
+	}
+	
+	public boolean hasEditMode()
+	{	return false;
+	}
+
+    public ScormEditComponentIF getEditComponent(Hashtable launchdata)
+    {	return null;
+    }
+    
+    public Parameter[] getEditableParameters()
+	{	return null;
+    }
+
+    public Parameter[] getAllParameters()
+    {	return null;
+    }
+}
