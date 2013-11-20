27fd8e50c67c99cc5467c2438ae3889989750178
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2013-11-20 17:47:07 +0000

    met nieuw manifest
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NabouwenAanzichten@6379 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/jar.bat
+++ toolfiles/jar.bat
@@ -1,2 +1,2 @@
-%JAVA_HOME%\bin\jar.exe cvfm ..\output\jar\nabouwenaanzichten.jar mainclass.mf -C ..\output\classes fi
-copy ..\output\jar\nabouwenaanzichten.jar ..\output\scorm\course\cp\sco
+%JAVA_HOME%\bin\jar.exe cvfm ..\output\jar\nabouwenaanzichten.jar ..\output\classes\meta-inf\Manifest.mf -C ..\output\classes fi
+
