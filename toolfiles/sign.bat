b8c7146ccbe2526c965c4680f20fae4fa6d45f3c
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2013-12-31 12:17:41 +0000

    toolfiles
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Kansbomen@6520 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/sign.bat
@@ -0,0 +1,3 @@
+%JAVA_HOME%\bin\pack200 --repack ../output/jar/kansbomen.jar
+%JAVA_HOME%\bin\jarsigner -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/kansbomen.jar pboon
+%JAVA_HOME%\bin\pack200 ../output/jar/kansbomen.jar.pack.gz ../output/jar/kansbomen.jar
