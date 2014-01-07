24358020119581dd46fa47707df38d1fb011aa32
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-07 17:07:41 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenOpdr@6545 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/copyjar.bat
@@ -0,0 +1,3 @@
+copy ..\output\jar\algebrapijlenopdr.jar ..\..\DWO\output\jar\algebrapijlenopdr.jar
+copy ..\output\jar\algebrapijlenopdr.jar ..\..\DWOApp\war\dwo\jars\algebrapijlenopdr.jar
+copy ..\output\jar\algebrapijlenopdr.jar.pack.gz ..\..\DWOApp\war\dwo\jars\algebrapijlenopdr.jar.pack.gz
