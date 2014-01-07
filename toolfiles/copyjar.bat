5bacb01ceb127faee0915c21feb96eba66745819
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-07 19:38:18 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NormaleVerdeling@6559 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/copyjar.bat
@@ -0,0 +1,3 @@
+copy ..\output\jar\normaleverdeling.jar ..\..\DWO\output\jar\normaleverdeling.jar
+copy ..\output\jar\normaleverdeling.jar ..\..\DWOApp\war\dwo\jars\normaleverdeling.jar
+copy ..\output\jar\normaleverdeling.jar.pack.gz ..\..\DWOApp\war\dwo\jars\normaleverdeling.jar.pack.gz
