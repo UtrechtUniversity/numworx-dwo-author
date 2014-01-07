dff05ddf47d9c5da3ab19d7a82ad840d1418c07e
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-07 17:15:46 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/CalculatorDwo@6550 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/copyjar.bat
@@ -0,0 +1,3 @@
+copy ..\output\jar\calculatordwo.jar ..\..\DWO\output\jar\calculatordwo.jar
+copy ..\output\jar\calculatordwo.jar ..\..\DWOApp\war\dwo\jars\calculatordwo.jar
+copy ..\output\jar\calculatordwo.jar.pack.gz ..\..\DWOApp\war\dwo\jars\calculatordwo.jar.pack.gz
