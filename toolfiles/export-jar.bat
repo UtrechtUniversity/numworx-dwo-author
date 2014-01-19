95d4578a6faa71ede84cdc82ca7e743c9e07442d
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-19 21:59:13 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraExpressies@6645 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-jar.bat
+++ toolfiles/export-jar.bat
@@ -3,3 +3,4 @@ copy ..\output\jar\algebraexpressies.jar.pack.gz R:\www-dev\dwo\jars\algebraexpr
 copy ..\output\jar\algebraexpressies.jar R:\tomcat\webapps-dwo\dwo\jars\algebraexpressies.jar
 copy ..\output\jar\algebraexpressies.jar.pack.gz R:\tomcat\webapps-dwo\dwo\jars\algebraexpressies.jar.pack.gz
 copy ..\output\jar\algebraexpressies.jar R:\www-dev\javaclasses\jars\algebraexpressies.jar
+copy ..\output\jar\algebraexpressies.jar.pack.gz R:\www-dev\javaclasses\jars\algebraexpressies.jar.pack.gz
