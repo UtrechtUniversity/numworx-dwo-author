633d3f55ad1b0282167e20254248f7eb47f0f42b
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-19 17:33:06 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Draaibank@6629 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-jar.bat
+++ toolfiles/export-jar.bat
@@ -2,3 +2,5 @@ copy ..\output\jar\draaibank.jar R:\www-dev\dwo\jars\draaibank.jar
 copy ..\output\jar\draaibank.jar.pack.gz R:\www-dev\dwo\jars\draaibank.jar.pack.gz
 copy ..\output\jar\draaibank.jar R:\tomcat\webapps-dwo\dwo\jars\draaibank.jar
 copy ..\output\jar\draaibank.jar.pack.gz R:\tomcat\webapps-dwo\dwo\jars\draaibank.jar.pack.gz
+copy ..\output\jar\draaibank.jar R:\www-dev\javaclasses\jars\draaibank.jar
+copy ..\output\jar\draaibank.jar.pack.gz R:\www-dev\javaclasses\jars\draaibank.jar.pack.gz
