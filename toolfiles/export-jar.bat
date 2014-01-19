9b4a810d3c3a1149348915d9e5ee9f21fdbe3c2c
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-19 19:58:06 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/GeomAlgebra@6638 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-jar.bat
+++ toolfiles/export-jar.bat
@@ -2,3 +2,5 @@ copy ..\output\jar\geomalgebra.jar R:\www-dev\dwo\jars\geomalgebra.jar
 copy ..\output\jar\geomalgebra.jar.pack.gz R:\www-dev\dwo\jars\geomalgebra.jar.pack.gz
 copy ..\output\jar\geomalgebra.jar R:\tomcat\webapps-dwo\dwo\jars\geomalgebra.jar
 copy ..\output\jar\geomalgebra.jar.pack.gz R:\tomcat\webapps-dwo\dwo\jars\geomalgebra.jar.pack.gz
+copy ..\output\jar\geomalgebra.jar R:\www-dev\javaclasses\jars\geomalgebra.jar
+copy ..\output\jar\geomalgebra.jar.pack.gz R:\www-dev\javaclasses\jars\geomalgebra.jar.pack.gz
