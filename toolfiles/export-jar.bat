6b0dea572347385ef7cb1c24271d6256bb219989
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-19 19:57:37 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NabouwenAanzichten@6637 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-jar.bat
+++ toolfiles/export-jar.bat
@@ -2,3 +2,5 @@ copy ..\output\jar\nabouwenaanzichten.jar R:\www-dev\dwo\jars\nabouwenaanzichten
 copy ..\output\jar\nabouwenaanzichten.jar.pack.gz R:\www-dev\dwo\jars\nabouwenaanzichten.jar.pack.gz
 copy ..\output\jar\nabouwenaanzichten.jar R:\tomcat\webapps-dwo\dwo\jars\nabouwenaanzichten.jar
 copy ..\output\jar\nabouwenaanzichten.jar.pack.gz R:\tomcat\webapps-dwo\dwo\jars\nabouwenaanzichten.jar.pack.gz
+copy ..\output\jar\nabouwenaanzichten.jar R:\www-dev\javaclasses\jars\nabouwenaanzichten.jar
+copy ..\output\jar\nabouwenaanzichten.jar.pack.gz R:\www-dev\javaclasses\jars\nabouwenaanzichten.jar.pack.gz
