d4e7fab74189400bb12fbb9d4e9f18016d5edea3
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-02-14 08:00:50 +0000

    signing met time stamp
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NabouwenAanzichten@6906 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/sign.bat
+++ toolfiles/sign.bat
@@ -1,3 +1,3 @@
 %JAVA_HOME%\bin\pack200 --repack ../output/jar/nabouwenaanzichten.jar
-%JAVA_HOME%\bin\jarsigner -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/nabouwenaanzichten.jar pboon
+%JAVA_HOME%\bin\jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/nabouwenaanzichten.jar pboon
 %JAVA_HOME%\bin\pack200 ../output/jar/nabouwenaanzichten.jar.pack.gz ../output/jar/nabouwenaanzichten.jar
