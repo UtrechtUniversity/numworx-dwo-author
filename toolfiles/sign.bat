cbe5081d89bd476108fd088f675ad50ad8816d6d
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-02-14 07:56:15 +0000

    signing met time stamp
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/MozArch@6902 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/sign.bat
+++ toolfiles/sign.bat
@@ -1,3 +1,3 @@
 %JAVA_HOME%\bin\pack200 --repack ../output/jar/mozarch.jar
-%JAVA_HOME%\bin\jarsigner -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/mozarch.jar pboon
+%JAVA_HOME%\bin\jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/mozarch.jar pboon
 %JAVA_HOME%\bin\pack200 ../output/jar/mozarch.jar.pack.gz ../output/jar/mozarch.jar
