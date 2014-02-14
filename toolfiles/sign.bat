b6422919dd958ac68cfc550905eaf5715fa0855a
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-02-14 07:55:21 +0000

    signing met time stamp
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Heks@6895 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/sign.bat
+++ toolfiles/sign.bat
@@ -1,3 +1,3 @@
 %JAVA_HOME%\bin\pack200 --repack ../output/jar/heks.jar
-%JAVA_HOME%\bin\jarsigner -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/heks.jar pboon
+%JAVA_HOME%\bin\jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/heks.jar pboon
 %JAVA_HOME%\bin\pack200 ../output/jar/heks.jar.pack.gz ../output/jar/heks.jar
