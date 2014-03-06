1c626f3ab5fb2d837ee600683d27b343730f4787
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-06 13:52:09 +0000

    Labels niet bold; plaatsing labels
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@7132 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/boxplot/BoxplotIndependentAxis.java
+++ src/fi/statistiek/boxplot/BoxplotIndependentAxis.java
@@ -15,214 +15,306 @@ import fi.statistiek.types.ColumnType;
 /**
  * 
  * Component that displays the independent axis in a boxplot view
- * @author Manu Drijvers
- *
+ * 
+ * @author Manu Drijvers, Sylvia van Borkulo
+ * 
  */
-public class BoxplotIndependentAxis extends JPanel {
+public class BoxplotIndependentAxis extends JPanel
+{
 	private BoxplotView boxplotView;
 	private BoxplotModel model;
-	
+
 	private boolean verticalBoxplots;
-	
+
 	private double boxWidth;
 	private boolean normalFit;
 	private int maxStringLength;
 	private ColumnType cType;
 	private AllowedTypes type;
 	private String columnName;
-	
+
 	/**
 	 * Constructor
-	 * @param model the model
-	 * @param boxplotView the view using this
-	 * @param verticalBoxplots true if using vertical boxplots, false otherwise
-	 * @param columnName the name of the column represented by the boxplotview
+	 * 
+	 * @param model
+	 *            the model
+	 * @param boxplotView
+	 *            the view using this
+	 * @param verticalBoxplots
+	 *            true if using vertical boxplots, false otherwise
+	 * @param columnName
+	 *            the name of the column represented by the boxplotview
 	 */
-	public BoxplotIndependentAxis(BoxplotModel model, BoxplotView boxplotView, boolean verticalBoxplots, String columnName) {
+	public BoxplotIndependentAxis(BoxplotModel model, BoxplotView boxplotView,
+		boolean verticalBoxplots, String columnName)
+	{
 		this.model = model;
 		this.boxplotView = boxplotView;
 		this.verticalBoxplots = verticalBoxplots;
 		this.columnName = columnName;
-		
+
 		this.setType();
 		this.determineNormalFit();
 	}
-	
-	private void setType() {
-		cType = this.model.getTableModel().getColumnTypes().get(this.model.getColumnSplitIndex());
+
+	private void setType()
+	{
+		cType = this.model.getTableModel().getColumnTypes()
+			.get(this.model.getColumnSplitIndex());
 		type = cType.getType();
 	}
-	
+
 	/**
-	 * Determine whether the labels wont overlap when drawn horizontally
-	 * and updates the preferred size
+	 * Determine whether the labels wont overlap when drawn horizontally and
+	 * updates the preferred size
 	 */
-	private void determineNormalFit() {
+	private void determineNormalFit()
+	{
 		FontMetrics fm = super.getFontMetrics(super.getFont());
-		
+
 		maxStringLength = 0;
-		
-		if(type.isNumber()) {
-			for(Double d : this.model.getSplitBinBoundaries()) {
-				if(fm.stringWidth(d.toString()) > maxStringLength) {
+
+		if (type.isNumber())
+		{
+			for (Double d : this.model.getSplitBinBoundaries())
+			{
+				if (fm.stringWidth(d.toString()) > maxStringLength)
+				{
 					maxStringLength = fm.stringWidth(d.toString());
 				}
 			}
 		}
-		else if(type.equals(AllowedTypes.ENUM)) {
-			for(String s : cType.getEnumOptions()) {
-				if(fm.stringWidth(s) > maxStringLength) {
+		else if (type.equals(AllowedTypes.ENUM))
+		{
+			for (String s : cType.getEnumOptions())
+			{
+				if (fm.stringWidth(s) > maxStringLength)
+				{
 					maxStringLength = fm.stringWidth(s);
 				}
 			}
 		}
-		else {
-			for(String s : this.model.getTableModel().getStringOptions(this.model.getColumnSplitIndex())) {
-				if(fm.stringWidth(s) > maxStringLength) {
+		else
+		{
+			for (String s : this.model.getTableModel().getStringOptions(
+				this.model.getColumnSplitIndex()))
+			{
+				if (fm.stringWidth(s) > maxStringLength)
+				{
 					maxStringLength = fm.stringWidth(s);
 				}
 			}
 		}
-		if(this.verticalBoxplots) {
-			boxWidth = (double)(this.boxplotView.getWidth()-this.boxplotView.getDependentAxisWidth()-BoxplotView.KEEP_CLEAR_WIDTH)/(double)this.model.getSplitClasses();
+		if (this.verticalBoxplots)
+		{
+			boxWidth = (double) (this.boxplotView.getWidth()
+				- this.boxplotView.getDependentAxisWidth() - BoxplotView.KEEP_CLEAR_WIDTH)
+				/ (double) this.model.getSplitClasses();
 			normalFit = maxStringLength <= boxWidth;
-			
-			if(normalFit) {
+
+			if (normalFit)
+			{
 				Dimension newPrefSize = new Dimension(0, 35);
-				if(!this.getPreferredSize().equals(newPrefSize)) {
+				if (!this.getPreferredSize().equals(newPrefSize))
+				{
 					super.setPreferredSize(newPrefSize);
-					this.invalidate(); //preferredsize has changed, invalidate
+					this.invalidate(); // preferredsize has changed, invalidate
 				}
 			}
-			else {
+			else
+			{
 				System.out.println("PreferredSize 0,strlen + 20");
 				Dimension newPrefSize = new Dimension(0, maxStringLength + 20);
-				if(!this.getPreferredSize().equals(newPrefSize)) {
-					super.setPreferredSize(new Dimension(0, maxStringLength + 20));
-					this.invalidate(); //preferredsize has changed, invalidate
+				if (!this.getPreferredSize().equals(newPrefSize))
+				{
+					super.setPreferredSize(new Dimension(0,
+						maxStringLength + 20));
+					this.invalidate(); // preferredsize has changed, invalidate
 				}
 			}
 		}
-		else {
-			this.boxWidth = (double)(super.getHeight()-BoxplotView.KEEP_CLEAR_WIDTH-boxplotView.getDependentAxisHeight())/(double)this.model.getSplitClasses();
-			Dimension newPrefSize = new Dimension(maxStringLength + fm.getHeight() + 5, 0);
-			if(!super.getPreferredSize().equals(newPrefSize)) {
+		else
+		{
+			this.boxWidth = (double) (super.getHeight()
+				- BoxplotView.KEEP_CLEAR_WIDTH - boxplotView
+					.getDependentAxisHeight())
+				/ (double) this.model.getSplitClasses();
+			Dimension newPrefSize = new Dimension(maxStringLength
+				+ fm.getHeight() + 5, 0);
+			if (!super.getPreferredSize().equals(newPrefSize))
+			{
 				super.setPreferredSize(newPrefSize);
 				this.invalidate();
 			}
-				
+
 		}
 	}
-	
-	public void paintComponent(Graphics g) {
+
+	public void paintComponent(Graphics g)
+	{
 		g.clearRect(0, 0, super.getWidth(), super.getHeight());
-		
-		if(!this.model.getTableModel().isColumnIndexValid(this.model.getColumnSplitIndex())) {
+
+		if (!this.model.getTableModel().isColumnIndexValid(
+			this.model.getColumnSplitIndex()))
+		{
 			return;
 		}
-		
+
 		this.determineNormalFit();
 		g.setColor(Color.BLACK);
-		
+
 		FontMetrics fm = g.getFontMetrics();
 		AffineTransform at = new AffineTransform();
-		at.rotate(Math.PI*0.5);
+		at.rotate(Math.PI * 0.5);
 		Font normalFont = g.getFont();
-		Font rotateFont = g.getFont().deriveFont(at);	
-		
-		if(this.verticalBoxplots) {
-			if(!normalFit) {
+		Font rotateFont = g.getFont().deriveFont(at);
+
+		if (this.verticalBoxplots)
+		{
+			if (!normalFit)
+			{
 				g.setFont(rotateFont);
 			}
-			
-			g.drawLine(boxplotView.getDependentAxisWidth(), 0, super.getWidth(), 0);
-			
-			String s;
-			if(type.isNumber()) {
-				for(int i = 0; i <= this.model.getSplitClasses(); i++) {
-					int x1 = (int)Math.round((double)i*boxWidth) + this.boxplotView.getDependentAxisWidth();
-					
-					//draw marker
-					g.drawLine(x1, 0, x1, 4);
-					
-					
-					//draw text
-					s = this.model.getSplitBinBoundaries().get(i).toString();
 
-					if(normalFit) {
-						int x2 = x1 - (int)(0.5*fm.stringWidth(s));
-						g.drawString(s, x2, 5+fm.getHeight());
+			g.drawLine(boxplotView.getDependentAxisWidth(), 0,
+				super.getWidth(), 0);
+
+			String s;
+			if (type.isNumber())
+			{
+				for (int i = 0; i <= this.model.getSplitClasses(); i++)
+				{
+					int x1 = (int) Math.round((double) i * boxWidth)
+						+ this.boxplotView.getDependentAxisWidth();
+
+					// draw marker
+					g.drawLine(x1, 0, x1, 4);
+
+					// draw text
+					s = this.model.getSplitBinBoundaries().get(i).toString();
+					
+					if (normalFit)
+					{
+						int x2 = x1 - (int) (0.5 * fm.stringWidth(s));
+						g.drawString(s, x2, 5 + fm.getHeight());
+//						System.out.println("BoxplotIndependentAxis.paintComponent(): vertical, s = " + s
+//							+ ", x = " + x2 + ", y = " + (5 + fm.getHeight()));
 					}
-					else {
-						int x2 = x1 - (int)(0.5 * fm.getHeight());
+					else
+					{
+						int x2 = x1 - (int) (0.5 * fm.getHeight());
 						g.drawString(s, x2, 5);
 					}
-				}				
-			}
-			else {
-				for(int i = 0; i < this.model.getSplitClasses(); i++) {
-					int x1 = (int)Math.round((i+0.5)*boxWidth) + this.boxplotView.getDependentAxisWidth();
-					
-					if(type.equals(AllowedTypes.ENUM)) {
+				}
+			} // number type 
+			else
+			{
+				for (int i = 0; i < this.model.getSplitClasses(); i++)
+				{
+					int x1 = (int) Math.round((i + 0.5) * boxWidth)
+						+ this.boxplotView.getDependentAxisWidth();
+					// TODO syl: 40 is hardcoded...
+					int x = (int) Math.round(i * boxWidth) + 40 + this.boxplotView.getDependentAxisWidth();
+
+					if (type.equals(AllowedTypes.ENUM))
+					{
 						s = cType.getEnumOptions()[i];
 					}
-					else {
-						s = this.model.getTableModel().getStringOptions(this.model.getColumnSplitIndex()).get(i);
+					else
+					{
+						s = this.model.getTableModel()
+							.getStringOptions(this.model.getColumnSplitIndex())
+							.get(i);
 					}
-					
-					if(normalFit) {
-						int x2 = x1 - (int)(0.5*fm.stringWidth(s));
-						g.drawString(s, x2, 5+fm.getHeight());
+
+					if (normalFit)
+					{
+						//int x2 = x1 - (int) (0.5 * fm.stringWidth(s));
+//						g.drawString(s, x2, 5 + fm.getHeight());
+						// x-parameter was te groot
+						g.drawString(s, x, 5 + fm.getHeight());
+//						System.out.println("BoxplotIndependentAxis.paintComponent(): vertical, s = " + s
+//							+ ", x = " + x2 + ", y = " + (5 + fm.getHeight()));
+//						System.out.println("BoxplotIndependentAxis.paintComponent(): vertical, s = " + s
+//							+ ", x = " + x + ", y = " + (5 + fm.getHeight()));
 					}
-					else {
-						int x2 = x1 - (int)(0.5 * fm.getHeight());
+					else
+					{
+						int x2 = x1 - (int) (0.5 * fm.getHeight());
 						g.drawString(s, x2, 5);
 					}
 				}
 			}
-			
-			Font boldFont = normalFont.deriveFont(Font.BOLD);
-			g.setFont(boldFont);
-			g.drawString(this.columnName, super.getWidth()/2-fm.stringWidth(columnName), super.getHeight()-2);
 
-		}
-		else {
-			g.drawLine(super.getWidth()-1, 0, super.getWidth()-1, super.getHeight()-boxplotView.getDependentAxisHeight());
-			
-			if(this.type.isNumber()) {
-				for(int i = 0; i <= this.model.getSplitClasses(); i++) {
-					String s = Double.toString(this.model.getSplitOptions().getBinBoundaries().get(i));
-					int y = super.getHeight()-(boxplotView.getDependentAxisHeight()+(int)Math.round(i*this.boxWidth));
-					g.drawLine(this.getWidth()-5, y, this.getWidth(), y);
-					g.drawString(s, this.getWidth()-fm.stringWidth(s)-7, y+(int)(0.5*fm.getHeight()));
+			// geen bold voor variabelelabel
+			// Font boldFont = normalFont.deriveFont(Font.BOLD);
+			// g.setFont(boldFont);
+			g.drawString(this.columnName,
+				super.getWidth() / 2 - fm.stringWidth(columnName),
+				super.getHeight() - 2);
+
+		} // vertical boxplots
+		else
+		{
+			g.drawLine(super.getWidth() - 1, 0, super.getWidth() - 1,
+				super.getHeight() - boxplotView.getDependentAxisHeight());
+
+			if (this.type.isNumber())
+			{
+				for (int i = 0; i <= this.model.getSplitClasses(); i++)
+				{
+					String s = Double.toString(this.model.getSplitOptions()
+						.getBinBoundaries().get(i));
+					int y = super.getHeight()
+						- (boxplotView.getDependentAxisHeight() + (int) Math
+							.round(i * this.boxWidth));
+					g.drawLine(this.getWidth() - 5, y, this.getWidth(), y);
+					g.drawString(s, this.getWidth() - fm.stringWidth(s) - 7, y
+						+ (int) (0.5 * fm.getHeight()));
 				}
 			}
-			else {
+			else
+			{
 				String s;
-				for(int i = 0; i < this.model.getSplitClasses(); i++) {
-					if(type.equals(AllowedTypes.ENUM)) {
+				for (int i = 0; i < this.model.getSplitClasses(); i++)
+				{
+					if (type.equals(AllowedTypes.ENUM))
+					{
 						s = this.cType.getEnumOptions()[i];
 					}
-					else {
-						s = this.model.getTableModel().getStringOptions(this.model.getColumnSplitIndex()).get(i);
+					else
+					{
+						s = this.model.getTableModel()
+							.getStringOptions(this.model.getColumnSplitIndex())
+							.get(i);
 					}
-					int y = super.getHeight()-(boxplotView.getDependentAxisHeight()+(int)Math.round((i+0.5)*this.boxWidth));
-					//int y = (int)Math.round((this.model.getSplitClasses()-i-0.5)*this.boxWidth);
-					g.drawString(s, this.getWidth()-fm.stringWidth(s)-2, y-(int)(0.5*fm.getHeight()));
+					int y = super.getHeight()
+						- (boxplotView.getDependentAxisHeight() + (int) Math
+							.round((i + 0.5) * this.boxWidth));
+					// int y =
+					// (int)Math.round((this.model.getSplitClasses()-i-0.5)*this.boxWidth);
+
+//					g.drawString(s, this.getWidth() - fm.stringWidth(s) - 2, y
+//						- (int) (0.5 * fm.getHeight()));
+					// labels iets lager
+					g.drawString(s, this.getWidth() - fm.stringWidth(s) - 4, y);
 				}
 			}
 			at = new AffineTransform();
-			at.rotate(Math.PI*1.5);
-			Font boldRotateFont = normalFont.deriveFont(at);
-			boldRotateFont = boldRotateFont.deriveFont(Font.BOLD);
-			g.setFont(boldRotateFont);
-			g.drawString(this.columnName, fm.getHeight() + 2, super.getHeight()/2+fm.stringWidth(columnName));
+			at.rotate(Math.PI * 1.5);
 
-			
-			
+			// geen bold voor variabelelabel
+			Font rotateFont2 = normalFont.deriveFont(at);
+			// rotateFont2 = boldRotateFont.deriveFont(Font.BOLD);
+			g.setFont(rotateFont2);
+//			g.drawString(this.columnName, fm.getHeight() + 2, super.getHeight()
+//				/ 2 + fm.stringWidth(columnName));
+			// iets verder van as af
+			g.drawString(this.columnName, fm.getHeight(), super.getHeight()
+				/ 2 + fm.stringWidth(columnName)/2);
+
 		}
-		
-	
+
 		g.setFont(normalFont);
-	}	
+	}
 }
