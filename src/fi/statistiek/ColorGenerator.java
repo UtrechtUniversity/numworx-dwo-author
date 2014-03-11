72bc2f77f6d335542de3b42acf9c31dce6c352c6
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-11 15:23:31 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@7166 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/statistiek/ColorGenerator.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package fi.statistiek;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author Sylvia van Borkulo
+ * 
+ */
+public class ColorGenerator
+{
+	// oud
+	private static Color[] COLORS =
+		{ Color.RED, Color.GREEN, Color.BLUE };
+	// nieuw (Dark2 van http://colorbrewer2.org/ 
+//	private static Color[] COLORS = {
+//		new Color(27, 158, 119),
+//		new Color(217,95,2),
+//		new Color(117,112,179),
+//		new Color(231,41,138),
+//		new Color(102,166,30),
+//		new Color(230,171,2),
+//		new Color(166,118,29),
+//		new Color(102,102,102)
+//	};
+	
+	private static ArrayList<Color> colorList = 
+		new ArrayList<Color>(Arrays.asList(COLORS));
+	
+	private static Random random = new Random();
+
+	/**
+	 * Get color for displaying multiple split groups in a single view
+	 * 
+	 * @param number
+	 *            the number of the split group
+	 * @return the color in which this split group will be displayed
+	 */
+	public static Color getColor(int number)
+	{
+		if (number < colorList.size())
+		{
+			return colorList.get(number);
+		}
+		else
+		{
+			Color c = new Color(random.nextInt(256),
+				random.nextInt(256), random.nextInt(256));
+			colorList.add(c);
+			return c;
+		}
+	}
+
+	/**
+	 * Get random color
+	 * 
+	 * @return the random color
+	 */
+	public static Color getColor()
+	{
+		Color c = new Color(random.nextInt(256),
+			random.nextInt(256), random.nextInt(256));
+		colorList.add(c);
+		return c;
+	}
+}
