3be7e768fbdffde879dcb1e80e4fa391ae9881f1
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 17:46:48 +0000

    extra talen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Stroomdiagrammen@7218 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/stroomdiagrammen/text/Text_ca.java
@@ -0,0 +1,55 @@
+package fi.stroomdiagrammen.text;
+
+public class Text_ca extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {
+            {"startButtonText", "Start Flow Diagrams"},
+            {"titelText", "Flow Diagrams"},
+            {"versionText", "version "},
+            {"programText", "programmer: Huub Nilwik"},
+            {"authorText", "autor: Gerard Koolstra"},
+            {"fiText", "FREUDENTHAL INSTITUTE"}, 
+
+            {"appletText", "Applet"},
+            {"stopText", "Stop"},
+            {"infoText", "Info"},
+
+            {"decSep", "."},                                                        
+
+            {"newDiagramText", "new diagram"},
+            {"previousText", "back"},
+            {"flowOnText", "flow on"},
+            {"flowOffText", "flow off"},
+            {"deleteText", "remove"},
+
+            {"represText", "Calculations"},
+            {"decimalText", "Decimal"},
+            
+            {"percentageText", "Percentages"},
+
+            {"fractionText", "Fractions"},
+
+            {"thicknessText", "Flow width"},
+            {"relativeText", "Relative"},
+            {"absoluteText", "Absolute"},
+
+            {"optionsText", "Options"},
+            {"labelsText", "Vertices with labels"},
+            {"addRootText", "Add another source"},
+            
+            {"berekenMenuText", "Calculations menu" },
+            {"berekenInBreukenText", "Calculate in fractions" },
+            {"stroombreedteMenuText", "Flow width menu" },
+            {"stroombreedteAbsoluutText", "Flow width absolute" },
+            {"optiesMenuText", "Options menu" },
+            {"labelsText", "Vertices with labels" },
+            {"aantalBronnenText", "Number of sources" },
+            
+		};
+}
+
+
+
+
