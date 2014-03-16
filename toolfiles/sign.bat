9e5a0b77f8906c63c9f5b54705f2a6dc60ca3b77
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 18:03:26 +0000

    extra taalfiles
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Grafiek3DTest@7224 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/sign.bat
+++ toolfiles/sign.bat
@@ -1,3 +1,3 @@
 %JAVA_HOME%\bin\pack200 --repack ../output/jar/grafiek3dtest.jar
-%JAVA_HOME%\bin\jarsigner -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/grafiek3dtest.jar pboon
+%JAVA_HOME%\bin\jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/grafiek3dtest.jar pboon
 %JAVA_HOME%\bin\pack200 ../output/jar/grafiek3dtest.jar.pack.gz ../output/jar/grafiek3dtest.jar
