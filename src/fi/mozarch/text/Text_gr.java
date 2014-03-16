6d51b8d43cd380bd8b2fe2a3dd60c6e5ded7658d
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 17:36:39 +0000

    extra talen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/MozArch@7213 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/mozarch/text/Text_gr.java
@@ -0,0 +1,20 @@
+package fi.mozarch.text;
+
+public class Text_gr extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {		{ "wisKnopLabel" , "Clear" },
+                { "opstuurKnopLabel", "Send"},
+				{ "BestandenKnopLabel", "Show/open files"},
+				{ "infoKnopLabel", "Info"},
+				
+				{ "fractielenTekst", "Fractiles" },
+				{ "startFiguurTekst", "First piece" },
+				{ "aantalHoekpuntenTekst", "Number of vertices" },
+				{ "aantalPerZijdeTekst", "Number of figures per edge" },
+				{ "fractielTypeTekst", "Fractile type" },
+				
+        };
+}
+
