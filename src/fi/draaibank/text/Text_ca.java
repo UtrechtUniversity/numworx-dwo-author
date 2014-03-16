c7e091134a8c08c841257f96a7552773435b82bc
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 17:26:25 +0000

    extra talen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Draaibank@7207 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/draaibank/text/Text_ca.java
@@ -0,0 +1,15 @@
+package fi.draaibank.text;
+
+public class Text_ca extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {
+                { "wisKnopLabel" , "Clear" },
+                { "terugKnopLabel" , "Back" },
+                { "demoVersionTekst", "Demo version"},
+                { "zoomOptionTekst", "Zoom option"},
+				
+        };
+}
+
