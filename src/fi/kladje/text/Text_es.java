4b100359dabebeaddcd8a284e7a3b30facb688c4
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 18:17:28 +0000

    extra languagefiles
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Kladje@7225 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/kladje/text/Text_es.java
@@ -0,0 +1,29 @@
+package fi.kladje.text;
+
+public class Text_es extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {
+        	{ "terugTekst" , "undo" },
+        	{ "wisTekst" , "erase all" },
+            { "kleurkeuzeTekst" , "Color choice option" },
+            { "lijnenTekst" , "Lines" },
+            { "ruitjesTekst" , "Grid" },
+            { "blancoTekst" , "Blank" },
+            { "lijnTekenenTekst" , "Drawing lines" },
+            { "rechthoekTekenenTekst" , "Drawing rectangles" },
+            { "cirkelTekenenTekst" , "Drawing circles" },
+            { "tekstTekenenTekst" , "Drawing text" },
+            { "roterenTekst" , "Rotate objects" },
+            { "schalenTekst" , "Scale objects" },
+
+
+            
+            
+		};
+}
+
+
+
+
--- toolfiles/sign.bat
+++ toolfiles/sign.bat
@@ -1,3 +1,3 @@
 %JAVA_HOME%\bin\pack200 --repack ../output/jar/kladje.jar
-%JAVA_HOME%\bin\jarsigner -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/kladje.jar pboon
+%JAVA_HOME%\bin\jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/kladje.jar pboon
 %JAVA_HOME%\bin\pack200 ../output/jar/kladje.jar.pack.gz ../output/jar/kladje.jar
