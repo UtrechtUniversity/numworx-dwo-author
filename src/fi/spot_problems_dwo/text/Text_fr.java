c54ab8ff078ae84858e02c88c858bb7fefa2fad1
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 17:43:58 +0000

    extra talen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Spot_Problems_dwo@7217 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/spot_problems_dwo/text/Text_fr.java
@@ -0,0 +1,56 @@
+package fi.spot_problems_dwo.text;
+
+public class Text_fr extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {
+        	{ "aantalTekst" , "Number" },
+            { "scoreMaxTekst" , "Maximum score" },
+            { "niveauTekst" , "Level" },
+            { "kijkNaActiefTekst" , "Checking active"},
+            
+            { "vNumbersText", "V numbers" },
+            { "wNumbersText", "W numbers" },
+            { "sqrNumbersText", "square numbers" },
+            { "oblNumbersText", "oblong numbers" },
+            { "triaNumbers1Text", "triangular numbers 1" },
+            { "triaNumbers2Text", "triangular numbers 2" },
+            { "triaNumbers3Text", "triangular numbers 3" },
+            { "pentaNumbersText", "pentagonal numbers" },
+            { "boxNumbersText", "box numbers" },
+            { "plusNumbersText", "plus numbers" },
+            { "spiralNumbersText", "spiral numbers" },
+            { "towerNumbersText", "tower numbers" },
+            { "blockNumbers1Text", "block numbers 1" },
+            { "fNumbers1Text", "F numbers 1" },
+            { "fNumbers2Text", "F numbers 2" },
+            { "flapNumbersText", "flap numbers" },
+            { "flipNumbersText", "flip numbers" },
+            { "tileNumbers1Text", "tile numbers 1" },
+            { "tileNumbers2Text", "tile numbers 2" },
+            { "tileNumbers3Text", "tile numbers 3" },
+            { "tileNumbers4Text", "tile numbers 4" },
+            { "hNumbers1Text", "H numbers 1" },
+            { "hNumbers2Text", "H numbers 2" },
+            { "lNumbers1Text", "L numbers 1" },
+            { "lNumbers2Text", "L numbers 2" },
+            { "xNumbers1Text", "X numbers 1" },
+            { "xNumbers2Text", "X numbers 2" },
+            { "zNumbersText", "Z numbers" },
+            { "insectNumbersText", "insect numbers" },
+            { "spiderNumbersText", "spider numbers" },
+            { "piramidNumbersText", "piramid numbers" },
+            { "tableNumbersText", "table numbers" },
+            { "stairNumbersText", "stair numbers" },
+            { "blockNumbers2Text", "block numbers 2" },
+            { "blockNumbers3Text", "block numbers 3" },
+            { "triaNumbers4Text", "triangular numbers 4" },
+            
+            
+		};
+}
+
+
+
+
