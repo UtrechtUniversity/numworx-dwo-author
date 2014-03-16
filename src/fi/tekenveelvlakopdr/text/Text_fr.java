d5b2d96b6ea35372aa7b41afe9bb2ee6a670da1f
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 17:51:17 +0000

    extra talen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/TekenVeelvlakOpdr@7220 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/tekenveelvlakopdr/text/Text_fr.java
@@ -0,0 +1,28 @@
+package fi.tekenveelvlakopdr.text;
+
+public class Text_fr extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {
+				{ "lijnKnopLabel" , "Construct line" },
+				{ "vlakKnopLabel" , "Construct plane" },
+				{ "verbergBasisKnopLabel" , "Hide base polyhedron" },
+				{ "toonBasisKnopLabel" , "Show base polyhedron" },
+				{ "wisLijnKnopLabel" , "Clear lines" },
+                { "wisVlakKnopLabel" , "Clear planes" },
+                { "terugKnopLabel" , "Undo" },
+				{ "zijdeLabel" , "zoom" },
+				{ "kubusLabel" , "cube" },
+				{ "tetraederLabel" , "tetrahedron" },
+				{ "dodecaederLabel" , "dodecahedron" },
+				{ "octaederLabel" , "octahedron" },
+				{ "icosaederLabel" , "icosahedron" },
+                
+                { "alleenViewerCBLabel" , "Viewer Only" },
+                { "draaibaarCBLabel" , "Rotatable" },
+                { "hulpPuntenCBLabel" , "Help points" },
+
+        };
+}
+
