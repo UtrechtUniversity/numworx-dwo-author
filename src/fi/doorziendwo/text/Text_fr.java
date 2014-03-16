cf753e7bdd4b7b230f252b4a7e472c77047cff83
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 17:24:40 +0000

    extra talen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DoorzienDWO@7206 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/doorziendwo/text/Text_fr.java
@@ -0,0 +1,290 @@
+package fi.doorziendwo.text;
+
+public class Text_fr extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {
+        	// start button
+            {"startButtonText", "Start Doorzien 4" } ,
+            // frame title/copyright                    
+            {"titelText",       "Doorzien DWE" } ,
+            {"versionText",     "version 20061104" } ,
+            {"programText",     "Programmer: Huub Nilwik" } ,
+            {"authorText",      "author: " } ,  
+            {"fiText",          "Freudenthal Institute" } , 
+            {"developText",     "Developed by the" } ,
+                                 
+            {"forEPNText",      "for EPN/Number and Space" } ,
+                   
+            {"copyRightText",   "\u00A9 2004-2006" } ,  
+                                
+            // applet menu                    
+            {"appletText",      "Applet" } ,
+            {"stopText",        "Stop" } ,
+            {"infoText",        "Info" } ,
+            // figure menu                    
+            {"figureText",      "Objects" } ,
+                                
+            {"octahedronText",  "Octahedron" } ,
+            {"blockText",       "Block" },
+
+            {"cylinderText",    "Cylinder" } ,
+
+            {"housesText",      "Houses" },
+
+            {"pirHouseText",    "House with piramidal roof" },
+                                
+            {"edgeHouseText",   "House with edged roof" },
+                                
+            {"conesText",       "Cones" } ,
+                                
+            {"cone1Text",       "Cone 1" },
+                                
+            {"cone2Text",       "Cone 2" },
+                                
+            {"cone3Text",       "Cone 3" },
+                                
+            {"cone4Text",       "Cone 4" },
+                                
+            {"cubeText",        "Cube" },
+                                
+            {"piramidsText",    "Piramids" },
+                                
+            {"threePiramidText", "Three sided piramid" } ,
+                                 
+            {"fourPiramidText",  "Four sided piramid" },
+                                 
+            {"fivePiramidText",  "Five sided piramid" },
+                                 
+            {"sixPiramidText",   "Six sided piramid" },
+                                 
+            {"sevenPiramidText", "Seven sided piramid" },
+                                 
+            {"eightPiramidText", "Eight sided piramid" },
+                                 
+                                 
+            {"prismsText",       "Prisms" } ,
+                                 
+            {"threePrismText",   "Three sided prism" },
+                                 
+            {"fourPrismText",    "Four sided prism" },
+                                 
+            {"fivePrismText",    "Five sided prism" },
+                                 
+            {"sixPrismText",     "Six sided prism" },
+                                 
+            {"dodecahedronText", "Dodecahedron" },
+                                 
+            {"icosahedronText", "Icosahedron" },
+                                
+            {"tetrahedronText", "Tetrahedron" } ,
+                                
+
+            {"myFigureText",    "My figure" },
+                                                                
+
+            // options menu
+            {"optionsText",     "Options" },
+                                 
+            {"centralProjText", "Central projection" },
+                                 
+            {"parallelProjText", "Parallel projection" },
+                                 
+            {"lettersText",     "Add letters to points" },
+                                 
+            {"helpPointsText",  "Help points" },
+                                 
+            {"noHelpPointsText", "No help points" },
+                                 
+            {"divideSidesText",  "Divide edges into" },
+                                 
+            {"twoPartsText",     "two parts" },
+                                 
+            {"threePartsText",   "three parts" },
+                                 
+            {"fourPartsText",    "four parts" },
+                                 
+            {"fivePartsText",    "five parts" },
+                                 
+            {"sixPartsText",     "six parts" },
+                                 
+            {"sevenPartsText",   "seven parts" },
+                                 
+            {"eightPartsText",   "eight parts" },
+                                 
+            {"ninePartsText",    "nine parts" },
+                                 
+            {"tenPartsText",     "ten parts" },
+                                 
+                                 
+            
+                                
+            // help messages
+            {"rotateText",      "Click and drag with the mouse to rotate the object" },
+                                
+            // right toolbar                    
+            {"escapeText",      "Abort action" },
+                                
+            
+            
+            {"wireFrameText",   "Wireframe object" },
+                                
+            {"solidText",       "Solid object" },
+                                
+            {"zoomInText",      "Zoom in" },
+                                
+            {"zoomOutText",     "Zoom out" },
+                                
+            {"conDrawText",     "Fold-out" },
+                                
+            {"conDrawSelectText", "Click with the mouse on the side which will be the center of the fold-out" },
+                                  
+            {"wholeFigureText", "Object" },
+                                
+            {"flatText",        "flat" },
+                                
+            {"volumeText",      "Volume" },
+                                
+            {"largeFigureText", "large object" },
+                                
+            {"smallFigureText", "small object" },
+                                
+                                
+                                
+            
+            // top toolbar                    
+            {"drawLineText",    "Draw line" },
+                                
+            
+            {"drawLinesText",   "Draw lines" },
+                                
+    // niet relevant meer                            
+            {"drawLineEndText", "Stop drawing lines in the object" },
+                                
+    // niet relevant meer                                                        
+            {"drawLineCancelText", "Stop drawing the line" },
+                                   
+                                
+            {"linePoint1Text",  "Choose the first point of the line by clicking with the mouse on a vertex or on an edge" },
+                                
+            {"linePoint2Text",  "Choose the second point of the line by clicking with the mouse on a vertex or on an edge" },
+                                
+            {"deleteLineText",  "Delete line" },
+                                
+            {"selectDeleteLineText",  "Click with the mouse on the line that should be deleted" },                        
+                                      
+            {"lengLinesText",  "Extend segments" },
+                               
+            {"shortLinesText", "Shorten segments" },
+                               
+                                      
+                                      
+                                      
+            {"drawPlaneText",   "Draw plane" },
+                                
+                                
+            {"drawPlanesText",  "Draw planes" },
+                                
+    // niet relevant meer                            
+            {"drawPlaneEndText", "Stop drawing planes in the object" },
+                                
+    // niet relevant meer                                                        
+            {"drawPlaneCancelText", "Stop drawing the plane " },
+                                    
+                                
+            {"planePoint1Text", "Choose the first point of the plane by clicking with the mouse on a vertex or on an edge" },
+                                
+            {"planePoint2Text", "Choose the second point of the plane by clicking with the mouse on a vertex or on an edge" },
+                                
+            {"planePoint3Text", "Choose the third point of the plane by clicking with the mouse on a vertex or on an edge" },
+                                
+
+            {"parPlaneText",  "Draw parallel plane" },
+                              
+            {"selectParPlaneText",  "Click with the mouse on a line of the plane which should be copied" },
+                                    
+            {"parPlanePointText",   "Choose a point of the new parallel plane by clicking with the mouse on a vertex or on an edge" },
+                                    
+                                
+                                
+            {"deletePlaneText", "Delete plane" },
+                                
+            {"selectDeletePlaneText", "Click with the mouse on a line of the plane that should be deleted" },
+                                      
+            {"planesFilledText", "Fill planes" },
+                                 
+            {"planesEmptyText",  "Make planes transparent" },
+                                 
+            {"rotatePlaneText", "Rotate plane" },
+                                
+            {"selectRotatePlaneText", "Click with the mouse on a line of the plane. The plane will be rotated around this line" },
+                                      
+            {"translatePlaneText", "Translate plane" },
+                                   
+            {"selectTranslatePlaneText", "Click with the mouse on a line of the plane that should be translated" },
+                                         
+            {"showCutText", "Show intersection" },
+                            
+            {"selectShowCutText", "Click with the mouse on a line of the plane whose intersection with the object should be shown" },
+                                  
+            {"hideCutText", "Hide intersection" } ,
+                            
+            {"cutFigureText", "Split object" } ,
+                              
+            {"glueFigureText", "Paste objects together" },
+                               
+                              
+                              
+            {"selectCutPlaneText", "Click with the mouse on a line of the plane along which the object should be cut" },
+                                   
+            {"selectCutFigureText", "Click with the mouse to select the part of the object which should become the new object, or use the Paste-button" },
+                                    
+
+            {"undoText", "Undo" } ,
+
+            {"redoText", "Redo" },
+                                    
+                                
+            {"decSep",          "." } ,   
+            
+            // ViewPanel
+            {"startButtonViewerText", "Start program for editing" } , 
+            {"opnieuwButtonViewerText", "Again" } , 
+            {"uitlegButtonViewerText", "Help" } , 
+             
+            //InteractieEditPanel        				   
+            {"EditPanel_modus", "Mode" } , 
+            {"EditPanel_viewer", "Viewer" } ,         	
+            {"EditPanel_doorzien", "Doorzien" } , 
+            {"EditPanel_vieweropties", "Demo/viewer options" } , 
+            {"EditPanel_doorzienopties", "Doorzien options" } , 
+                    				   
+    	    {"EditPanel_draaibaar", "Rotatable" } ,  
+            {"EditPanel_Rand", "Border" } ,  
+            {"EditPanel_ontwerpmogelijkheid", "Design option" } ,  
+            {"EditPanel_reset-optie", "Reset-option" } ,  
+            {"EditPanel_vouwslider", "Slider for folding" },  
+            
+            
+            {"EditPanel_demoOptie", "Demo" } , 
+            
+            {"EditPanel_figurenMenuOptie", "Menu figures" } , 
+            {"EditPanel_optiesMenuOptie", "Menu options" } , 
+            {"EditPanel_helpBarOptie", "Help bar" } , 
+            
+            {"EditPanel_lijnTekenOptie", "Drawing lines" } , 
+            {"EditPanel_lijnVerlengOptie", "Lengthen lines" } , 
+            
+            {"EditPanel_vlakTekenOptie", "Drawing planes" } , 
+            {"EditPanel_evenwijdigVlakOptie", "Parallel planes" } , 
+            {"EditPanel_toonDoorsnedeOptie", "Show cut surface" } , 
+            {"EditPanel_splitsFiguurOptie", "Split figure" }, 
+            {"EditPanel_bouwplaatOptie", "Foldout" }, 
+            
+            {"EditPanel_previewOptie", "Preview option" }, 
+				
+				
+        };
+}
+
