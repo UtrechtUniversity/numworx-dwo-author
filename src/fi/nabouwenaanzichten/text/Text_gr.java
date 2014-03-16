5f4e38e4cc714fa282005b74bf9a50bfec3b7408
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-16 17:38:50 +0000

    extra talen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NabouwenAanzichten@7214 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/nabouwenaanzichten/text/Text_gr.java
@@ -0,0 +1,121 @@
+package fi.nabouwenaanzichten.text;
+
+public class Text_gr extends java.util.ListResourceBundle
+{
+        public Object [] [] getContents() { return contents; }
+        static final Object [] [] contents =
+        {
+				{ "aantalKLabel" , "Number of cubes: " },
+				{ "voorLabel" , "front" },
+				{ "rechtsLabel" , "right" },
+				{ "bovenLabel" , "top" },
+                { "volLeegKnopLabel1" , "Fill up" },
+                { "volLeegKnopLabel2" , "Remove all" },
+				{ "aanzichtenKnopLabel1" , "Show views" },
+				{ "aanzichtenKnopLabel2" , "Hide views" },
+				{ "opdrachtKnopLabel1" , "Problem" },
+				{ "opdrachtKnopLabel2" , "Start" },
+				{ "ipRegel1Label" , "Build" },
+				{ "ipRegel2Label" , "Break down" },
+				{ "fig1" , "Figure 1" },
+				{ "fig2" , "Figure 2" },
+				{ "fig3" , "Figure 3" },
+				{ "fig4" , "Figure 4" },
+				{ "fig5" , "Figure 5" },
+				{ "fig6" , "Figure 6" },
+				{ "fig7" , "Figure 7" },
+				{ "fig8" , "Figure 8" },
+				{ "fig9" , "Figure 9" },
+				{ "fig10" , "Figure 10" },
+				
+				{ "niveau" , "Level " },
+				{ "scoreLabel" , "Score: " },
+				
+				{ "textPanelRegel0_0" , "Below you see the silhouet of a cube building." },
+				{ "textPanelRegel0_1" , "Copy this building on the grid on the right, " },
+				{ "textPanelRegel0_2" , "You can do this by clicking on the grid." },
+				{ "textPanelRegel0_3" , "If you do this correct you get 10 points.  " },
+				{ "textPanelRegel0_4" , "" },
+				{ "textPanelRegel0_5" , "" },
+				{ "textPanelRegel0_6" , "" },
+				{ "textPanelRegel0_7" , "" },
+				{ "textPanelRegel0_8" , "" },
+				{ "textPanelRegel0_9" , "" },
+				{ "textPanelRegel0_10" , "" },
+				{ "textPanelRegel0_11" , "" },
+				{ "textPanelRegel0_12" , "" },
+				
+				{ "textPanelRegel1_0" , "Below you see the front side view and the " },
+				{ "textPanelRegel1_1" , "right side view of a cube building. Make a  " },
+				{ "textPanelRegel1_2" , "building on the grid on the right, that has " },
+				{ "textPanelRegel1_3" , "the same views. " },
+				{ "textPanelRegel1_4" , "Click on the grid to build (or to break down) " },
+				{ "textPanelRegel1_5" , "If you do this correct, you get 5 points." },
+				{ "textPanelRegel1_6" , "Use as few cubes as possible to get the" },
+				{ "textPanelRegel1_7" , "10 point maximum score." },
+				{ "textPanelRegel1_8" , "" },
+				{ "textPanelRegel1_9" , "" },
+				{ "textPanelRegel1_10" , "" },
+				{ "textPanelRegel1_11" , "" },
+				{ "textPanelRegel1_12" , "" },
+				
+				{ "textPanelRegel2_0" , "Copy the construction you see the three" },
+				{ "textPanelRegel2_1" , "views of below." },
+				{ "textPanelRegel2_2" , "When you get it right, you will get 5 points." },
+				{ "textPanelRegel2_3" , "Use as few cubes as possible to get the" },
+				{ "textPanelRegel2_4" , "10 point maximum score." },
+				{ "textPanelRegel2_5" , "Click on the grid or on another cube to add " },
+				{ "textPanelRegel2_6" , "a new cube. Click and hold to remove a cube. " },
+				{ "textPanelRegel2_7" , "" },
+				{ "textPanelRegel2_8" , "" },
+				{ "textPanelRegel2_9" , "" },
+				{ "textPanelRegel2_10" , "" },
+				{ "textPanelRegel2_11" , "" },
+				{ "textPanelRegel2_12" , "" },
+				
+				// edit state opties
+				{ "rotatieVast" , "No rotation" },
+				{ "bouwSloopKeuze", "Choice build/break down"},
+				{ "isVoorbeeld", "No building/breaking down"},
+				{ "perspectief", "Perspective" },
+				
+				{ "volLeegKnop", "Button fill up/remove all" },
+				{ "aantalBlokjes", "Show number of cubes" },
+				
+				{ "zwartePijl", "Front: black arrow" },
+				{ "rodeBalk", "Front: red beam" },
+				{ "geenVoorkant", "No indication of front" },
+
+				{ "plattegrond", "Grid with" },
+				{ "plattegrond2", "       heights" },
+				{ "tekenAanzicht", "Draw view mode" },
+				
+				{ "blokkenbouwsel", "Block building" },
+				{ "silhouet", "Building in silhouet" },				
+				{ "3Aanzichten", "3 views" },
+				{ "voorZijAanzicht", "Front and side view" },
+				{ "bovenVoorAanzicht", "Top and front view" },
+				{ "bovenZijAanzicht", "Top and side view" },
+				{ "bovenAanzicht", "View from above" },
+				{ "voorAanzicht", "Frontal view" },
+				{ "rechtsAanzicht", "View from the right" },
+				
+				{ "roosterGrootte", "Grid size (2 - 15)" },				
+				
+				{ "viewerOpties", "viewer options" },
+				{ "nakijkOpties", "check options" },
+				
+				{ "kijkNa", "Enable check" },
+				
+				{ "controleer", "Check" },
+				
+				{ "aantalKubus" , "Number of cubes" },
+				
+				{ "kijkNaTekst" , "Check" },
+				
+				{ "maxScoreTekst" , "Maximum score" },
+				
+				{ "blokjesTekst" , "cube(s)" },
+        };
+}
+
