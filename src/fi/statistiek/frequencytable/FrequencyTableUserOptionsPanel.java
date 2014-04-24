ef1b8b39144aa4029b0856496aa8afddc8bc34a7
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-04-24 16:21:49 +0000

    mogelijkheid om te splitsen ingebouwd
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@7610 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/frequencytable/FrequencyTableUserOptionsPanel.java
+++ src/fi/statistiek/frequencytable/FrequencyTableUserOptionsPanel.java
@@ -72,10 +72,33 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 	private JCheckBox showFreqBox;
 	private JCheckBox showCumulativeBox;
 
-	private JButton okButton;
+	// split settings
+	private JButton splitButton;
+	private JLabel splitsVarLabel;
+	private JComboBox splitVarBox;
+	private JLabel splitBinsLabel;
+	private JComboBox splitBinsBox;
+	private JButton splitChooseBoundariesButton;
+	private JSeparator separator3;
+	private JLabel splitMinBoundaryLabel;
+	private JTextField splitMinBoundaryField;
+	private JLabel splitBinWidthLabel;
+	private JTextField splitBinWidthField;
+	private JLabel splitBoundariesLabel;
+	private JTextArea splitBoundariesArea;
+	private JScrollPane splitBoundariesAreaScrollPane;
+	private JLabel splitNoObjectsLabel;
+	private JLabel splitMinValueLabel;
+	private JLabel splitMaxValueLabel;
 
-	private boolean boundariesVisible;
+	private boolean BoundariesVisible;
+	private boolean splitBoundariesVisible;
+	private boolean splitOptionsVisible;
 	private boolean enumClasses;
+	private boolean splitEnumClasses;
+	private boolean boundariesVisible;
+
+	private JButton okButton;
 
 	private ArrayList<Double> binBoundaries;
 
@@ -144,6 +167,7 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		this.boundariesArea = new JTextArea();
 		this.boundariesArea.setEditable(false);
 		this.boundariesAreaScrollPane = new JScrollPane(this.boundariesArea);
+		this.boundariesAreaScrollPane.getVerticalScrollBar().setUnitIncrement(Statistiek.scrollSpeedUnit);
 		this.boundariesAreaScrollPane.setBorder(BorderFactory
 			.createLoweredBevelBorder());
 		this.boundariesAreaScrollPane.setMaximumSize(new Dimension(120, 140));
@@ -181,6 +205,103 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		this.showCumulativeBox.setActionCommand("showCumulativeBox");
 		this.showCumulativeBox.addActionListener(this.controller);
 
+		// split settings
+		this.splitButton = new JButton(
+			Statistiek.rb.getString("splitoptionsButton"));
+		this.splitButton.setPreferredSize(new Dimension(140, 25));
+		this.splitButton.setFont(Statistiek.font);
+		this.splitButton.setActionCommand("splitButton");
+		this.splitButton.addActionListener(this);
+
+		this.splitsVarLabel = new JLabel(
+			Statistiek.rb.getString("splitvariableLabel"));
+		this.splitsVarLabel.setFont(Statistiek.font);
+
+		this.splitVarBox = new JComboBox();
+		this.splitVarBox.setFont(Statistiek.font);
+		this.splitVarBox.setPreferredSize(new Dimension(100, 25));
+		this.splitVarBox.setMaximumSize(new Dimension(100, 25));
+		this.splitVarBox.setActionCommand("splitVarBox");
+		this.splitVarBox.addActionListener(this.controller);
+
+		this.splitBinsLabel = new JLabel(
+			Statistiek.rb.getString("noClassesLabel"));
+		this.splitBinsLabel.setFont(Statistiek.font);
+
+		Integer[] options1 = new Integer[50];
+		for (int i = 0; i < 50; i++)
+		{
+			options1[i] = i + 1;
+		}
+
+		this.splitBinsBox = new JComboBox(options1);
+		this.splitBinsBox.setFont(Statistiek.font);
+		this.splitBinsBox.setMaximumSize(new Dimension(100, 25));
+		this.splitBinsBox.setPreferredSize(new Dimension(100, 25));
+		this.splitBinsBox.setActionCommand("splitBinsBox");
+		this.splitBinsBox.addActionListener(this.controller);
+
+		this.splitChooseBoundariesButton = new JButton(
+			Statistiek.rb.getString("binsButton"));
+		this.splitChooseBoundariesButton.setFont(Statistiek.font);
+		this.splitChooseBoundariesButton
+			.setPreferredSize(new Dimension(100, 25));
+		this.splitChooseBoundariesButton
+			.setActionCommand("splitChooseBinsButton");
+		this.splitChooseBoundariesButton.addActionListener(this);
+
+		this.separator3 = new JSeparator();
+		this.separator3.setBorder(BorderFactory
+			.createEtchedBorder(EtchedBorder.LOWERED));
+		this.separator3.setMaximumSize(new Dimension(140, 3));
+
+		this.splitMinBoundaryLabel = new JLabel(
+			Statistiek.rb.getString("startvalueLabel"));
+		this.splitMinBoundaryLabel.setFont(Statistiek.font);
+
+		this.splitMinBoundaryField = new JTextField();
+		this.splitMinBoundaryField.setMaximumSize(new Dimension(40, 25));
+		this.splitMinBoundaryField.setMinimumSize(new Dimension(40, 25));
+		this.splitMinBoundaryField.setPreferredSize(new Dimension(40, 25));
+		this.splitMinBoundaryField.setActionCommand("splitMinBoundary");
+		this.splitMinBoundaryField.addActionListener(controller);
+		// this.minBoundaryField.addFocusListener(controller);
+
+		this.splitBinWidthLabel = new JLabel(
+			Statistiek.rb.getString("classwidthLabel"));
+		this.splitBinWidthLabel.setFont(Statistiek.font);
+
+		this.splitBinWidthField = new JTextField();
+		this.splitBinWidthField.setMaximumSize(new Dimension(40, 25));
+		this.splitBinWidthField.setMinimumSize(new Dimension(40, 25));
+		this.splitBinWidthField.setPreferredSize(new Dimension(40, 25));
+		this.splitBinWidthField.setActionCommand("splitBinWidth");
+		this.splitBinWidthField.addActionListener(controller);
+
+		this.splitBoundariesLabel = new JLabel(
+			Statistiek.rb.getString("binsButton"));
+		this.splitBoundariesLabel.setFont(Statistiek.font);
+
+		this.splitBoundariesArea = new JTextArea();
+		this.splitBoundariesArea.setEditable(false);
+		this.splitBoundariesAreaScrollPane = new JScrollPane(
+			this.splitBoundariesArea);
+		this.splitBoundariesAreaScrollPane.getVerticalScrollBar().setUnitIncrement(Statistiek.scrollSpeedUnit);
+		this.splitBoundariesArea.setBorder(BorderFactory
+			.createLoweredBevelBorder());
+		this.splitBoundariesArea.setMaximumSize(new Dimension(120, 140));
+		this.splitBoundariesArea.setMinimumSize(new Dimension(120, 140));
+		this.splitBoundariesArea.setPreferredSize(new Dimension(130, 140));
+
+		this.splitNoObjectsLabel = new JLabel("");
+		this.splitNoObjectsLabel.setFont(Statistiek.font);
+
+		this.splitMinValueLabel = new JLabel("");
+		this.splitMinValueLabel.setFont(Statistiek.font);
+
+		this.splitMaxValueLabel = new JLabel("");
+		this.splitMaxValueLabel.setFont(Statistiek.font);
+
 		// ok-cancel
 		this.okButton = new JButton("OK");
 		this.okButton.addActionListener(this);
@@ -188,10 +309,11 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 
 	private void layoutGuiComponents()
 	{
-		// Variable
-		Box hb1, hb2, hb3, hb4, hb5, hb6, hb7, hb8, hb9, hb10, hb11, hb12, hb13;
-		Box vb1, vb2, vb3, vb4, vb5, vb6, vb7;
+		Box hb1, hb2, hb3, hb4, hb5, hb6, hb7, hb8, hb9, hb10, hb11, hb12, hb13, hb14;
+		Box vb1, vb2, vb3, vb4;
 
+		// Variable
+		
 		hb1 = Box.createHorizontalBox();
 		hb1.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		hb1.add(columnIndexLabel);
@@ -210,6 +332,7 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		vb1.add(Box.createVerticalGlue());
 
 		// Bins
+		
 		hb1 = Box.createHorizontalBox();
 		hb1.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		hb1.add(this.minBoundaryLabel);
@@ -260,7 +383,7 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		vb2.add(Box.createVerticalGlue());
 		
 		// Display
-
+		
 		hb1 = Box.createHorizontalBox();
 		hb1.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		hb1.add(showPercBox);
@@ -283,6 +406,93 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		vb3.add(hb2);
 		vb3.add(hb3);
 		vb3.add(Box.createVerticalGlue());
+		
+		// splitOptions
+		
+		hb1 = Box.createHorizontalBox();
+		hb1.setBorder(BorderFactory.createEmptyBorder(10, 5, 5, 5));
+		hb1.add(splitButton);
+
+		hb2 = Box.createHorizontalBox();
+		hb2.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+		hb2.add(splitsVarLabel);
+
+		hb3 = Box.createHorizontalBox();
+		hb3.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb3.add(splitVarBox);
+
+		hb4 = Box.createHorizontalBox();
+		hb4.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+		hb4.add(splitBinsLabel);
+
+		hb5 = Box.createHorizontalBox();
+		hb5.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb5.add(splitBinsBox);
+
+		hb6 = Box.createHorizontalBox();
+		hb6.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb6.add(separator3);
+
+		hb7 = Box.createHorizontalBox();
+		hb7.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb7.add(splitChooseBoundariesButton);
+
+		hb8 = Box.createHorizontalBox();
+		hb8.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb8.add(this.splitMinBoundaryLabel);
+		hb8.add(Box.createHorizontalStrut(5));
+		hb8.add(Box.createHorizontalGlue());
+		hb8.add(this.splitMinBoundaryField);
+
+		hb9 = Box.createHorizontalBox();
+		hb9.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb9.add(this.splitBinWidthLabel);
+		hb9.add(Box.createHorizontalStrut(5));
+		hb9.add(Box.createHorizontalGlue());
+		hb9.add(this.splitBinWidthField);
+
+		hb10 = Box.createHorizontalBox();
+		hb10.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb10.add(this.splitBoundariesLabel);
+
+		hb11 = Box.createHorizontalBox();
+		hb11.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		hb11.add(this.splitBoundariesAreaScrollPane);
+
+		hb12 = Box.createHorizontalBox();
+		hb12.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+		hb12.add(this.splitNoObjectsLabel);
+		hb12.add(Box.createHorizontalGlue());
+
+		hb13 = Box.createHorizontalBox();
+		hb13.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+		hb13.add(this.splitMinValueLabel);
+		hb13.add(Box.createHorizontalGlue());
+
+		hb14 = Box.createHorizontalBox();
+		hb14.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+		hb14.add(this.splitMaxValueLabel);
+		hb14.add(Box.createHorizontalGlue());
+
+		vb4 = Box.createVerticalBox();
+		vb4.setBorder(BorderFactory.createTitledBorder(border,
+			Statistiek.rb.getString("splitsLabel"), TitledBorder.CENTER,
+			TitledBorder.TOP, Statistiek.font));
+		vb4.add(hb1);
+		vb4.add(hb2);
+		vb4.add(hb3);
+		vb4.add(hb4);
+		vb4.add(hb5);
+		vb4.add(hb6);
+		vb4.add(hb7);
+		vb4.add(hb8);
+		vb4.add(hb9);
+		vb4.add(hb10);
+		vb4.add(hb11);
+		vb4.add(hb12);
+		vb4.add(hb13);
+		vb4.add(hb14);
+		vb4.add(Box.createVerticalGlue());
 
 		Box hb0 = Box.createHorizontalBox();
 		// hb0.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
@@ -292,6 +502,8 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		hb0.add(vb2);
 		hb0.add(Box.createHorizontalStrut(10));
 		hb0.add(vb3);
+		hb0.add(Box.createHorizontalStrut(10));
+		hb0.add(vb4);
 		hb0.add(Box.createHorizontalStrut(10));
 
 		vb0 = Box.createVerticalBox();
@@ -329,6 +541,10 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 
 	public void update()
 	{
+//		System.out.println("FrequencyTableUserOptionsPanel.update(): this.model.columnIndexValid() = "
+//			+ this.model.columnIndexValid() + ", this.model.getSplitOptions().getColumnSplitIndex() = "
+//			+ this.model.getSplitOptions().getColumnSplitIndex());
+		
 		this.columnIndexBox.removeActionListener(this.controller);
 		this.columnIndexBox.removeAllItems();
 
@@ -406,6 +622,111 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		this.showPercBox.setSelected(this.model.isShowPercentage());
 		this.showFreqBox.setSelected(this.model.isShowFreq());
 		this.showCumulativeBox.setSelected(this.model.isShowCumulative());
+		
+		this.splitVarBox.removeActionListener(this.controller);
+		this.splitVarBox.removeAllItems();
+		this.splitVarBox.addItem(Statistiek.rb.getString("chooseItem"));
+		for (int column = 0; column < this.model.getTableModel()
+			.getColumnCount(); column++)
+		{
+			splitVarBox.addItem(this.model.getTableModel()
+				.getColumnName(column));
+		}
+		
+		// check of columnindex valid
+		if (this.model.columnIndexValid())
+		{
+			//System.out.println("FrequencyTableUserOptionsPanel.update(): COLUMN INDEX VALID!");
+			this.splitVarBox.setSelectedIndex(this.model.getSplitOptions()
+				.getColumnSplitIndex() + 1);
+		}
+		else
+		{
+			// set no item selected
+			this.splitVarBox.setSelectedIndex(-1);
+		}
+	
+		this.splitVarBox.addActionListener(this.controller);
+
+		// test syl: combobox met aantal bins nog niet geimplementeerd
+//		this.binsBox.removeActionListener(this.controller);
+//		this.binsBox.setSelectedItem(new Integer(this.model.getNoBins()));
+//		this.binsBox.addActionListener(this.controller);
+		
+		this.splitBinsBox.removeActionListener(this.controller);
+		this.splitBinsBox.setSelectedItem(new Integer(this.model
+			.getSplitOptions().getBinBoundaries().size() - 1));
+		this.splitBinsBox.addActionListener(this.controller);
+		
+		
+		if (this.model.columnIndexValid())
+		{
+    		if (this.model.getSplitOptions().getColumnSplitIndex() > -1)
+    		{
+    			ColumnType cSplitType = this.model.getTableModel().getColumnTypes()
+    				.get(this.model.getSplitOptions().getColumnSplitIndex());
+    			
+//    			System.out.println("FrequencyTableUserOptionsPanel.update(): cSplitType = " +
+//    				cSplitType);
+    			
+    			AllowedTypes splitType = cSplitType.getType();
+    			if (splitType.equals(AllowedTypes.DOUBLE)
+    				|| splitType.equals(AllowedTypes.INTEGER))
+    			{
+    				this.splitMinBoundaryField.setText(Statistiek.df
+    					.format(this.model.getSplitOptions().getBinBoundaries()
+    						.get(0)));
+    				Double d = this.model.getSplitOptions().getBinBoundaries()
+    					.get(1)
+    					- this.model.getSplitOptions().getBinBoundaries().get(0);
+    				this.splitBinWidthField.setText(Statistiek.df.format(d));
+    				StringBuilder sb = new StringBuilder();
+    				for (int i = 0; i < this.model.getSplitOptions()
+    					.getBinBoundaries().size() - 1; i++)
+    				{
+    					sb.append(Statistiek.df.format(this.model.getSplitOptions()
+    						.getBinBoundaries().get(i)));
+    					sb.append(" - ");
+    					sb.append(Statistiek.df.format(this.model.getSplitOptions()
+    						.getBinBoundaries().get(i + 1)));
+    					sb.append("\n");
+    				}
+    				sb.delete(sb.length() - 3, sb.length());
+    				this.splitBoundariesArea.setText(sb.toString());
+    				this.splitNoObjectsLabel.setText(Statistiek.rb
+    					.getString("numberLabel")
+    					+ this.model.getTableModel().getRowCount());
+    				this.splitMinValueLabel.setText(Statistiek.rb
+    					.getString("minLabel")
+    					+ this.model.getTableModel().getColumnMin(
+    						this.model.getSplitOptions().getColumnSplitIndex()));
+    				this.splitMaxValueLabel.setText(Statistiek.rb
+    					.getString("maxLabel")
+    					+ this.model.getTableModel().getColumnMax(
+    						this.model.getSplitOptions().getColumnSplitIndex()));
+    				this.splitBinsBox.getParent().setVisible(true);
+    				this.splitBinsLabel.getParent().setVisible(true);
+    				setSplitEnumClasses(false);
+    			}
+    			else if (splitType.equals(AllowedTypes.ENUM))
+    			{
+    				StringBuilder sb = new StringBuilder();
+    				for (String s : cSplitType.getEnumOptions())
+    				{
+    					sb.append(s);
+    					sb.append("\n");
+    				}
+    				sb.substring(0, sb.length() - 1);
+    				this.splitBoundariesArea.setText(sb.toString());
+    				this.splitBinsBox.getParent().setVisible(false);
+    				this.splitBinsLabel.getParent().setVisible(false);
+    				setSplitEnumClasses(true);
+    			}
+    		}
+		}
+		
+		boolean split = this.model.getSplitOptions().getColumnSplitIndex() > -1;
+		this.setVisibleSplitOptions(split);
 	}
 
 	public double getBinWidth()
@@ -453,6 +774,32 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 		{
 			dialogButton.closeDialog();
 		}
+		else if (e.getSource() == splitButton)
+		{
+			if (splitOptionsVisible)
+			{
+				setVisibleSplitOptions(false);
+				this.splitVarBox.setSelectedIndex(0);
+			}
+			else
+			{
+				setVisibleSplitOptions(true);
+			}
+			resize(vb0);
+		}
+		else if (e.getSource() == splitChooseBoundariesButton)
+		{
+			if (splitBoundariesVisible)
+			{
+				setVisibleSplitBoundaryOptions(false);
+			}
+			else
+			{
+				setVisibleSplitBoundaryOptions(true);
+			}
+
+			resize(vb0);
+		}
 		else if (e.getSource() == dialogButton)
 		{
 			Thread startDraad = new Thread()
@@ -488,4 +835,81 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 	{
 		return this.showCumulativeBox != null && this.showCumulativeBox.isSelected();
 	}
+	
+	private void setVisibleSplitOptions(boolean b)
+	{
+		splitOptionsVisible = b;
+		splitsVarLabel.getParent().setVisible(b);
+		splitVarBox.getParent().setVisible(b);
+		if (!b)
+			splitBinsLabel.getParent().setVisible(b);
+		if (!b)
+			splitBinsBox.getParent().setVisible(b);
+		splitChooseBoundariesButton.getParent().setVisible(b);
+		if (!b)
+		{
+			splitButton.setText(Statistiek.rb.getString("splitoptionsButton"));
+			setVisibleSplitBoundaryOptions(false);
+		}
+		else
+		{
+			splitButton.setText(Statistiek.rb
+				.getString("removeSplitoptionsButton"));
+		}
+	}
+	
+	private void setVisibleSplitBoundaryOptions(boolean b)
+	{
+		splitBoundariesVisible = b;
+		separator3.getParent().setVisible(b);
+		splitMinBoundaryLabel.getParent().setVisible(b && !splitEnumClasses);
+		splitMinBoundaryField.getParent().setVisible(b && !splitEnumClasses);
+		splitBinWidthLabel.getParent().setVisible(b && !splitEnumClasses);
+		splitBinWidthField.getParent().setVisible(b && !splitEnumClasses);
+		splitBoundariesLabel.getParent().setVisible(b);
+		splitBoundariesArea.getParent().setVisible(b);
+		splitBoundariesAreaScrollPane.getParent().setVisible(b);
+		splitNoObjectsLabel.getParent().setVisible(b && !splitEnumClasses);
+		splitMinValueLabel.getParent().setVisible(b && !splitEnumClasses);
+		splitMaxValueLabel.getParent().setVisible(b && !splitEnumClasses);
+		if (!b)
+		{
+			splitChooseBoundariesButton.setText(Statistiek.rb
+				.getString("binsButton"));
+		}
+		else
+		{
+			splitChooseBoundariesButton.setText(Statistiek.rb
+				.getString("hideButtonLabel"));
+		}
+	}
+
+	private void setSplitEnumClasses(boolean b)
+	{
+		splitEnumClasses = b;
+		// if(b) {
+		splitMinBoundaryLabel.getParent().setVisible(
+			splitBoundariesVisible && !b);
+		splitMinBoundaryField.getParent().setVisible(
+			splitBoundariesVisible && !b);
+		splitBinWidthLabel.getParent().setVisible(splitBoundariesVisible && !b);
+		splitBinWidthField.getParent().setVisible(splitBoundariesVisible && !b);
+		splitNoObjectsLabel.getParent()
+			.setVisible(splitBoundariesVisible && !b);
+		splitMinValueLabel.getParent().setVisible(splitBoundariesVisible && !b);
+		splitMaxValueLabel.getParent().setVisible(splitBoundariesVisible && !b);
+		// }
+		resize(vb0);
+	}
+
+	public int getSplitVarBoxSelectedIndex()
+	{
+		return this.splitVarBox.getSelectedIndex();
+	}
+
+	public int getSplitBinsBoxSelectedInt()
+	{
+		return ((Integer) this.splitBinsBox.getSelectedItem()).intValue();
+	}
+
 }
