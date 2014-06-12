6eeb3d684dce95c65a803e48464e417cfd570631
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-06-12 16:03:46 +0000

    Bin boundaries slimmer bepalen na het zetten van columnIndex
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@7845 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/histogram/HistogramModel.java
+++ src/fi/statistiek/histogram/HistogramModel.java
@@ -281,6 +281,15 @@ public class HistogramModel extends Observable implements TableModelListener,
     						this.tableModel.getColumnMin(this.columnIndex),
     						this.tableModel.getColumnMax(this.columnIndex),
     						this.noBins);
+    				
+    				// test syl: kan mooier, maar het werkt wel: opnieuw berekenen 
+    				// met de berekende binboundaries, omdat er mogelijk minder bins nodig zijn
+    				// TODO appropriateBoundaries(min, max) implementeren die binwidth en het aantal klassen bepaalt 
+    				this.binBoundaries = Statistiek.appropriateBoundariesFromBinSettings(this.tableModel.getColumnMin(this.columnIndex),
+    					this.tableModel.getColumnMax(this.columnIndex), 
+    					this.binBoundaries.get(1) - this.binBoundaries.get(0), this.binBoundaries.get(0));
+    				
+    				this.noBins = this.binBoundaries.size() - 1;
 				}
 
 				// set bin label positioning
