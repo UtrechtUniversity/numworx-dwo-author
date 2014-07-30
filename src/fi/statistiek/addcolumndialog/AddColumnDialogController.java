2171f4d1c00af58a954ec410c8ccac8c3a81859f
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-07-30 11:25:02 +0000

    Enum uitgebreid met removeAllElements; automatisch vullen lijst
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@8064 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/addcolumndialog/AddColumnDialogController.java
+++ src/fi/statistiek/addcolumndialog/AddColumnDialogController.java
@@ -1,4 +1,5 @@
 package fi.statistiek.addcolumndialog;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -7,83 +8,148 @@ import java.awt.event.FocusListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+import fi.statistiek.types.AllowedTypes;
+
 /**
  * Controller for add column dialog
- * @author Manu Drijvers
- *
+ * 
+ * @author Manu Drijvers, Sylvia van Borkulo
+ * 
  */
-public class AddColumnDialogController implements ActionListener, DocumentListener, FocusListener{
+public class AddColumnDialogController implements ActionListener,
+	DocumentListener, FocusListener
+{
 	private AddColumnDialogModel model;
 	private AddColumnDialogView view;
-	
+
 	/**
 	 * Constructor
-	 * @param model MVC Model
-	 * @param view MVC View
+	 * 
+	 * @param model
+	 *            MVC Model
+	 * @param view
+	 *            MVC View
 	 */
-	public AddColumnDialogController(AddColumnDialogModel model, AddColumnDialogView view) {
+	public AddColumnDialogController(AddColumnDialogModel model,
+		AddColumnDialogView view)
+	{
 		this.model = model;
 		this.view = view;
 		this.view.addActionListeners(this);
 		this.view.addFocusListeners(this);
 	}
-	
+
 	/**
 	 * ActionListener implementation
 	 */
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent arg0)
+	{
 		String actionCommand = arg0.getActionCommand();
-		if(actionCommand.equals("addEnumElementField")) {
-			String newElement = this.view.getEnumOption();
-			this.model.addEnumOption(newElement);
-			this.view.clearAddEnumElementField();
+		if (actionCommand.equals("addEnumElementField"))
+		{
+			if (this.wasEnum())
+			{
+				String newElement = this.view.getEnumOption();
+				this.model.addEnumOption(newElement);
+				this.view.clearAddEnumElementField();
+			}
+			else
+			{
+				this.view.addStringOption(this.view.getEnumOption());
+				this.view.update(null, null);
+			}
 		}
-		else if(actionCommand.equals("removeSelectedElement")) {
-			this.model.removeEnumOption(this.view.getSelectedEnumOptionIndex());
+		else if (actionCommand.equals("removeSelectedElement"))
+		{
+			if (this.wasEnum())
+			{
+				this.model.removeEnumOption(this.view.getSelectedOptionInListIndex());
+			}
+			else
+			{
+				this.view.removeStringOption(this.view.getSelectedOptionInListIndex());
+				this.view.update(null, null);
+			}
 		}
-		else if(actionCommand.equals("typeBox")) {
+		else if (actionCommand.equals("removeAllElements"))
+		{
+			if (this.wasEnum())
+			{
+				this.model.removeAllEnumOption();
+			}
+			else
+			{
+				this.view.removeAllStringOptions();
+				this.view.update(null, null);
+			}
+		}
+		else if (actionCommand.equals("typeBox"))
+		{
 			this.model.setType(this.view.getSelectedType());
 		}
-		else if(actionCommand.equals("doneButton")) {
+		else if (actionCommand.equals("doneButton"))
+		{
+			if (!this.wasEnum())
+			{
+				this.view.updateEnumOptions();
+			}
+
 			this.model.setDonePressed(true);
 			this.view.setVisible(false);
 		}
-		else if(actionCommand.equals("nameField")) {
+		else if (actionCommand.equals("nameField"))
+		{
 			this.model.setName(this.view.getCurrentName());
 		}
 	}
 	
+	/**
+	 * Return whether the column originally was of type enumeration.
+	 * @return
+	 */
+	private boolean wasEnum()
+	{
+		return this.view.getOriginalColumnType().equals(AllowedTypes.ENUM);
+	}
+
 	/**
 	 * DocumentListener implementation
 	 */
-	public void changedUpdate(DocumentEvent arg0) {
+	public void changedUpdate(DocumentEvent arg0)
+	{
 		this.model.setUitleg(this.view.getUitleg());
-		
-	}
-	
-	public void insertUpdate(DocumentEvent arg0) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void removeUpdate(DocumentEvent arg0) {
+	public void insertUpdate(DocumentEvent arg0)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void focusGained(FocusEvent arg0) {
+	public void removeUpdate(DocumentEvent arg0)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
+	public void focusGained(FocusEvent arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
 	/**
 	 * FocusListener implementation
 	 */
-	public void focusLost(FocusEvent arg0) {
-		if(arg0.getSource() == this.view.getNameField()) {
+	public void focusLost(FocusEvent arg0)
+	{
+		if (arg0.getSource() == this.view.getNameField())
+		{
 			this.model.setName(this.view.getCurrentName());
 		}
-		else if(arg0.getSource() == this.view.getUitlegArea()) {
+		else if (arg0.getSource() == this.view.getUitlegArea())
+		{
 			this.model.setUitleg(this.view.getUitleg());
 		}
 	}
