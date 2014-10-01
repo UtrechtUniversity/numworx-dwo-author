221adf19fcf36838a8df86240c49460e1ff6e4fa
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-10-01 10:08:08 +0000

    move project to trunk
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/BalansFruitApplet/trunk@8309 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export.bat
@@ -0,0 +1,4 @@
+%windir%\system32\xcopy.exe ..\src\fi\balansfruitapplet N:\home\java\src.final\fi\balansfruitapplet /E /I /Y
+%windir%\system32\xcopy.exe ..\output\classes\fi\balansfruitapplet N:\home\www-dev\InfoGroups\javaclasses\fi\balansfruitapplet /E /I /Y
+copy ..\output\jar\balansfruitapplet.jar N:\home\www-dev\InfoGroups\javaclasses\jars\balansfruitapplet.jar
+copy ..\output\scorm\course\balansfruitapplet.zip N:\home\www-dev\InfoGroups\javaclasses\scorms\balansfruitapplet.zip
