6e605686a419ad39b4873c43012bb7743dc12c8f
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-11-13 13:06:54 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@8531 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/test/fi/statistiek/StatistiekTest.java
@@ -0,0 +1,414 @@
+package test.fi.statistiek;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.omg.PortableInterceptor.SUCCESSFUL;
+
+import fi.statistiek.Statistiek;
+import static org.junit.Assert.*;
+
+public class StatistiekTest
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testAppropriateBoundaries()
+	{
+		double min = 0;
+		double max = 10;
+		int noBins = 5;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(0.0, 3.0, 6.0, 9.0, 12.0, 15.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesOneBin40_70()
+	{
+		double min = 40;
+		double max = 70;
+		int noBins = 1;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(40.0, 80.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins40_70()
+	{
+		double min = 40;
+		double max = 70;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(40.0, 60.00, 80.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins156_171()
+	{
+		double min = 156;
+		double max = 171;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(156.0, 166.00, 176.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins90_93()
+	{
+		double min = 90;
+		double max = 93;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(90.0, 92.00, 94.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins40_90()
+	{
+		double min = 40;
+		double max = 90;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(40.0, 70.00, 100.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins156_200()
+	{
+		double min = 156;
+		double max = 200;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(156.0, 181.0, 206.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins6_8d9()
+	{
+		double min = 6;
+		double max = 8.9;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(6.0, 8.0, 10.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins1_21()
+	{
+		double min = 1;
+		double max = 21;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(1.0, 16.0, 31.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins5_10()
+	{
+		double min = 5;
+		double max = 10;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(5.0, 8.0, 11.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins1976_2014()
+	{
+		double min = 1976;
+		double max = 2014;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(1976.0, 1996.0, 2016.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins1_12()
+	{
+		double min = 1;
+		double max = 12;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(1.0, 11.0, 21.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins0_360()
+	{
+		double min = 0;
+		double max = 360;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(0.0, 200.0, 400.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins0d5_18()
+	{
+		double min = 0.5;
+		double max = 18;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(0.0, 10.0, 20.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBins2d6_41d7()
+	{
+		double min = 2.6;
+		double max = 41.7;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(0.0, 25.0, 50.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBinsm12d2_27d2()
+	{
+		double min = -12.2; // m12d2: m = min, d = decimal separator
+		double max = 27.2;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(-40.0, 0.0, 40.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBinsm17d1_20d9()
+	{
+		double min = -17.1; // m17d1: m = min, d = decimal separator
+		double max = 20.9;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(-40.0, 0.0, 40.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTwoBinsm9d7_35()
+	{
+		double min = -9.7; // m9d7: m = min, d = decimal separator
+		double max = 35;
+		int noBins = 2;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(-50.0, 0.0, 50.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesTenBins40_90()
+	{
+		double min = 40;
+		double max = 90;
+		int noBins = 10;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(
+			40.0, 46.0, 52.0, 58.0, 64.0, 70.0, 76.0, 82.0, 88.0, 94.0, 100.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesSixBins2d48_2d57()
+	{
+		double min = 2.48;
+		double max = 2.57;
+		int noBins = 6;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(
+			2.48, 2.50, 2.52, 2.54, 2.56, 2.58, 2.60));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesSixBins248_257()
+	{
+		double min = 248;
+		double max = 257;
+		int noBins = 6;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(
+			248.0, 250.0, 252.0, 254.0, 256.0, 258.0, 260.0));
+		
+		ArrayList<Double> actual = Statistiek.appropriateBoundaries(min, max, noBins);
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettings()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = 3;
+		double minBoundary = 0;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(0.0, 3.0, 6.0, 9.0, 12.0));
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals("Expected " + expected.toArray()
+			+ ", actual " + actual.toArray(), actual, expected);
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettingsBinWidthTooSmall()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = 10/51;
+		double minBoundary = 0;
+		
+		ArrayList<Double> expected = null;
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettingsBinWidthSmallButCorrect()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = (double) 10/50;
+		double minBoundary = 0;
+		
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(
+			0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 
+			2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 
+			4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 
+			6.0, 6.2, 6.4, 6.6, 6.8, 7.0, 7.2, 7.4, 7.6, 7.8, 
+			8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 9.2, 9.4, 9.6, 9.8, 
+			10.0, 10.2, 10.4, 10.6, 10.8, 11.0));
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals("Expected " + expected.toArray()
+			+ ", actual " + actual.toArray(), actual, expected);
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettingsBinWidthTooLarge()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = 25;
+		double minBoundary = 0;
+		
+		ArrayList<Double> expected = null;
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettingsBinWidthNegative()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = -1;
+		double minBoundary = 0;
+		
+		ArrayList<Double> expected = null;
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettingsBinWidthZero()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = 0;
+		double minBoundary = 0;
+		
+		ArrayList<Double> expected = null;
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettingsMinBoundaryTooSmall()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = 1;
+		double minBoundary = -6;
+		
+		ArrayList<Double> expected = null;
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAppropriateBoundariesFromBinSettingsMinBoundaryTooLarge()
+	{
+		double min = 0;
+		double max = 10;
+		double binWidth = 1;
+		double minBoundary = 1;
+		
+		ArrayList<Double> expected = null;
+		ArrayList<Double> actual = Statistiek.appropriateBoundariesFromBinSettings(min, max, binWidth, minBoundary);
+		assertEquals(expected, actual);
+	}
+}
---
+++ src/test/fi/statistiek/descriptives/DescriptivesModelTest.java
@@ -0,0 +1,480 @@
+package test.fi.statistiek.descriptives;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import test.fi.statistiek.StatTableModelTest;
+import fi.statistiek.SplitOptions;
+import fi.statistiek.StatTableModel;
+import fi.statistiek.Statistiek;
+import fi.statistiek.descriptives.DescriptivesController;
+import fi.statistiek.descriptives.DescriptivesModel;
+import fi.statistiek.descriptives.DescriptivesView;
+import fi.statistiek.histogram.HistogramModel.FrequencyTuple;
+
+public class DescriptivesModelTest
+{
+	private DescriptivesView view;
+	private DescriptivesModel model;
+	private DescriptivesController controller;
+	private static double delta = 1e-15; // error in comparing doubles 
+
+	@Before
+	public void setUp() throws Exception
+	{
+		this.setUpTestCase1();
+	}
+
+	/**
+	 * Set up test case 1:
+	 * 		10 cases
+	 * 		columnIndex = 1 (gewicht)
+	 * 		columnSplitIndex = 5 (geslacht)
+	 * 		cases 0-2 selected
+	 */
+	private void setUpTestCase1()
+	{
+		Statistiek statistiek = new Statistiek();
+
+		// use the test data
+		StatTableModelTest statTableModelTest = new StatTableModelTest();
+		StatTableModel statTableModel = 
+			statTableModelTest.getStatTableModelWithSelection();
+		
+		this.controller = new DescriptivesController(
+			statTableModel, "Kengetallen", 1);
+		this.model = new DescriptivesModel(
+			statTableModel, "Kengetallen");
+		view = new DescriptivesView(this.model, this.controller);
+		// columnIndex = -1!
+		this.model.setColumnIndex(1); // gewicht
+		this.setSplitGeslacht();
+		this.view.update(null, null);		
+	}
+
+	/**
+	 * Set up test case 2:
+	 * 		12 cases
+	 * 		columnIndex = 1 (gewicht)
+	 * 		columnSplitIndex = 5 (geslacht)
+	 * 		cases 0-2 and 10 selected
+	 */
+	private void setUpTestCase2()
+	{
+		Statistiek statistiek = new Statistiek();
+
+		// use the test data
+		StatTableModelTest statTableModelTest = new StatTableModelTest();
+		StatTableModel statTableModel = 
+			statTableModelTest.getStatTableModelWithSelectionAndWildcards();
+		
+		this.controller = new DescriptivesController(
+			statTableModel, "Kengetallen", 1);
+		this.model = new DescriptivesModel(
+			statTableModel, "Kengetallen");
+		view = new DescriptivesView(this.model, this.controller);
+		// columnIndex = -1!
+		this.model.setColumnIndex(1); // gewicht
+		this.setSplitGeslacht();
+		this.view.update(null, null);		
+	}
+
+	private void setSplitGeslacht()
+	{
+		this.model.setColumnSplitIndex(5); // geslacht
+		this.model.setSplitOptions(this.model.getSplitOptions());
+		this.controller.setSplit(5);
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testGetBinBoundaries()
+	{
+		double min = this.model.getTableModel().getColumnMin(1); // columnIndex = 1 (gewicht)
+		double max = this.model.getTableModel().getColumnMax(1); // columnIndex = 1 (gewicht)
+		ArrayList<Double> expected = new ArrayList(Arrays.asList(min, max + 10)); // verschil in tientallen, dus 1 tiental meer voor bovengrens
+		ArrayList<Double> actual = this.model.getBinBoundaries();
+		assertArrayEquals("", expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void testGetViewName()
+	{
+		String expected = "Kengetallen";
+		String actual = this.model.getViewName();
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnIndex()
+	{
+		int expected = 1;
+		int actual = this.model.getColumnIndex();
+		
+		assertEquals("", expected, actual, delta);
+	}
+
+	@Test
+	public void testNumberClassFrequency()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnumClassFrequencyNull()
+	{
+		FrequencyTuple[][] expected = null; // columnIndex = 1 (gewicht), not enum
+		FrequencyTuple[][] actual = this.model.enumClassFrequency();
+		
+		assertArrayEquals("", expected, actual);
+	}
+
+	@Test
+	public void testEnumClassFrequencyEnum()
+	{
+		// set enum column index
+		this.model.setColumnIndex(4); // columnIndex = 4 (profiel), enum
+		
+		FrequencyTuple[][] expected = this.getExpectedEnumClassFrequencyEnum(); 
+		
+		FrequencyTuple[][] actual = this.model.enumClassFrequency();
+		
+		int numberOfColumnClasses = 4; // columnIndex = 4 (profiel)
+		int numberOfSplitClasses = 2; // split = geslacht
+		for (int i = 0; i < numberOfSplitClasses; i++)
+		{
+			for (int j = 0; j < numberOfColumnClasses; j++)
+			{
+				assertEquals("", expected[i][j].label, actual[i][j].label);
+				assertEquals("", expected[i][j].frequency, actual[i][j].frequency, delta);
+				assertEquals("", expected[i][j].selectionFrequency, actual[i][j].selectionFrequency, delta);
+			}
+		}
+		
+		// reset
+		this.model.setColumnIndex(1); // columnIndex = 1 (gewicht)
+	}
+
+	private FrequencyTuple[][] getExpectedEnumClassFrequencyEnum()
+	{
+		int numberOfColumnClasses = 4; // columnIndex = 4 (profiel)
+		int numberOfSplitClasses = 2; // split = geslacht
+
+		FrequencyTuple[][] expected = new FrequencyTuple[numberOfSplitClasses][]; // split = geslacht
+		
+		String[] labels = {"NG", "NT", "EM", "CM"}; // non-alphabetic order: "NG", "NT", "EM", "CM"
+		// was CM, EM, NG, NT
+		int[][] frequencies = {{3, 0, 0, 0}, {3, 1, 2, 1}}; // m, v
+		int[][] freqSelection = {{0, 0, 0, 0}, {1, 1, 0, 1}}; // m, v
+		
+		for (int i = 0; i < numberOfSplitClasses; i++)
+		{
+			expected[i] = new FrequencyTuple[numberOfColumnClasses];
+			for (int j = 0; j < numberOfColumnClasses; j++)
+			{
+				expected[i][j] = new FrequencyTuple(labels[j], frequencies[i][j],
+					freqSelection[i][j]);
+			}
+		}
+		
+		return expected;
+	}
+
+	@Test
+	public void testEnumClassFrequencyString()
+	{
+		// set enum column index
+		this.model.setColumnIndex(0); // columnIndex = 0 (naam), string
+		
+		FrequencyTuple[][] expected = this.getExpectedEnumClassFrequencyString(); 
+		
+		FrequencyTuple[][] actual = this.model.enumClassFrequency();
+		
+		int numberOfColumnClasses = 10; // columnIndex = 0 (naam)
+		int numberOfSplitClasses = 2; // split = geslacht
+		for (int i = 0; i < numberOfSplitClasses; i++)
+		{
+			for (int j = 0; j < numberOfColumnClasses; j++)
+			{
+//				System.out.println("(" + i + "," + j + ")");
+				assertEquals("", expected[i][j].label, actual[i][j].label);
+				assertEquals("", expected[i][j].frequency, actual[i][j].frequency, delta);
+				assertEquals("", expected[i][j].selectionFrequency, actual[i][j].selectionFrequency, delta);
+			}
+		}
+		
+		// reset
+		this.model.setColumnIndex(1); // columnIndex = 1 (gewicht)
+	}
+
+	private FrequencyTuple[][] getExpectedEnumClassFrequencyString()
+	{
+		int numberOfColumnClasses = 10; // columnIndex = 0 (naam)
+		int numberOfSplitClasses = 2; // split = geslacht
+
+		FrequencyTuple[][] expected = new FrequencyTuple[numberOfSplitClasses][];
+		
+		String[] labels = {"Ada", "Al", "Mariozee", "Mieke", "Peter", "Sietske", 
+			"Susanne", "Sylvia", "Thea", "Wim"}; // alphabetic order
+		int[][] frequencies = {{0, 1, 0, 0, 1, 0, 0, 0, 0, 1}, {1, 0, 1, 1, 0, 1, 1, 1, 1, 0}}; // m, v
+		int[][] freqSelection = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 1, 0, 1, 0, 0}}; // m, v
+		
+		for (int i = 0; i < numberOfSplitClasses; i++)
+		{
+			expected[i] = new FrequencyTuple[numberOfColumnClasses];
+			for (int j = 0; j < numberOfColumnClasses; j++)
+			{
+				expected[i][j] = new FrequencyTuple(labels[j], frequencies[i][j],
+					freqSelection[i][j]);
+			}
+		}
+		
+		return expected;
+	}
+
+	@Test
+	public void testGetSplitOptions()
+	{
+		SplitOptions expected = new SplitOptions();
+		expected.setColumnSplitIndex(5); // geslacht
+		
+		SplitOptions actual = this.model.getSplitOptions();
+		
+		assertArrayEquals("", 
+			expected.getBinBoundaries().toArray(), 
+			actual.getBinBoundaries().toArray());
+		assertEquals("", 
+			expected.getColumnSplitIndex(), 
+			actual.getColumnSplitIndex());
+		
+		int numberOfSplitClasses = 2;
+		for (int splitClass = 0; splitClass < numberOfSplitClasses; splitClass++)
+		{
+			assertEquals("", 
+				expected.getSplitClassLabel(splitClass, this.model.getTableModel()), 
+				actual.getSplitClassLabel(splitClass, this.model.getTableModel()));
+		}
+	}
+
+	@Test
+	public void testGetColumnSplitIndex()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColumnModeSplit0NoSelection()
+	{
+		String expected = Statistiek.rb.getString("notAvailable");
+		String actual = this.model.getColumnMode(
+			1, 0, false); // columnIndex = 1 (gewicht), split 0 (m), no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnModeSplit1NoSelection()
+	{
+		String expected = "55";
+		String actual = this.model.getColumnMode(
+			1, 1, false); // columnIndex = 1 (gewicht), split 1 (v), no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnModeSplit0Selection()
+	{
+		String expected = Statistiek.rb.getString("notAvailable");
+		String actual = this.model.getColumnMode(
+			1, 0, true); // columnIndex = 1 (gewicht), split 0 (m), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnModeSplit1Selection()
+	{
+		String expected = Statistiek.rb.getString("notAvailable");
+		String actual = this.model.getColumnMode(
+			1, 1, true); // columnIndex = 1 (gewicht), split 1 (v), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMaxSplit0NoSelection()
+	{
+		String expected = "70";
+		String actual = this.model.getColumnMax(
+			1, 0, false); // columnIndex = 1 (gewicht), split 0 (m), no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMaxSplit1NoSelection()
+	{
+		String expected = "56";
+		String actual = this.model.getColumnMax(
+			1, 1, false); // columnIndex = 1 (gewicht), split 1 (v), no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMaxSplit0Selection()
+	{
+		String expected = Statistiek.rb.getString("notAvailable");
+		String actual = this.model.getColumnMax(
+			1, 0, true); // columnIndex = 1 (gewicht), split 0 (m), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMaxSplit1Selection()
+	{
+		String expected = "55";
+		String actual = this.model.getColumnMax(
+			1, 1, true); // columnIndex = 1 (gewicht), split 1 (v), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMinSplit0NoSelection()
+	{
+		String expected = "51";
+		String actual = this.model.getColumnMin(
+			1, 0, false); // columnIndex = 1 (gewicht), split 0 (m), no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMinSplit1NoSelection()
+	{
+		String expected = "40";
+		String actual = this.model.getColumnMin(
+			1, 1, false); // columnIndex = 1 (gewicht), split 1 (v), no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMinSplit0Selection()
+	{
+		String expected = Statistiek.rb.getString("notAvailable");
+		String actual = this.model.getColumnMin(
+			1, 0, true); // columnIndex = 1 (gewicht), split 0 (m), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMinSplit1Selection()
+	{
+		String expected = "40";
+		String actual = this.model.getColumnMin(
+			1, 1, true); // columnIndex = 1 (gewicht), split 1 (v), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMeanSplit0NoSelection()
+	{
+		String expected = "59";
+		String actual = this.model.getColumnMean(
+			1, 0, false); // columnIndex = 1 (gewicht), split 0 (m), no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMeanSplit1NoSelection()
+	{
+		double expected = 51.86;
+		double actual = Statistiek.round(Double.parseDouble(this.model.getColumnMean(
+			1, 1, false)), 2); // columnIndex = 1 (gewicht), split 1 (v), no selection
+		
+		assertEquals("", expected, actual, delta);
+	}
+
+	@Test
+	public void testGetColumnMeanSplit0Selection()
+	{
+		String expected = Statistiek.rb.getString("notAvailable");
+		String actual = this.model.getColumnMean(
+			1, 0, true); // columnIndex = 1 (gewicht), split 0 (m), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMeanSplit1Selection()
+	{
+		String expected = "49";
+		String actual = this.model.getColumnMean(
+			1, 1, true); // columnIndex = 1 (gewicht), split 1 (v), selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnSDSplit0NoSelection()
+	{
+		Double expected = 8.04;
+		Double actual = Statistiek.round(Double.parseDouble(this.model.getColumnSD(
+			1, 0, false)), 2); // columnIndex = 1 (gewicht), split 0, no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnSDSplit1NoSelection()
+	{
+		Double expected = 5.11;
+		Double actual = Statistiek.round(Double.parseDouble(this.model.getColumnSD(
+			1, 1, false)), 2); // columnIndex = 1 (gewicht), split 1, no selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnSDSplit0Selection()
+	{
+		String expected = Statistiek.rb.getString("notAvailable");
+		String actual = this.model.getColumnSD(1, 0, true); // columnIndex = 1 (gewicht), split 0, selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnSDSplit1Selection()
+	{
+		Double expected = 6.48;
+		Double actual = Statistiek.round(Double.parseDouble(this.model.getColumnSD(
+			1, 1, true)), 2); // columnIndex = 1 (gewicht), split 1, selection
+		
+		assertEquals("", expected, actual);
+	}
+
+	@Test
+	public void testGetColumnMedian()
+	{
+		fail("Not yet implemented");
+	}
+
+}
