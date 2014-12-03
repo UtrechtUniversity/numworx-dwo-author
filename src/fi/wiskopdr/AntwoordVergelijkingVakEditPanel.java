a61fac5638bd7346813fe9dcecae9971e0407bd6
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-12-03 14:24:38 +0000

    popup aanpasbaar vinkje hoger
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@8665 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/wiskopdr/AntwoordVergelijkingVakEditPanel.java
+++ src/fi/wiskopdr/AntwoordVergelijkingVakEditPanel.java
@@ -230,7 +230,7 @@ public class AntwoordVergelijkingVakEditPanel extends JLayeredPane implements In
         feedbackEditor.setBackground(new Color(255,255,200));
         add(feedbackEditor,0);
         
-        feedbackSizeCB = makeCheckBox(80,352,180,20,WiskOpdr.rb.getString("feedbackSizeCBLabel"),false,true);
+        feedbackSizeCB = makeCheckBox(80,330,180,20,WiskOpdr.rb.getString("feedbackSizeCBLabel"),false,true);
 		        
         vormEditor = new FormuleEditor(true);
         vormEditor.setResizable(true);
