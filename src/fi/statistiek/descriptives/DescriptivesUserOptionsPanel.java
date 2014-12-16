179fdfde68dbfce2525efcaa0e6cd06b51c73083
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-12-16 09:42:23 +0000

    scrollbare klassen-tekstvak; verbetering tonen decimalen klassenbreedte
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@8734 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/descriptives/DescriptivesUserOptionsPanel.java
+++ src/fi/statistiek/descriptives/DescriptivesUserOptionsPanel.java
@@ -213,9 +213,9 @@ public class DescriptivesUserOptionsPanel extends JPanel implements
 			this.splitBoundariesArea);
 		this.splitBoundariesArea.setBorder(BorderFactory
 			.createLoweredBevelBorder());
-		this.splitBoundariesArea.setMaximumSize(new Dimension(120, 140));
-		this.splitBoundariesArea.setMinimumSize(new Dimension(120, 140));
-		this.splitBoundariesArea.setPreferredSize(new Dimension(130, 140));
+		this.splitBoundariesAreaScrollPane.setMaximumSize(new Dimension(120, 140));
+		this.splitBoundariesAreaScrollPane.setMinimumSize(new Dimension(120, 140));
+		this.splitBoundariesAreaScrollPane.setPreferredSize(new Dimension(130, 140));
 
 		this.splitNoObjectsLabel = new JLabel("");
 		this.splitNoObjectsLabel.setFont(Statistiek.font);
@@ -469,22 +469,18 @@ public class DescriptivesUserOptionsPanel extends JPanel implements
     			if (splitType.equals(AllowedTypes.DOUBLE)
     				|| splitType.equals(AllowedTypes.INTEGER))
     			{
-    				this.splitMinBoundaryField.setText(Statistiek.df
-    					.format(this.model.getSplitOptions().getBinBoundaries()
-    						.get(0)));
-    				Double d = this.model.getSplitOptions().getBinBoundaries()
-    					.get(1)
-    					- this.model.getSplitOptions().getBinBoundaries().get(0);
-    				this.splitBinWidthField.setText(Statistiek.df.format(d));
+    				this.splitMinBoundaryField.setText(this.model.getSplitOptions().getBinBoundaries()
+    						.get(0).toString());
+    				this.splitBinWidthField.setText(Statistiek.getFormattedBinWidth(this.model.getSplitOptions().getBinBoundaries()));
     				StringBuilder sb = new StringBuilder();
     				for (int i = 0; i < this.model.getSplitOptions()
     					.getBinBoundaries().size() - 1; i++)
     				{
-    					sb.append(Statistiek.df.format(this.model.getSplitOptions()
-    						.getBinBoundaries().get(i)));
+    					sb.append(this.model.getSplitOptions()
+    						.getBinBoundaries().get(i).toString());
     					sb.append(" - ");
-    					sb.append(Statistiek.df.format(this.model.getSplitOptions()
-    						.getBinBoundaries().get(i + 1)));
+    					sb.append(this.model.getSplitOptions()
+    						.getBinBoundaries().get(i + 1).toString());
     					sb.append("\n");
     				}
     				this.splitBoundariesArea.setText(sb.toString());
@@ -617,6 +613,14 @@ public class DescriptivesUserOptionsPanel extends JPanel implements
 	public void setSplitBinWidth(double d)
 	{
 		this.splitBinWidthField.setText(String.valueOf(d));
+	}
+	
+	/**
+	 *Set the split bin width based on the model's split bin boundaries. 
+	 */
+	public void setSplitBinWidth()
+	{
+		this.splitBinWidthField.setText(Statistiek.getFormattedBinWidth(this.model.getSplitOptions().getBinBoundaries()));
 	}
 	
 	public int getSplitBinsBoxSelectedInt()
