c19cc6ad4a8d3b9b601a60e818f1f626c4d354b0
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-12-16 09:38:44 +0000

    verbetering tonen decimalen klassenbreedte
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@8732 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/boxplot/BoxplotController.java
+++ src/fi/statistiek/boxplot/BoxplotController.java
@@ -184,7 +184,7 @@ public class BoxplotController implements StatistiekView, ActionListener
 		{
 			// reset to old values
 			ArrayList<Double> oldBoundaries = this.model.getSplitOptions().getBinBoundaries(); 
-			this.view.setSplitBinWidth(oldBoundaries.get(1) - oldBoundaries.get(0));
+			this.view.setSplitBinWidth();
 			this.view.setSplitMinBoundary(oldBoundaries.get(0));
 		}
 	}
