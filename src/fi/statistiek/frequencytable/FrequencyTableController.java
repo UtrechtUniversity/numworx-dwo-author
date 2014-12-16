fd29dc3dc851ec05048d267fd362dc6d33685019
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-12-16 09:45:08 +0000

    verbetering tonen decimalen (split) klassenbreedte
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@8739 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/frequencytable/FrequencyTableController.java
+++ src/fi/statistiek/frequencytable/FrequencyTableController.java
@@ -148,7 +148,7 @@ public class FrequencyTableController implements StatistiekView,
 		{
 			// reset to old values
 			ArrayList<Double> oldBoundaries = this.model.getSplitOptions().getBinBoundaries(); 
-			this.view.setSplitBinWidth(oldBoundaries.get(1) - oldBoundaries.get(0));
+			this.view.setSplitBinWidth();
 			this.view.setSplitMinBoundary(oldBoundaries.get(0));
 		}
 	}
@@ -301,7 +301,7 @@ public class FrequencyTableController implements StatistiekView,
 		{
 			// reset to old values
 			ArrayList<Double> oldBoundaries = this.model.getBinBoundaries(); 
-			this.view.setBinWidth(oldBoundaries.get(1) - oldBoundaries.get(0));
+			this.view.setBinWidth();
 			this.view.setMinBoundary(oldBoundaries.get(0));
 		}
 	}
