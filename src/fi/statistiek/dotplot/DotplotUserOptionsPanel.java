683d053607619766ffe19c60f68e4c0753d6cc87
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-03-04 09:16:21 +0000

    decimale komma ipv punt voor nl
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@9107 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/dotplot/DotplotUserOptionsPanel.java
+++ src/fi/statistiek/dotplot/DotplotUserOptionsPanel.java
@@ -759,32 +759,31 @@ public class DotplotUserOptionsPanel extends JPanel implements ActionListener
 			if (splitType.equals(AllowedTypes.DOUBLE)
 				|| splitType.equals(AllowedTypes.INTEGER))
 			{
-				this.splitMinBoundaryField.setText(this.model.getSplitOptions().getBinBoundaries()
-						.get(0).toString());
+				this.splitMinBoundaryField.setText(
+					Statistiek.getStringValue(this.model.getSplitOptions().getBinBoundaries().get(0)));
 				this.splitBinWidthField.setText(Statistiek.getFormattedBinWidth(this.model.getSplitBinBoundaries()));
 				StringBuilder sb = new StringBuilder();
 				for (int i = 0; i < this.model.getSplitOptions()
 					.getBinBoundaries().size() - 1; i++)
 				{
-					sb.append(this.model.getSplitOptions()
-						.getBinBoundaries().get(i).toString());
-					sb.append(" - ");
-					sb.append(this.model.getSplitOptions()
-						.getBinBoundaries().get(i + 1).toString());
+					sb.append(
+						Statistiek.getStringValue(this.model.getSplitOptions().getBinBoundaries().get(i)));
+					sb.append(" -< ");
+					sb.append(
+						Statistiek.getStringValue(this.model.getSplitOptions().getBinBoundaries().get(i + 1)));
 					sb.append("\n");
 				}
 				this.splitBoundariesArea.setText(sb.toString());
-				this.splitNoObjectsLabel.setText(Statistiek.rb
-					.getString("numberLabel")
+				this.splitNoObjectsLabel.setText(Statistiek.rb.getString("numberLabel")
 					+ this.model.getTableModel().getRowCount());
-				this.splitMinValueLabel.setText(Statistiek.rb
-					.getString("minLabel")
-					+ this.model.getTableModel().getColumnMin(
-						this.model.getSplitOptions().getColumnSplitIndex()));
-				this.splitMaxValueLabel.setText(Statistiek.rb
-					.getString("maxLabel")
-					+ this.model.getTableModel().getColumnMax(
-						this.model.getSplitOptions().getColumnSplitIndex()));
+				String splitMinValue = Statistiek.getStringValue(
+					this.model.getTableModel().getColumnMin(this.model.getSplitOptions().getColumnSplitIndex()));
+				this.splitMinValueLabel.setText(Statistiek.rb.getString("minLabel")
+					+ splitMinValue);
+				String splitMaxValue = Statistiek.getStringValue(
+					this.model.getTableModel().getColumnMax(this.model.getSplitOptions().getColumnSplitIndex()));
+				this.splitMaxValueLabel.setText(Statistiek.rb.getString("maxLabel")
+					+ splitMaxValue);
 				this.splitBinsBox.getParent().setVisible(true);
 				this.splitBinsLabel.getParent().setVisible(true);
 				setSplitEnumClasses(false);
