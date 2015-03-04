398f5cbf02aa1e2507e52b739cf82c6763932618
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-03-04 09:20:31 +0000

    decimale komma ipv punt voor nl
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@9109 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/frequencytable/FrequencyTableUserOptionsPanel.java
+++ src/fi/statistiek/frequencytable/FrequencyTableUserOptionsPanel.java
@@ -600,18 +600,16 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 			if (type.equals(AllowedTypes.DOUBLE)
 				|| type.equals(AllowedTypes.INTEGER))
 			{
-				this.minBoundaryField.setText(this.model
-					.getBinBoundaries().get(0).toString());
+				this.minBoundaryField.setText(
+					Statistiek.getStringValue(this.model.getBinBoundaries().get(0)));
 				// set the bin width based on the bin boundaries
 				this.binWidthField.setText(Statistiek.getFormattedBinWidth(this.model.getBinBoundaries()));
 				StringBuilder sb = new StringBuilder();
 				for (int i = 0; i < this.model.getNoBins(); i++)
 				{
-					sb.append(this.model
-						.getBinBoundaries().get(i).toString());
+					sb.append(Statistiek.getStringValue(this.model.getBinBoundaries().get(i)));
 					sb.append(" -< ");
-					sb.append(this.model
-						.getBinBoundaries().get(i + 1).toString());
+					sb.append(Statistiek.getStringValue(this.model.getBinBoundaries().get(i + 1)));
 					sb.append("\n");
 				}
 
@@ -619,12 +617,14 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
 				this.noObjectsLabel.setText(Statistiek.rb
 					.getString("numberLabel")
 					+ this.model.getTableModel().getRowCount());
+				String minValue = Statistiek.getStringValue(
+					this.model.getTableModel().getColumnMin(this.model.getColumnIndex()));
 				this.minValueLabel.setText(Statistiek.rb.getString("minLabel")
-					+ this.model.getTableModel().getColumnMin(
-						this.model.getColumnIndex()));
+					+ minValue);
+				String maxValue = Statistiek.getStringValue(
+					this.model.getTableModel().getColumnMax(this.model.getColumnIndex()));
 				this.maxValueLabel.setText(Statistiek.rb.getString("maxLabel")
-					+ this.model.getTableModel().getColumnMax(
-						this.model.getColumnIndex()));
+					+ maxValue);
 				setEnumClasses(false);
 			}
 			else if (type.equals(AllowedTypes.ENUM))
@@ -729,33 +729,31 @@ public class FrequencyTableUserOptionsPanel extends JPanel implements
     			if (splitType.equals(AllowedTypes.DOUBLE)
     				|| splitType.equals(AllowedTypes.INTEGER))
     			{
-    				this.splitMinBoundaryField.setText(this.model.getSplitOptions().getBinBoundaries()
-    						.get(0).toString());
+    				this.splitMinBoundaryField.setText(
+    					Statistiek.getStringValue(this.model.getSplitOptions().getBinBoundaries().get(0)));
     				// set the split bin width based on the split bin boundaries
     				this.splitBinWidthField.setText(Statistiek.getFormattedBinWidth(this.model.getSplitOptions().getBinBoundaries()));
     				StringBuilder sb = new StringBuilder();
     				for (int i = 0; i < this.model.getSplitOptions()
     					.getBinBoundaries().size() - 1; i++)
     				{
-    					sb.append(this.model.getSplitOptions()
-    						.getBinBoundaries().get(i).toString());
+    					sb.append(
+    						Statistiek.getStringValue(this.model.getSplitOptions().getBinBoundaries().get(i)));
     					sb.append(" -< ");
-    					sb.append(this.model.getSplitOptions()
-    						.getBinBoundaries().get(i + 1).toString());
+    					sb.append(
+    						Statistiek.getStringValue(this.model.getSplitOptions().getBinBoundaries().get(i + 1)));
     					sb.append("\n");
     				}
     				this.splitBoundariesArea.setText(sb.toString());
-    				this.splitNoObjectsLabel.setText(Statistiek.rb
-    					.getString("numberLabel")
+    				this.splitNoObjectsLabel.setText(Statistiek.rb.getString("numberLabel")
     					+ this.model.getTableModel().getRowCount());
-    				this.splitMinValueLabel.setText(Statistiek.rb
-    					.getString("minLabel")
-    					+ this.model.getTableModel().getColumnMin(
-    						this.model.getSplitOptions().getColumnSplitIndex()));
-    				this.splitMaxValueLabel.setText(Statistiek.rb
-    					.getString("maxLabel")
-    					+ this.model.getTableModel().getColumnMax(
-    						this.model.getSplitOptions().getColumnSplitIndex()));
+    				String splitMinValue = Statistiek.getStringValue(
+    					this.model.getTableModel().getColumnMin(this.model.getSplitOptions().getColumnSplitIndex()));
+    				this.splitMinValueLabel.setText(Statistiek.rb.getString("minLabel") + splitMinValue);
+    				String splitMaxValue = Statistiek.getStringValue(
+    					this.model.getTableModel().getColumnMax(this.model.getSplitOptions().getColumnSplitIndex()));
+    				this.splitMaxValueLabel.setText(Statistiek.rb.getString("maxLabel")
+    					+ splitMaxValue);
     				this.splitBinsBox.getParent().setVisible(true);
     				this.splitBinsLabel.getParent().setVisible(true);
     				setSplitEnumClasses(false);
