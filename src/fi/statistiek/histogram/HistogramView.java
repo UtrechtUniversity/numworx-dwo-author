33eed02eb6f484d46a4b07f24343ac40369039fe
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-03-19 11:10:00 +0000

    fix tooltip gestapelde percentages enum var (waarde van as)
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@9310 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/histogram/HistogramView.java
+++ src/fi/statistiek/histogram/HistogramView.java
@@ -3191,7 +3191,7 @@ public class HistogramView extends JPanel implements Observer
     										// percentage relatief aan totaalaantal
     										//value = ((double) frequencies_number[i][j*2] / aantal_totaal) * 100;
     										
-    										// test syl: bij gestapeld moet de waarde relatief aan totaal 
+    										// bij gestapeld moet de waarde relatief aan totaal 
     										// en tooltip de waarde aan de as
     										for (int k = 0; k <= i; k++)
     										{
@@ -3228,7 +3228,14 @@ public class HistogramView extends JPanel implements Observer
     									{
 //        										System.out.println("HistogramBarPanel.mouseMoved(): split gestapeld!");
             								// als gestapeld in 1 view, dan percentage relatief aan totaal per bin 
-                							value = ((double) frequencies_enum[i][j].frequency / aantalPerBin[j]) * 100;
+                							//value = ((double) frequencies_enum[i][j].frequency / aantalPerBin[j]) * 100;
+    										
+    										// bij gestapeld moet de waarde relatief aan totaal 
+    										// en tooltip de waarde aan de as
+    										for (int k = 0; k <= i; k++)
+    										{
+    											value = value + ((double) frequencies_enum[k][j].frequency / aantal_totaal) * 100;
+    										}
     									}
     								}
     								else
