60e7359f3c50b7cec8793e19df14e97714c73dc9
nilwi101 <nilwi101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-09-23 07:09:34 +0000

    laatste correctie voor OK ontgroening 20150923
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenOpdr@10510 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/algebrapijlenopdr/UitvoerSchuifComponent.java
+++ src/fi/algebrapijlenopdr/UitvoerSchuifComponent.java
@@ -553,13 +553,25 @@ public class UitvoerSchuifComponent extends AlgebraSchuifComponent implements Ac
 			zoomInKnop.setVisible(false);
 			zoomUitKnop.setVisible(false);
 			
-			pijlUit[0].im = null;
+			for (int pCnt = 0; pCnt < pijlUit.length; pCnt++)
+			{ 	if (pijlUit[pCnt] != null)
+					pijlUit[pCnt].im = null;
+			
+			}
 			
 			if (asv.apip.kijkNaActief)
 				asv.apip.answerChanged();
 		}
 		else if (!isBeginExpressie)
-		{	if (scrollable  && expressie != null && !Double.isNaN(expressie.geefWaarde().doubleValue()))
+		{	
+			for (int pCnt = 0; pCnt < pijlUit.length; pCnt++)
+			{ 	if (pijlUit[pCnt] != null)
+					pijlUit[pCnt].im = null;
+			
+			}
+
+			
+			if (scrollable  && expressie != null && !Double.isNaN(expressie.geefWaarde().doubleValue()))
 			{	if (scrollCorr == 0)
 					add(plusMinKnop);
 				scrollCorr = 10;
