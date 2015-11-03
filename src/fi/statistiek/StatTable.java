5c3f9ba8bee1b91c5890ff29fe8c53b1b4df99bf
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-11-03 09:58:22 +0000

    fix rechtermuisknop sorteer e.d. op mac, control-klik toegevoegd
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@10837 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/StatTable.java
+++ src/fi/statistiek/StatTable.java
@@ -315,7 +315,8 @@ public class StatTable extends JPanel implements StatistiekView,
 
 		public void mouseReleased(MouseEvent e)
 		{
-			if (e.isPopupTrigger())  // test syl: CTRL - klik?
+			if (e.isPopupTrigger() 
+				|| e.getButton() == MouseEvent.BUTTON3 || e.isControlDown())  // voor mac
 			{
 				Point p = e.getPoint();
 				int column = StatTable.this.table.columnAtPoint(p);
