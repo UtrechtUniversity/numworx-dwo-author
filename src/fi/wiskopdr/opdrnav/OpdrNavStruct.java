6197c9e36d10d23312957f14d9301383d30aeec4
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-11-23 15:00:56 +0000

    array out of bounds json kort arrays af, java niet
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@11008 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/wiskopdr/opdrnav/OpdrNavStruct.java
+++ src/fi/wiskopdr/opdrnav/OpdrNavStruct.java
@@ -1443,7 +1443,7 @@ public class OpdrNavStruct extends JLayeredPane implements MouseListener, Action
 			{
 				for (int j = 0; j < aantalOpdrachten[i]; j++)
 				{
-					this.scores[i][j] = orScores[i][j];
+					this.scores[i][j] = getInt( orScores, i, j);
 					isCorrect[i][j] = getBoolean(orGoedFout, i, j);
 				}
 			}
@@ -1458,7 +1458,7 @@ public class OpdrNavStruct extends JLayeredPane implements MouseListener, Action
 				for (int j = 0; j < aantalOpdrachten[i]; j++)
 				{
 					or[i].zetGemaakt(j + 1, getBoolean(orGoedFout, i, j));
-					or[i].zetScore(j + 1, orScores[i][j]);
+					or[i].zetScore(j + 1, getInt(orScores,i,j));
 				}
 			}
 		}
