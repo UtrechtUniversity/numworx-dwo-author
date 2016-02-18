044d8f9c2264fcf8dee6f8d32ab6f1f174a281b9
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-02-18 15:18:25 +0000

    outliersChanged() toegevoegd
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@11746 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/SelectionListener.java
+++ src/fi/statistiek/SelectionListener.java
@@ -1,8 +1,9 @@
 package fi.statistiek;
 
 /**
- * Interface for classes that listen to selection of StatTableModel
- * @author Manu Drijvers
+ * Interface for classes that listen to selection of StatTableModel.
+ * 
+ * @author Manu Drijvers, Sylvia van Borkulo
  *
  */
 public interface SelectionListener {
@@ -11,4 +12,9 @@ public interface SelectionListener {
 	 * Called when selection is changed
 	 */
 	public void selectionChanged();
+	
+	/**
+	 * Called when the set of marked outliers is changed
+	 */
+	public void outliersChanged();
 }
