b38b9c5c9b0ffadcb8a9fb253e718351137abe34
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-05-17 11:05:47 +0000

    fix traagheid: setVisible(false) voor removeAll(), setViewportView(mainPanel) in update() ipv setBounds()
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@12889 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/frequencytable/FrequencyTableView.java
+++ src/fi/statistiek/frequencytable/FrequencyTableView.java
@@ -274,7 +274,9 @@ public class FrequencyTableView extends JPanel implements Observer
 			this.dialogButton.setVisible(this.model.getStatTableModel()
 				.isViewsEditable());
 			
-			this.mainPanel.removeAll();
+			this.mainPanel.setVisible(false);
+			this.mainPanel.removeAll(); // this can be very slow if there are many children
+			this.mainPanel.setVisible(true);
 			
 			// initialize table height
 			this.tableHeight = 0;
@@ -455,6 +457,7 @@ public class FrequencyTableView extends JPanel implements Observer
 	
 			// set mainPanel size op het eind zodat splitClassPanels bestaan
 			this.setMainPanelSize();
+			this.scrollPane.setViewportView(mainPanel);
 	
 			this.mainPanel.revalidate();
 
@@ -923,17 +926,6 @@ public class FrequencyTableView extends JPanel implements Observer
 		super.setBounds(x, y, w, h);
 
 		this.setMainPanelSize();
-		
-		this.scrollPane.setViewportView(mainPanel);
-
-		// System.out.println("HistogramView.setBounds(): Size histogram: " +
-		// this.getBounds().toString()
-		// + ", scrollbarVisible=" +
-		// scrollPane.getVerticalScrollBar().isVisible());
-		
-//		 System.out.println("HistogramView.setBounds(): scrollPane w="
-//			 + scrollPane.getWidth()
-//			 + ", h=" + scrollPane.getHeight());
 	}
 
 	public void setModel(FrequencyTableModel model)
