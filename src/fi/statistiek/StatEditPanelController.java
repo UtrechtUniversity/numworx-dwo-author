cd0059ec2cb3fd07f9fb8d1e79571172adda29f5
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-05-19 10:33:23 +0000

    optimalisatie selectionIndices, rowOutlierIndices en cellOutlierIndices ipv lijsten booleans in state
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Statistiek/trunk@12904 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiek/StatEditPanelController.java
+++ src/fi/statistiek/StatEditPanelController.java
@@ -153,58 +153,53 @@ public class StatEditPanelController extends JPanel implements
 		{
 			this.model.getStatTableModel().setState((Hashtable) b.get("tableModel"));
 		}
-		if (b.containsKey("selectionList"))
+		
+		if (b.containsKey("selectionList")) // de oude manier
 		{
 			this.model.getStatTableModel().setSelectionList(
 				(ArrayList<Boolean>) b.get("selectionList"));
 		}
+		else if (b.containsKey("selectionIndices")) // de nieuwe manier
+		{
+			this.model.getStatTableModel().setSelectionIndices(
+				(ArrayList<Integer>) b.get("selectionIndices"));
+		}
 		else
 		{
-			ArrayList<Boolean> selectionList = new ArrayList<Boolean>(
-				this.model.getStatTableModel().getRowCount());
-			for (int i = 0; i < this.model.getStatTableModel().getRowCount(); i++)
-			{
-				selectionList.add(false);
-			}
-			this.model.getStatTableModel().setSelectionList(selectionList);
+			ArrayList<Integer> indicesList = new ArrayList<Integer>();
+			this.model.getStatTableModel().setRowOutlierIndices(indicesList);
 		}
 
-		if (b.containsKey("rowOutlierList"))
+		if (b.containsKey("rowOutlierList")) // if old version set the rowOutlierList the old fashion way
 		{
 			this.model.getStatTableModel().setRowOutlierList(
 				(ArrayList<Boolean>) b.get("rowOutlierList"));
 		}
+		else if (b.containsKey("rowOutlierIndices")) // de nieuwe manier
+		{
+			this.model.getStatTableModel().setRowOutlierIndices(
+				(ArrayList<Integer>) b.get("rowOutlierIndices"));
+		}
 		else
 		{
-			ArrayList<Boolean> rowOutlierList = new ArrayList<Boolean>(
-				this.model.getStatTableModel().getRowCount());
-			for (int i = 0; i < this.model.getStatTableModel().getRowCount(); i++)
-			{
-				rowOutlierList.add(false);
-			}
-			this.model.getStatTableModel().setRowOutlierList(rowOutlierList);
+			ArrayList<Integer> indicesList = new ArrayList<Integer>();
+			this.model.getStatTableModel().setRowOutlierIndices(indicesList);
 		}
 
-		if (b.containsKey("cellOutlierList"))
+		if (b.containsKey("cellOutlierList")) // de oude manier
 		{
 			this.model.getStatTableModel().setCellOutlierList(
 				(ArrayList<ArrayList<Boolean>>) b.get("cellOutlierList"));
 		}
+		else if (b.containsKey("cellOutlierIndices")) // de nieuwe manier
+		{
+			this.model.getStatTableModel().setCellOutlierIndices(
+				(ArrayList<ArrayList<Integer>>) b.get("cellOutlierIndices"));
+		}
 		else
 		{
-			ArrayList<ArrayList<Boolean>> cellOutlierList = new ArrayList<ArrayList<Boolean>>(
-				this.model.getStatTableModel().getColumnCount());
-			for (int i = 0; i < this.model.getStatTableModel().getColumnCount(); i++)
-			{
-				ArrayList list = new ArrayList<Boolean>(this.model.getStatTableModel().getRowCount());
-				for (int j = 0; j < this.model.getStatTableModel().getRowCount(); j++)
-				{
-					list.add(false);
-				}
-				
-				cellOutlierList.add(list);
-			}
-			this.model.getStatTableModel().setCellOutlierList(cellOutlierList);
+			ArrayList<ArrayList<Integer>> indicesList = new ArrayList<ArrayList<Integer>>();
+			this.model.getStatTableModel().setCellOutlierIndices(indicesList);
 		}
 
 		if (b.containsKey("statistiekViewTypes")
@@ -244,9 +239,9 @@ public class StatEditPanelController extends JPanel implements
 		System.out.println("StatEditPanelController.getEditState()");
 		Hashtable h = new Hashtable();
 		h.put("tableModel", this.model.getStatTableModel().getState());
-		h.put("selectionList", this.model.getStatTableModel().getSelectionList());
-		h.put("rowOutlierList", this.model.getStatTableModel().getRowOutlierList());
-		h.put("cellOutlierList", this.model.getStatTableModel().getCellOutlierList());
+		h.put("selectionIndices", this.model.getStatTableModel().getSelectionIndices());
+		h.put("rowOutlierIndices", this.model.getStatTableModel().getRowOutlierIndices());
+		h.put("cellOutlierIndices", this.model.getStatTableModel().getCellOutlierIndices());
 
 		int noViews = this.model.getViews().size();
 		String[] statistiekViewTypes = new String[noViews];
