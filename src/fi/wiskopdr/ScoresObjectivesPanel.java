7800b9f51b540e7efa6c84427954fcb5eeaee43d
tacom103 <tacom103@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-08-05 15:34:10 +0000

    Overzichtelijkere weergavemogelijkheden (work in progress)
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@13661 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/wiskopdr/ScoresObjectivesPanel.java
+++ src/fi/wiskopdr/ScoresObjectivesPanel.java
@@ -1,12 +1,16 @@
 package fi.wiskopdr;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.*;
 import java.util.*;
 
 import javax.swing.*;
 
-public class ScoresObjectivesPanel extends JPanel 
+public class ScoresObjectivesPanel extends JPanel implements MouseListener
 {
 	
 	/**
@@ -14,7 +18,7 @@ public class ScoresObjectivesPanel extends JPanel
 	 */
 	private static final long serialVersionUID = 2942694616353969444L;
 	private boolean kleurNeutraal;
-
+	
 	public static void main(String[] args ) {
 		
 		JFrame f = new JFrame("testing");
@@ -49,20 +53,27 @@ public class ScoresObjectivesPanel extends JPanel
 	
 	private String[][] objectives;
 	private String[] categorieString;
+	private String scoreText = "Score"; //TODO: in text opnemen 
+	private String categorieText = "Categorie"; //TODO: in text opnemen
+
 	
 	private String[][] objectivesForDiagram;
 	private String[] categorieStringForDiagram;
 	private int[][] totaalScoresForDiagram;
 	private int[][] totaalMaxForDiagram;
+	private boolean[] categorieUitgeklapt;
 	
 	int straal = 100;
 	int marge = 5;
 	int[] mpX;
 	int[] mpY;
+	//tbv nieuwe weergave:
+	//int tekstKolomBreedte, marge, regelHoogte, indent;
 	
 	int[][] totaalScoreObjectives;
 	int[][] totaalMaxObjectives;
 	double[][] scoresPercObjectives;
+	double[] categorieScoresPercObjectives;
 	int[] totaalMax;
 	double[][] hoek, cumHoek, labelHoek; 
 	double[][] hoekGraden, cumHoekGraden;
@@ -70,12 +81,14 @@ public class ScoresObjectivesPanel extends JPanel
 	int[][] straalRij;
 	
 	JTextArea[][] objectivesTextAreas;
+	JLabel[] categorieLabels;
 	Font theFont;
 	FontMetrics theFM;
 	Font theBoldFont;
 	FontMetrics theBoldFM;
 	
 	Color[][] kleurRij;
+	Color[] categorieKleurRij;
 	
 	public ScoresObjectivesPanel(Map map)
 	{
@@ -92,9 +105,10 @@ public class ScoresObjectivesPanel extends JPanel
 		categorieString = (String[]) map.get("categorieString");
 		
 		/* Makkelijk voor testen (deel hierboven afschermen)
-		objectives = new String[] {"Procenten/ verhoudingen", "Meten/ meetkunde", "Verbanden", "Getallen"};
-		totaalScoreObjectives = new int[] {60, 40, 80, 10};;
-		totaalMaxObjectives = new int[] {110, 70, 90, 15};
+		objectives = new String[][] {{"Procenten/ verhoudingen", "Meten/ meetkunde", "Verbanden", "Getallen"}, {"Nogwat"}};
+		totaalScoreObjectives = new int[][] {{30, 40, 80, 10}, {12}};
+		totaalMaxObjectives = new int[][] {{110, 70, 90, 15}, {100}};
+		categorieString = new String[] {"Categorie", "Extra"};
 		*/
 		
 		totaalMax = new int[objectives.length];
@@ -114,6 +128,7 @@ public class ScoresObjectivesPanel extends JPanel
 		categorieStringForDiagram = new String[nietNulTeller];
 		totaalScoresForDiagram = new int[nietNulTeller][];
 		totaalMaxForDiagram = new int[nietNulTeller][];
+		categorieUitgeklapt = new boolean[nietNulTeller];
 		
 		nietNulTeller = 0;
 		for(int j = 0; j < objectives.length; j++)
@@ -123,10 +138,12 @@ public class ScoresObjectivesPanel extends JPanel
 				totaalScoresForDiagram[nietNulTeller] = totaalScoreObjectives[j];
 				totaalMaxForDiagram[nietNulTeller] = totaalMaxObjectives[j];
 				totaalMax[nietNulTeller] = totaalMax[j];
+				categorieUitgeklapt[nietNulTeller] = false;
 				nietNulTeller++;
 			}
 		
 		scoresPercObjectives = new double[objectivesForDiagram.length][];
+		categorieScoresPercObjectives = new double[objectivesForDiagram.length];
 		hoek = new double[objectivesForDiagram.length][];
 		cumHoek = new double[objectivesForDiagram.length][];
 		hoekGraden = new double[objectivesForDiagram.length][];
@@ -188,11 +205,45 @@ public class ScoresObjectivesPanel extends JPanel
 				labelEindPuntX[j][i] = (int) ((straal + marge) * Math.sin(labelHoek[j][i]) + mpX[j]);
 				labelEindPuntY[j][i] = (int) (- (straal + marge) * Math.cos(labelHoek[j][i]) + mpY[j]);
 			}
+			int somScoresPerc = 0;
 			for(int k=0 ; k<objectivesForDiagram[j].length; k++)
 		   	{	if(totaalMaxForDiagram[j][k]==0) scoresPercObjectives[j][k] = 0;
 		   		else scoresPercObjectives[j][k] = Math.round(100.0*totaalScoresForDiagram[j][k]/totaalMaxForDiagram[j][k]);
+		   		somScoresPerc += scoresPercObjectives[j][k];
 		   	}
+			categorieScoresPercObjectives[j] = Math.round(somScoresPerc/objectivesForDiagram[j].length); 
 		}
+		
+		
+		
+		/* tbv nieuwe weergave
+		categorieLabels = new JLabel[objectivesForDiagram.length];
+		marge = 10;
+		regelHoogte = theFM.getHeight() + marge;
+		tekstKolomBreedte = 0;
+		indent = 20;
+		for(int j = 0; j < objectivesForDiagram.length; j++)
+		{
+			tekstKolomBreedte = Math.max(theFM. stringWidth(categorieStringForDiagram[j]) + marge, tekstKolomBreedte);
+			for(int i = 0; i < objectivesForDiagram[j].length; i++ )
+			{
+				tekstKolomBreedte = Math.max(theFM.stringWidth(objectivesForDiagram[j][i]) + marge + indent, tekstKolomBreedte);
+			}
+		}
+		
+		int categorieX = marge + 5;
+		int categorieY = regelHoogte + 5;
+		for(int j = 0; j < objectivesForDiagram.length; j++)
+		{
+			categorieY += regelHoogte;
+			categorieLabels[j] = new JLabel(categorieStringForDiagram[j]);
+			categorieLabels[j].setFont(theFont);
+			categorieLabels[j].setBounds(categorieX, categorieY, tekstKolomBreedte, regelHoogte);
+			add(categorieLabels[j]);
+			categorieLabels[j].addMouseListener(this);
+			
+		}
+		*/
 		
 		straalRij = new int[objectivesForDiagram.length][];
 		objectivesTextAreas = new JTextArea[objectivesForDiagram.length][];
@@ -232,6 +283,7 @@ public class ScoresObjectivesPanel extends JPanel
 				add(objectivesTextAreas[j][i]);
 			}
 		}
+		
 	}
 	
 	public int breedteLabel(int i, int j)
@@ -241,6 +293,7 @@ public class ScoresObjectivesPanel extends JPanel
 		
 		return breedte;	
 	}
+	
 	
 	public int hoogteLabel(int i, int j)
 	{
@@ -260,12 +313,21 @@ public class ScoresObjectivesPanel extends JPanel
 	}
 	
 	public void zetKleuren()
-	{	kleurRij = new Color[objectivesForDiagram.length][];
+	{	categorieKleurRij = new Color[objectivesForDiagram.length];
+		kleurRij = new Color[objectivesForDiagram.length][];
 		for(int j = 0; j < objectivesForDiagram.length; j++)
-		{	kleurRij[j] = new Color[objectivesForDiagram[j].length];
+		{	
+			int red = 255; 
+			int green = 255;
+			if(categorieScoresPercObjectives[j] < 50)
+				green = (int) (green * categorieScoresPercObjectives[j] / 50);
+			else 
+				red -= (int) (red * (categorieScoresPercObjectives[j] - 50)/50);
+			categorieKleurRij[j] = new Color(red, green, 0);
+			kleurRij[j] = new Color[objectivesForDiagram[j].length];
 			for(int i = 0; i < objectivesForDiagram[j].length; i++)
-			{	int red = 255; 
-				int green = 255;
+			{	red = 255; 
+				green = 255;
 				if(scoresPercObjectives[j][i] < 50)
 					green = (int) (green * scoresPercObjectives[j][i] / 50);
 				else 
@@ -274,6 +336,133 @@ public class ScoresObjectivesPanel extends JPanel
 			}	
 		}
 	}
+	
+//	public void paintComponentNieuw(Graphics g)
+//	{
+//		super.paintComponent(g);
+//		this.setBackground(Color.WHITE);
+//		Graphics2D g2 = (Graphics2D) g;
+//		Line2D.Double line;
+//		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+//		        RenderingHints.VALUE_ANTIALIAS_ON);
+//		
+//		zetKleuren();
+//		
+//		if(objectivesForDiagram.length == 1)//maar één categorie, later andere mogelijkheid verder uitwerken
+//		{
+//			int scoreBreedte = theFM.stringWidth(scoreText) + marge;
+//			int labelX = marge + 5;
+//			int scoreX = labelX + tekstKolomBreedte;
+//			int kleurX = marge + tekstKolomBreedte;
+//			int kleurY = marge;
+//			int tekstY = regelHoogte + 2;
+//			g.setFont(theBoldFont);
+//			g.drawString(categorieStringForDiagram[0], labelX, tekstY);
+//			g.drawString(scoreText, scoreX, tekstY);
+//			g.setFont(theFont);
+//			
+//			for(int i = 0; i < objectivesForDiagram[0].length; i++)
+//			{
+//				tekstY += regelHoogte;
+//				kleurY += regelHoogte;
+//				g.setColor(kleurRij[0][i]);
+//				g.fillRect(kleurX, kleurY, scoreBreedte, regelHoogte);
+//				g.setColor(Color.BLACK);
+//				g.drawString(objectivesForDiagram[0][i], labelX, tekstY);
+//				g.drawString((int) scoresPercObjectives[0][i]+"%", scoreX, tekstY);
+//			}
+//			int lijnHoogte = marge;
+//			g2.setColor(Color.BLACK);
+//			for(int i = 0; i < objectivesForDiagram[0].length + 2; i++)
+//			{	line = new Line2D.Double(marge, lijnHoogte, tekstKolomBreedte + scoreBreedte + marge, lijnHoogte);
+//				g2.draw(line);
+//				lijnHoogte += regelHoogte;
+//			}
+//			lijnHoogte -= regelHoogte;
+//			line = new Line2D.Double(marge, marge, marge, lijnHoogte);
+//			g2.draw(line);
+//			line = new Line2D.Double(marge + tekstKolomBreedte, marge, marge + tekstKolomBreedte, lijnHoogte);
+//			g2. draw(line);
+//			line = new Line2D.Double(marge + tekstKolomBreedte + scoreBreedte, marge, marge + tekstKolomBreedte + scoreBreedte, lijnHoogte);
+//			g2.draw(line);
+//			
+//			
+//		}
+//		else
+//		{	int categorieX = marge + 5;
+//			int scoreBreedte = theFM.stringWidth(scoreText) + marge;
+//			int labelX = marge + indent + 5;
+//			int scoreX = categorieX + tekstKolomBreedte;
+//			int kleurX = marge + tekstKolomBreedte;
+//			int lijnHoogte = marge;
+//			
+//			//int tekstY = regelHoogte + 2;
+//			int tekstVerschil = regelHoogte - marge + 2;
+//			int tussenRuimte = 5;
+//			g.setFont(theBoldFont);
+//			g.drawString(categorieText, categorieX, lijnHoogte + tekstVerschil);
+//			g.drawString(scoreText, scoreX, lijnHoogte + tekstVerschil);
+//			g.setFont(theFont);
+//			tekstVerschil += 3;
+//			line = new Line2D.Double(marge, lijnHoogte, tekstKolomBreedte + scoreBreedte + marge, lijnHoogte);
+//			g2.draw(line);
+//			lijnHoogte += regelHoogte;			
+//			
+//			for(int j = 0; j < objectivesForDiagram.length; j++)
+//			{
+//				//dubbel lijntje boven elke categorie.
+//				line = new Line2D.Double(marge, lijnHoogte, tekstKolomBreedte + scoreBreedte + marge, lijnHoogte);
+//				g2.draw(line);
+//				lijnHoogte += tussenRuimte;
+//				g.setColor(categorieKleurRij[j]);
+//				g.fillRect(kleurX, lijnHoogte, scoreBreedte, regelHoogte);
+//				g.setColor(Color.BLACK);
+//				line = new Line2D.Double(marge, lijnHoogte, tekstKolomBreedte + scoreBreedte + marge, lijnHoogte);
+//				g2.draw(line);
+//				
+//				//categorienaam en score invullen
+//				tekstVerschil += 5 - regelHoogte;
+//				//kleurY += regelHoogte;
+//				//g.setColor(Color.BLACK);
+//				categorieLabels[j].setLocation(categorieX, lijnHoogte + tekstVerschil);
+//				tekstVerschil += regelHoogte - 8;  //label-location en drawString hebben verschillende y nodig.
+//				g.drawString((int) categorieScoresPercObjectives[j]+"%", scoreX, lijnHoogte + tekstVerschil);
+//			
+//				if(categorieUitgeklapt[j])
+//				{
+//					for(int i = 0; i < objectivesForDiagram[j].length; i++)
+//					{
+//						lijnHoogte += regelHoogte;
+//						//kleurY += regelHoogte;
+//						g.setColor(kleurRij[j][i]);
+//						g.fillRect(kleurX, lijnHoogte, scoreBreedte, regelHoogte);
+//						g.setColor(Color.BLACK);
+//						g.drawString(objectivesForDiagram[j][i], labelX, lijnHoogte + tekstVerschil);
+//						g.drawString((int) scoresPercObjectives[j][i]+"%", scoreX, lijnHoogte + tekstVerschil);
+//						line = new Line2D.Double(marge, lijnHoogte, tekstKolomBreedte + scoreBreedte + marge, lijnHoogte);
+//						g2.draw(line);
+//						
+//					}
+//				}
+//				tekstVerschil += 3;
+//				lijnHoogte += regelHoogte;
+//			}
+//			line = new Line2D.Double(marge, lijnHoogte, tekstKolomBreedte + scoreBreedte + marge, lijnHoogte);
+//			g2.draw(line);
+//			
+//			//tabellijnen tekenen
+//			line = new Line2D.Double(marge, marge, marge, lijnHoogte);
+//			g2.draw(line);
+//			line = new Line2D.Double(marge + tekstKolomBreedte, marge, marge + tekstKolomBreedte, lijnHoogte);
+//			g2. draw(line);
+//			line = new Line2D.Double(marge + tekstKolomBreedte + scoreBreedte, marge, marge + tekstKolomBreedte + scoreBreedte, lijnHoogte);
+//			g2.draw(line);
+//			
+//		}
+//		
+//		
+//		
+//	}
 	
 	public void paintComponent(Graphics g)
 	{	super.paintComponent(g);
@@ -323,5 +512,45 @@ public class ScoresObjectivesPanel extends JPanel
 			else
 				g.drawString(categorieStringForDiagram[j], mpX[j] - theBoldFM.stringWidth(categorieStringForDiagram[j])/2, 370);
 		}
+	}
+
+	
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		for(int j = 0; j < objectivesForDiagram.length; j++)
+		{
+			if(e.getSource() == categorieLabels[j])
+			{
+				categorieUitgeklapt[j] = !categorieUitgeklapt[j];
+				repaint();
+				break;
+			}
+		}
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		setCursor(new Cursor(Cursor.HAND_CURSOR));
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}	
 }
