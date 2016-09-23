d622e17470ce75bf789178b10862eec843baed7e
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-09-23 15:10:12 +0000

    spaces in JAVA_HOME
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenOpdr@14042 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/sign.bat
+++ toolfiles/sign.bat
@@ -1,3 +1,3 @@
-%JAVA_HOME%\bin\pack200 --repack ../output/jar/algebrapijlenopdr.jar
-%JAVA_HOME%\bin\jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/algebrapijlenopdr.jar pboon
-%JAVA_HOME%\bin\pack200 ../output/jar/algebrapijlenopdr.jar.pack.gz ../output/jar/algebrapijlenopdr.jar
+"%JAVA_HOME%\bin\pack200" --repack ../output/jar/algebrapijlenopdr.jar
+"%JAVA_HOME%\bin\jarsigner" -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/algebrapijlenopdr.jar pboon
+"%JAVA_HOME%\bin\pack200" ../output/jar/algebrapijlenopdr.jar.pack.gz ../output/jar/algebrapijlenopdr.jar
