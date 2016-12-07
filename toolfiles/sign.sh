ddbf8c7798016c5b9cb2bf6799da7bf369848a20
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-12-07 08:49:23 +0000

    nieuwe geodefiner, was nog niet gecommit
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@15177 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/sign.sh
+++ toolfiles/sign.sh
@@ -1,12 +1,14 @@
-$JAVA_HOME/bin/pack200 --repack ../output/jar/wiskopdr.jar
-$JAVA_HOME/bin/jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/wiskopdr.jar pboon
-$JAVA_HOME/bin/pack200 ../output/jar/wiskopdr.jar.pack.gz ../output/jar/wiskopdr.jar
-$JAVA_HOME/bin/pack200 --repack ../output/jar/graphtool.jar
-$JAVA_HOME/bin/jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/graphtool.jar pboon
-$JAVA_HOME/bin/pack200 ../output/jar/graphtool.jar.pack.gz ../output/jar/graphtool.jar
-$JAVA_HOME/bin/pack200 --repack ../output/jar/balansfruitapplet.jar
-$JAVA_HOME/bin/jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/balansfruitapplet.jar pboon
-$JAVA_HOME/bin/pack200 ../output/jar/balansfruitapplet.jar.pack.gz ../output/jar/balansfruitapplet.jar
-$JAVA_HOME/bin/pack200 --repack ../output/jar/geodefiner.jar
-$JAVA_HOME/bin/jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/geodefiner.jar pboon
-$JAVA_HOME/bin/pack200 ../output/jar/geodefiner.jar.pack.gz ../output/jar/geodefiner.jar
+#!/bin/sh
+PATH=$JAVA_HOME/bin:$PATH
+pack200 --repack ../output/jar/wiskopdr.jar
+jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/wiskopdr.jar pboon
+pack200 ../output/jar/wiskopdr.jar.pack.gz ../output/jar/wiskopdr.jar
+pack200 --repack ../output/jar/graphtool.jar
+jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/graphtool.jar pboon
+pack200 ../output/jar/graphtool.jar.pack.gz ../output/jar/graphtool.jar
+pack200 --repack ../output/jar/balansfruitapplet.jar
+jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/balansfruitapplet.jar pboon
+pack200 ../output/jar/balansfruitapplet.jar.pack.gz ../output/jar/balansfruitapplet.jar
+pack200 --repack ../output/jar/geodefiner.jar
+jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pboon.keystore -storepass passww -keypass passw ../output/jar/geodefiner.jar pboon
+pack200 ../output/jar/geodefiner.jar.pack.gz ../output/jar/geodefiner.jar
