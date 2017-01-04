4bc7a96382e5d0cae55173d3a2e0516ffa4a1805
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2017-01-04 13:56:04 +0000

    common memento naar een java.util.List
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner-common/trunk@81 32ccb885-960d-4b53-a61f-10870b223014

---
+++ src/main/java/nl/numworx/geodefiner/common/Memento.java
@@ -0,0 +1,115 @@
+package nl.numworx.geodefiner.common;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import nl.uu.fi.dwo.interaction.client.json.ObjectList;
+import fi.euclides.event.NameMapper;
+import fi.euclides.event.Tracker;
+import fi.euclides.model.Codec;
+import fi.euclides.model.Destroyable;
+import fi.euclides.model.Lijn;
+import fi.euclides.model.Punt;
+import fi.euclides.model.math.Numbers;
+import fi.euclides.persist.DataInput;
+import fi.euclides.persist.DataOutput;
+import fi.euclides.proof.LabelDelegate;
+
+public class Memento extends fi.euclides.persist.Memento implements DataInput, DataOutput {
+	
+	private List<Object> list;
+	private ObjectList olist; int cursor;
+
+	public Memento(Tracker tracker) {
+		list = new ArrayList<Object>();
+		this.tracker = tracker;
+		setDataInputStream(this);
+		setDataOutputStream(this);
+	}
+
+	public void prepare(List<Destroyable> initial) {
+		readObjects = new Vector<>(initial);
+		writeObjects = new Vector<>(initial);
+		maxIndex = 0;
+	}
+	
+	public void fromList(ObjectList list) {
+		olist = list;
+		cursor = 0;
+	}
+	
+	public List toList() {
+		return list;
+	}
+	
+	@Override
+	public void writeUTF(String string) throws IOException {
+		list.add(string);
+	}
+
+	@Override
+	public String readUTF() throws IOException {
+		return olist.getString(cursor++);
+	}
+
+
+	@Override
+	public NameMapper getModel() {
+		return tracker.getMapper();
+	}
+
+
+	@Override
+	public int readUnsignedByte() throws IOException {
+		return olist.getInt(cursor++);
+	}
+
+	@Override
+	public int readInt() throws IOException {
+		return olist.getInt(cursor++);
+	}
+
+	@Override
+	public long readLong() throws IOException {
+		return Long.parseLong(olist.getString(cursor++));
+	}
+
+	@Override
+	public boolean readBoolean() throws IOException {
+		return olist.getBoolean(cursor++);
+	}
+
+	@Override
+	public double readDouble() throws IOException {
+		return olist.getDouble(cursor++);
+	}
+
+	@Override
+	public void writeByte(int i) throws IOException {
+		list.add(i);
+	}
+
+	@Override
+	public void writeInt(int i) throws IOException {
+		list.add(i);
+	}
+	
+	@Override
+	public void writeDouble(double d) throws IOException {
+		list.add(d);
+	}
+
+	@Override
+	public void writeLong(long a) throws IOException {
+		list.add(Long.toString(a));
+	}
+
+	@Override
+	public void writeBoolean(boolean b) throws IOException {
+		list.add(b);
+	}
+
+}
