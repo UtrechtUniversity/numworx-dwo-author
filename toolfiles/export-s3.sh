17af57dd6d09926aafe62390fec6b1770b2dec6a
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-01-27 12:09:31 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@15609 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export-s3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../output/jar
+S="wiskopdr.jar wiskopdr.jar.pack.gz graphtool.jar graphtool.jar.pack.gz balansfruitapplet.jar balansfruitapplet.jar.pack.gz geodefiner.jar geodefiner.jar.pack.gz"
+D=s3://test-dwo-nl/jars/
+s3cmd -P -M --no-mime-magic put $S $D
