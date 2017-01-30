dd579603eeef612e901cae4bec621327d6e08998
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2017-01-30 09:15:19 +0000

    equals met scale
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner-common/trunk@120 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/common/Equals.java
+++ src/main/java/nl/numworx/geodefiner/common/Equals.java
@@ -7,7 +7,9 @@ import fi.euclides.model.Kegelsnede2;
 import fi.euclides.model.Label;
 import fi.euclides.model.Lijn;
 import fi.euclides.model.Locus;
+import fi.euclides.model.Model;
 import fi.euclides.model.Punt;
+import fi.euclides.model.PuntenLijn;
 import fi.euclides.model.Segment;
 import fi.euclides.model.Triangle;
 import fi.euclides.model.math.Numbers;
@@ -25,6 +27,12 @@ class Equals extends LabelTester {
 		return new Destroyable[3];
 	}
 
+	private Numbers getScale() {
+		Model model = getTracker().getModel();
+		return Numbers.abs(Numbers.sub(model.getO().getX(), model.getU().getX()));
+	}
+	
+	
 	class EqualsVisitor implements Visitor {
 
 		Numbers test = Numbers.ONE;
@@ -38,11 +46,16 @@ class Equals extends LabelTester {
 		public void visitPunt(Punt p) {
 			if (b instanceof Punt) {
 				Punt pb = (Punt) b;
-				Numbers dy1 = Numbers.sub(p.getY(), pb.getY());
-				Numbers dx1 = Numbers.sub(p.getX(), pb.getX());
-				test = Numbers.hypot(dx1, dy1);
+				test = puntenTest(p, pb);
 			}
-			
+		}
+
+		private Numbers puntenTest(Punt p, Punt pb) {
+			Numbers dy1 = Numbers.sub(p.getY(), pb.getY());
+			Numbers dx1 = Numbers.sub(p.getX(), pb.getX());
+			Numbers test = Numbers.hypot(dx1, dy1);
+			test = Numbers.div(test, getScale()); // Overleg nodig?
+			return test;
 		}
 
 		@Override
@@ -53,11 +66,29 @@ class Equals extends LabelTester {
 		@Override
 		public void visitCirkel(Cirkel a) {
 			test = a .equals(b) ? Numbers.ZERO : Numbers.ONE;
+			if (test == Numbers.ONE && b instanceof Cirkel) {
+				Cirkel cb = (Cirkel) b;
+				test = puntenTest(cb.getCenter(), a.getCenter());
+				Numbers rtest = Numbers.abs(Numbers.sub(a.getR2n(), cb.getR2n()));
+				rtest = Numbers.div(rtest, getScale());
+				test = Numbers.add(test, rtest);
+			}
 		}
 
 		@Override
 		public void visitSegment(Segment a) {
-			test = a .equals(b) ? Numbers.ZERO : Numbers.ONE;		}
+			test = a .equals(b) ? Numbers.ZERO : Numbers.ONE;
+			if ( test == Numbers.ONE && b instanceof Segment) {
+				Punt a1 = a.getP1(); Punt a2 = a.getP2();
+				Punt b1 = ((PuntenLijn) b).getP1();
+				Punt b2 = ((PuntenLijn) b).getP2();
+				Numbers t1 = Numbers.add(puntenTest(a1,b1),puntenTest(a2,b2));
+				Numbers t2 = Numbers.add(puntenTest(a1,b2),puntenTest(a2,b1));
+				// minimum nemen
+				int s = Numbers.signum(Numbers.sub(t1, t2));
+				if(s < 0) test = t1; else  test = t2;
+			}
+		}
 
 		@Override
 		public void visitLabel(Label label) {
