e16c390ca971f1e1d68fcf22c8cd666cc15e4dac
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2017-01-31 14:52:50 +0000

    IntervalLabelTrack
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner-common/trunk@128 32ccb885-960d-4b53-a61f-10870b223014

---
+++ src/main/java/nl/numworx/geodefiner/common/IntervalLabelTrack.java
@@ -0,0 +1,36 @@
+package nl.numworx.geodefiner.common;
+
+import fi.euclides.model.Label;
+import fi.euclides.model.Punt;
+import fi.euclides.model.Track;
+import fi.euclides.model.Visitor;
+import fi.euclides.model.math.Numbers;
+
+class IntervalLabelTrack extends Track {
+
+	private Label l;
+	
+	IntervalLabelTrack(Label label, Numbers x, Numbers y) {
+		super(x, y);
+		l = label;
+	}
+
+	public void visit(Visitor v) {
+		l.visit(v);
+	}
+	/* (non-Javadoc)
+	 * @see euclides.Track#setXY(double, double)
+	 */
+	public void setXY(Numbers x, Numbers y) {
+//		l.moveTo(x,y);
+		Numbers dx = Numbers.sub(x, p.getX());
+		Numbers dy = Numbers.sub(y, p.getY());
+		Numbers newX = Numbers.add(dx, l.getX());
+		
+		
+		l.moveTo(newX, Numbers.add(dy, l.getY()));
+		super.setXY(x,y);
+		
+	}
+
+}
