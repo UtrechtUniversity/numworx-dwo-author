c6a9b42d9019857607dd03e60b43289c40007f67
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2017-02-14 13:04:40 +0000

    install integerfactory
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner/trunk@146 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/GeoDefiner.java
+++ src/main/java/nl/numworx/geodefiner/GeoDefiner.java
@@ -15,6 +15,8 @@ import org.cbook.cbookif.CBookWidgetInstanceIF;
 
 import fi.beans.wiskopdrbeans.InteractiePanel;
 import fi.beans.wiskopdrbeans.WiskOpdrApplet;
+import fi.euclides.math.IntegerFactory;
+import fi.euclides.model.math.Numbers;
 //import fi.euclides.openmath.Popcorn;
 import fi.euclides.swing.DoubleFormat;
 //import fi.euclides.swing.SwingSymbols;
@@ -29,7 +31,7 @@ public class GeoDefiner extends JApplet implements CBookWidgetIF, WiskOpdrApplet
 // FIXME Alleen met cbookeditor, verplaatsen naar Main en applet.Main
 		if(WiskOpdr.applet == null)
 			WiskOpdr.applet = new WiskOpdr();
-
+		Numbers.setFactory(IntegerFactory.INSTANCE);
 	}
 		
 	public static void main(String[] args) {
