07c123dc3e2ea4fcbab70535394b940202f83057
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-04-26 13:53:04 +0000

    nieuwe TSA
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@16300 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/sign.sh
+++ toolfiles/sign.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 PATH=$JAVA_HOME/bin:$PATH
+TSA=http://sha256timestamp.ws.symantec.com/sha256/timestamp
 pack200 --repack ../output/jar/wiskopdr.jar
-jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/wiskopdr.jar pboon
+jarsigner -sigfile UU -tsa $TSA -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/wiskopdr.jar pboon
 pack200 ../output/jar/wiskopdr.jar.pack.gz ../output/jar/wiskopdr.jar
 pack200 --repack ../output/jar/graphtool.jar
-jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/graphtool.jar pboon
+jarsigner -sigfile UU -tsa $TSA -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/graphtool.jar pboon
 pack200 ../output/jar/graphtool.jar.pack.gz ../output/jar/graphtool.jar
 pack200 --repack ../output/jar/balansfruitapplet.jar
-jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/balansfruitapplet.jar pboon
+jarsigner -sigfile UU -tsa $TSA -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/balansfruitapplet.jar pboon
 pack200 ../output/jar/balansfruitapplet.jar.pack.gz ../output/jar/balansfruitapplet.jar
 pack200 --repack ../output/jar/geodefiner.jar
-jarsigner -sigfile UU -tsa https://timestamp.geotrust.com/tsa -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/geodefiner.jar pboon
+jarsigner -sigfile UU -tsa $TSA -keystore ../../../tools/pb.keystore -storepass passww -keypass passww ../output/jar/geodefiner.jar pboon
 pack200 ../output/jar/geodefiner.jar.pack.gz ../output/jar/geodefiner.jar
