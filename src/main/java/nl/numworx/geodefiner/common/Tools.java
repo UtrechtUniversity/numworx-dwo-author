0da874649d66e075a5cf58943056b1889e6e77b1
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2017-07-27 08:40:38 +0000

    more Tools
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner-common/trunk@309 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/common/Tools.java
+++ src/main/java/nl/numworx/geodefiner/common/Tools.java
@@ -10,4 +10,38 @@ public interface Tools {
 	
 	int PERPENDICULAR = 5;
 	int PARALLEL = 6;
+	
+	int CIRCLE = 7;
+	int ARC = 8;
+	int TRIANGLE = 9;
+	
+	int MIDPOINT = 10;
+	int BISECTRICE = 11;
+	int MIRROR = 12;
+	
+	int CONIC_SECTION = 13;
+	int FOCUS = 14;
+	
+	int TANGENT = 15;
+	int POLELINE = 16;
+	
+	int LOCUS = 17;
+	
+	int DISTANCE = 18;
+	int AREA = 19;
+	int ANGLE = 20;
+	int VECTOR = 21;
+	
+	int TRAIL = 22;
+	int FORMULA = 23;
+
+	int PAN = 24;
+	int DESTROY = 25;
+	int RESET = 26;
+// NOT YET
+	int UNDO = 27;
+	int REDO = 28;
+	
+	
+	int TOOL_SIZE = 27;
 }
