599c2f133579da21e495430435c2f3433bed2edc
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-12-08 13:18:32 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/branches/maven@18512 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-s3-dev.sh
+++ toolfiles/export-s3-dev.sh
@@ -3,6 +3,7 @@ PATH=$PATH:/usr/local/bin
 cd ../target
 S="wiskopdr.jar wiskopdr.jar.pack.gz graphtool.jar graphtool.jar.pack.gz balansfruitapplet.jar balansfruitapplet.jar.pack.gz geodefiner.jar geodefiner.jar.pack.gz"
 D=s3://ebs-dev-dwo-nl/jars/
-for i in $S do 
+for i in $S  
+do
 	aws s3 cp --acl public-read $i $D
 done
