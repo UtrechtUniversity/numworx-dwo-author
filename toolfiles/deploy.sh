0aa41f029f30df3a7d3119740d98dc1396916ba4
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-03-12 14:05:12 +0000

    mount fisme-sites test
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@19462 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/deploy.sh
+++ toolfiles/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+if ! test -d /Volumes/fisme-sites/www-dev
+then
+	echo "MOUNT FISME-SITES"
+	exit 1;
+else
+	echo "fisme-sites mounted"
+fi
 set -xe
 jh=$(/usr/libexec/java_home -v 1.8)
 export JAVA_HOME=$jh
