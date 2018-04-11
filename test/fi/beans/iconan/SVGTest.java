5c47440b9bae4ef453401065edcb7dea09a8311d
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-04-11 09:08:43 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@19800 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- test/fi/beans/iconan/SVGTest.java
+++ test/fi/beans/iconan/SVGTest.java
@@ -16,8 +16,8 @@ public class SVGTest extends TestCase {
     ic.getNamemap().put("ellipse", data);
     assertEquals("height", 150, s.getHeight("ellipse"));
     assertEquals("map", Integer.valueOf(150), ic.getNamemap().get("ellipse/h"));
-    
   }
+
   public void testWidth() throws Exception {
     Iconan ic = new Iconan();
     SVGStrategy s = new SVGStrategy(ic);
@@ -30,4 +30,18 @@ public class SVGTest extends TestCase {
     assertEquals("map", Integer.valueOf(500), ic.getNamemap().get("ellipse/w"));
     
   }
+  
+  public void testViewBox() throws Exception {
+    Iconan ic = new Iconan();
+    SVGStrategy s = new SVGStrategy(ic);
+    InputStream in = getClass().getResourceAsStream("easypeasy.svg");
+    byte[] data = new byte[in.available()];
+    in.read(data);
+    in.close();
+    ic.getNamemap().put("easypeasy", data);
+    assertEquals("height", 91, s.getHeight("easypeasy"));
+    assertEquals("map/h", Integer.valueOf(91), ic.getNamemap().get("easypeasy/h"));
+    assertEquals("map/w", Integer.valueOf(110), ic.getNamemap().get("easypeasy/w"));
+  }
+
 }
