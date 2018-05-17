e919dcc38a0177a5d4b10d937d2bfa867c78c049
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-05-17 08:59:05 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Kladje@20200 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-jar.sh
+++ toolfiles/export-jar.sh
@@ -11,3 +11,5 @@ fi
 APPLET=kladje
 cp ../target/$APPLET.jar $R/www-dev/dwo/jars/
 cp ../target/$APPLET.jar.pack.gz $R/www-dev/dwo/jars/
+cp ../target/$APPLET.jar $R/www-dev/javaclasses/jars/
+cp ../target/$APPLET.jar.pack.gz $R/www-dev/javaclasses/jars/
