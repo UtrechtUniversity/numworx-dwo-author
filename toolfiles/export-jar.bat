8f0741d2f21292e0c65d174d339e1dd0fd78c04d
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-05-17 08:57:45 +0000

    remove tomcat stuff
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/Kladje@20199 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-jar.bat
+++ toolfiles/export-jar.bat
@@ -1,7 +1,5 @@
 copy ..\output\jar\kladje.jar R:\www-dev\dwo\jars\kladje.jar
 copy ..\output\jar\kladje.jar.pack.gz R:\www-dev\dwo\jars\kladje.jar.pack.gz
-copy ..\output\jar\kladje.jar R:\tomcat\webapps-dwo\dwo\jars\kladje.jar
-copy ..\output\jar\kladje.jar.pack.gz R:\tomcat\webapps-dwo\dwo\jars\kladje.jar.pack.gz
 copy ..\output\jar\kladje.jar R:\www-dev\javaclasses\jars\kladje.jar
 copy ..\output\jar\kladje.jar.pack.gz R:\www-dev\javaclasses\jars\kladje.jar.pack.gz
 
