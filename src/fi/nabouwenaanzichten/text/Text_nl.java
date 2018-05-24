59bfab7e0edd165725c89afdc06f1a8a8d10fffd
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-05-24 09:47:15 +0000

    DWOWIDGET-181 implementatie cross widget action.correct/false/false2
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NabouwenAanzichten@20240 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/nabouwenaanzichten/text/Text_nl.java
+++ src/fi/nabouwenaanzichten/text/Text_nl.java
@@ -121,8 +121,9 @@ public class Text_nl extends java.util.ListResourceBundle
 				
 				{ "CBA_blockBuilding", "blokkenbouwsel"},
 				{ "CBA_text.buildingProgram", "bouwprogramma"},
-				
-				
+				{ "CBA_action.correct", "Actie: correct antwoord"},
+				{ "CBA_action.false", "Actie: fout antwoord"},
+				{ "CBA_action.false_2", "Actie: 2x fout antwoord"}
         };
 }
 
