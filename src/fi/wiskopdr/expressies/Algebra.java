13ab7218b971c3beb5833fb1165ef333eed9627d
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-01-15 10:32:09 +0000

    DWOWIDGET-205 getransponeerde matrix is niet exact gelijk aan doelmatrix
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@22309 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/wiskopdr/expressies/Algebra.java
+++ src/fi/wiskopdr/expressies/Algebra.java
@@ -880,20 +880,29 @@ public class Algebra
 		else if (isMatrix(e1) && isMatrix(e2))
 		{
 			Matrix matrix1, matrix2;
-			if (e1 instanceof Matrix)
-				matrix1 = (Matrix) e1;
-			else
-				matrix1 = e1.geefMatrix();
-				
-			if (e2 instanceof Matrix)
-				matrix2 = (Matrix) e2;
-			else
-				matrix2 = e2.geefMatrix();
 			
-			if (matrix1 == null || matrix2 == null) // er is iets mis, ongeldige matrix-expressie
+			if ((isGetransponeerdeMatrix(e1) && !isGetransponeerdeMatrix(e2))
+				|| (isGetransponeerdeMatrix(e2) && !isGetransponeerdeMatrix(e1)))
+			{
 				zijnGelijk = false;
+			}
 			else
-				zijnGelijk = zijnGelijkMatrices(matrix1, matrix2);			
+			{
+				if (e1 instanceof Matrix)
+					matrix1 = (Matrix) e1;
+				else
+					matrix1 = e1.geefMatrix();
+					
+				if (e2 instanceof Matrix)
+					matrix2 = (Matrix) e2;
+				else
+					matrix2 = e2.geefMatrix();
+				
+				if (matrix1 == null || matrix2 == null) // er is iets mis, ongeldige matrix-expressie
+					zijnGelijk = false;
+				else
+					zijnGelijk = zijnGelijkMatrices(matrix1, matrix2);
+			}
 		}
 		else
 			zijnGelijk = zijnGelijk(e1, e2, false);
@@ -901,6 +910,23 @@ public class Algebra
 		return zijnGelijk;
 	}
 	
+	/**
+	 * Als de gegeven expressie een getransponeerde matrix is, d.w.z.
+	 * macht(matrix, T), dan wordt true geretourneerd anders false.
+	 * 
+	 * @param e
+	 * @return
+	 */
+	private static boolean isGetransponeerdeMatrix(Expressie e)
+	{
+		boolean isGetransponeerd = false;
+		
+		if (isMatrix(e) && e instanceof Macht && "T".equals(e.kind2.toString()))
+			isGetransponeerd = true;
+			
+		return isGetransponeerd;
+	}
+
 	/**
 	 * Als elk van de kinderen van de vectoren paarsgewijs gelijk zijn, dan true,
 	 * anders false.
