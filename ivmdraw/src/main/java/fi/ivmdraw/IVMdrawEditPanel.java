d766f8cd3c6609bdab798f0dab7ed05f06f3444e
boon0104 <boon0104@a766e028-ca59-4e03-b166-cfe2b108fda4>
2019-05-28 20:29:09 +0000

    InteractieEditPanel werkend in WiskOpdr
    git-svn-id: https://svn.science.uu.nl/repos/sci.boon0104.StageHvA/IVMdraw-parent/trunk@8 a766e028-ca59-4e03-b166-cfe2b108fda4

--- ivmdraw/src/main/java/fi/ivmdraw/IVMdrawEditPanel.java
+++ ivmdraw/src/main/java/fi/ivmdraw/IVMdrawEditPanel.java
@@ -2,44 +2,48 @@ package fi.ivmdraw;
 
 import java.util.Hashtable;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import fi.beans.wiskopdrbeans.InteractieEditPanel;
 
 public class IVMdrawEditPanel extends JPanel implements InteractieEditPanel {
 
-  public IVMdrawEditPanel(IVMdrawPanel ivmDrawPanel) {
-    // TODO Auto-generated constructor stub
-  }
+	IVMdrawPanel ivmDrawPanel;
+	
+	public IVMdrawEditPanel(IVMdrawPanel ivmDrawPanel) {
+		this.ivmDrawPanel = ivmDrawPanel;
+		 JLabel label = new JLabel("Dit is het interactieEditpanel");
+		    add(label);
+	}
 
-  public Hashtable getEditState() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	public Hashtable getEditState() {
+		return ivmDrawPanel.getEditState();
+	}
 
-  public void setEditState(Hashtable arg0) {
-    // TODO Auto-generated method stub
+	public void setEditState(Hashtable arg0) {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
-  public void start() {
-    // TODO Auto-generated method stub
+	public void start() {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
-  public void stop() {
-    // TODO Auto-generated method stub
+	public void stop() {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
-  public void zetBreedte(int arg0) {
-    // TODO Auto-generated method stub
+	public void zetBreedte(int arg0) {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
-  public void zetHoogte(int arg0) {
-    // TODO Auto-generated method stub
+	public void zetHoogte(int arg0) {
+		// TODO Auto-generated method stub
 
-  }
+	}
 
 }
