dd0d41dec3263725555a9abc1ce1808d578fcad4
boon0104 <boon0104@a766e028-ca59-4e03-b166-cfe2b108fda4>
2019-05-29 21:00:08 +0000

    Vaaskeuze toegevoegd
    git-svn-id: https://svn.science.uu.nl/repos/sci.boon0104.StageHvA/IVMdraw-parent/trunk@10 a766e028-ca59-4e03-b166-cfe2b108fda4

--- ivmdraw/src/main/java/fi/ivmdraw/IVMdrawEditPanel.java
+++ ivmdraw/src/main/java/fi/ivmdraw/IVMdrawEditPanel.java
@@ -1,7 +1,9 @@
 package fi.ivmdraw;
 
+import java.util.HashMap;
 import java.util.Hashtable;
 
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -10,15 +12,27 @@ import fi.beans.wiskopdrbeans.InteractieEditPanel;
 public class IVMdrawEditPanel extends JPanel implements InteractieEditPanel {
 
 	IVMdrawPanel ivmDrawPanel;
+	JComboBox vaasKeuze;
 	
 	public IVMdrawEditPanel(IVMdrawPanel ivmDrawPanel) {
 		this.ivmDrawPanel = ivmDrawPanel;
-		 JLabel label = new JLabel("Dit is het interactieEditpanel");
-		    add(label);
+		JLabel label = new JLabel("Dit is het interactieEditpanel");
+		add(label);
+		    
+		vaasKeuze = new JComboBox();
+		vaasKeuze.addItem("Vaas 1");
+		vaasKeuze.addItem("Vaas 2");
+		vaasKeuze.addItem("Vaas 3");
+		vaasKeuze.addItem("Vaas 4");
+		vaasKeuze.addItem("Vaas 5");
+		add(vaasKeuze);
 	}
 
 	public Hashtable getEditState() {
-		return ivmDrawPanel.getEditState();
+		Hashtable h = ivmDrawPanel.getEditState();
+		h.put("vaasNummer", new Integer(vaasKeuze.getSelectedIndex()));
+		return h;
+		
 	}
 
 	public void setEditState(Hashtable arg0) {
