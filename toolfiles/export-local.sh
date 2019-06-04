5e031949a11b880bbdd6203649d016dfa9adaf06
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-06-04 07:38:59 +0000

    local export script
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@23213 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ toolfiles/export-local.sh
@@ -0,0 +1,12 @@
+R=~/Public/jars
+
+cp ../target/wiskopdr.jar $R/wiskopdr.jar
+cp ../target/wiskopdr.jar.pack.gz $R/wiskopdr.jar.pack.gz
+cp ../target/graphtool.jar $R/graphtool.jar
+cp ../target/graphtool.jar.pack.gz $R/graphtool.jar.pack.gz
+cp ../target/balansfruitapplet.jar $R/balansfruitapplet.jar
+cp ../target/balansfruitapplet.jar.pack.gz $R/balansfruitapplet.jar.pack.gz
+cp ../target/geodefiner.jar $R/geodefiner.jar
+cp ../target/geodefiner.jar.pack.gz $R/geodefiner.jar.pack.gz
+cp ../target/statistiek.jar $R/statistiek.jar
+cp ../target/statistiek.jar.pack.gz $R/statistiek.jar.pack.gz
