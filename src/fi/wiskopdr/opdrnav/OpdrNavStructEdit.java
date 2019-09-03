2666970d430678742e90ff108e93f218cf729226
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-09-03 21:29:29 +0000

    layout knoppen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@23487 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/wiskopdr/opdrnav/OpdrNavStructEdit.java
+++ src/fi/wiskopdr/opdrnav/OpdrNavStructEdit.java
@@ -263,12 +263,12 @@ public class OpdrNavStructEdit extends JLayeredPane implements MouseListener, Ac
 		//
 		
 		imagesButton = new JButton(WiskOpdr.rb.getString("imagesButtonLabel"));
-		imagesButton.setFont(font);
+		//imagesButton.setFont(font);
 		imagesButton.setMargin(new Insets(3, 5, 3, 5));
-		//imagesButton.setBorder(BorderFactory.createEmptyBorder());
+		imagesButton.setBorder(BorderFactory.createEmptyBorder());
 		imagesButton.addActionListener(this);
-		//imagesButton.setBackground(new Color(180,195,228));
-		//imagesButton.setForeground(new Color(51,74,112));
+		imagesButton.setBackground(new Color(120,150,202));
+		imagesButton.setForeground(new Color(255,255,255));
         add(imagesButton,0);
 		
 		sizeLabel = new JLabel("123,456Mb");
@@ -279,13 +279,13 @@ public class OpdrNavStructEdit extends JLayeredPane implements MouseListener, Ac
 		add(sizeLabel,0);
 		
 		instellingenKnop = new JButton(WiskOpdr.rb.getString("optiesButtonLabel"));
-		instellingenKnop.setFont(font);
-		//instellingenKnop.setBorder(BorderFactory.createEmptyBorder());
+		//instellingenKnop.setFont(font);
+		instellingenKnop.setBorder(BorderFactory.createEmptyBorder());
 		instellingenKnop.setMargin(new Insets(3, 5, 3, 5));
 		instellingenKnop.setBounds(10, actKeuzePanelY + aantalActiviteiten * 20 - 20, 1600, 20);
 		instellingenKnop.addActionListener(this);
-		//instellingenKnop.setBackground(new Color(180,195,228));
-		//instellingenKnop.setForeground(new Color(51,74,112));
+		instellingenKnop.setBackground(new Color(120,150,202));
+		instellingenKnop.setForeground(new Color(255,255,255));
 		add(instellingenKnop,0);
 
 		instellingenDialog = DialogFacade.newInstance(this, WiskOpdr.rb.getString("optiesButtonLabel"), true);
