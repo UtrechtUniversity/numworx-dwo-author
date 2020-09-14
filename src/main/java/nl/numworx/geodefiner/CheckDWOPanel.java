732d02b1595f8c3f60bb1b82f9ae4b714f931cd2
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2020-09-14 13:22:00 +0000

    Met studentmodel test, objectives zijn nu null als je leerdoelen hebt.
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner/trunk@886 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/CheckDWOPanel.java
+++ src/main/java/nl/numworx/geodefiner/CheckDWOPanel.java
@@ -98,7 +98,7 @@ public class CheckDWOPanel extends JPanel implements ChangeListener, ActionListe
 		formule.setPreferredSize(new Dimension(200,50));
 		formule.setMaximumSize(new Dimension(Short.MAX_VALUE, 50));
 		formule.formuleVak.addActionListener(this);
-		if(WiskOpdr.objectives!=null)
+		if(WiskOpdr.objectives!=null || WiskOpdr.studentModel != null)
 			objBtn = new ObjectiveChoiceButton(WiskOpdr.objectives, WiskOpdr.categorieString, WiskOpdr.studentModel);
 		add(checkDWO);
 		add(extern);
