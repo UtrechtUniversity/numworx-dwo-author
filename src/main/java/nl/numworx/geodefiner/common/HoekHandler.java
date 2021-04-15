d2ad718929f87b3c49cc6828227408a46b0f443b
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2021-04-15 09:49:02 +0000

    Hoek in de buurt van het snijpunt
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner-common/trunk@969 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/common/HoekHandler.java
+++ src/main/java/nl/numworx/geodefiner/common/HoekHandler.java
@@ -1,5 +1,15 @@
 package nl.numworx.geodefiner.common;
 
+import fi.euclides.model.CarryingLine;
+import fi.euclides.model.Destroyable;
+import fi.euclides.model.Label;
+import fi.euclides.model.Lijn;
+import fi.euclides.model.Punt;
+import fi.euclides.model.Ray;
+import fi.euclides.model.Segment;
+import fi.euclides.model.SnijPunt;
+import fi.euclides.model.math.Numbers;
+
 public class HoekHandler extends fi.euclides.proof.HoekHandler {
 
 	public HoekHandler(String string) {
@@ -8,6 +18,40 @@ public class HoekHandler extends fi.euclides.proof.HoekHandler {
 
 	public HoekHandler() {
 		super();
+	}
+
+	@Override
+	public <T extends Destroyable> T visit(T t) {
+		
+		volg( (Label) t);
+		return super.visit(t);
+	}
+
+	private void volg(Label l) {
+		Destroyable depend[] = l.getDepend();
+		if (depend[1] instanceof Punt)
+		{
+			Punt p = (Punt) depend[1];
+			Volgpunt vp = new Volgpunt(p);
+			vp.setDxy(Numbers.createInteger(10), Numbers.createInteger(-10));
+			l.setP(vp);
+		} else if (depend[1] instanceof Lijn) {
+			SnijPunt p = new SnijPunt(lijn(depend[0]), lijn( depend[1]));
+			Volgpunt vp = new Volgpunt(p);
+			vp.setDxy(Numbers.createInteger(10), Numbers.createInteger(-10));
+			l.setP(vp);
+		}
+	}
+
+	private Lijn lijn(Destroyable d) {
+		if (d instanceof Segment) {
+			return new CarryingLine((Segment)d);
+		}
+		if (d instanceof Ray) {
+			return new CarryingLine((Ray)d);
+
+		}
+		return (Lijn) d;
 	}
 
 }
