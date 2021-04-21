7a4664e2222ac35050fb64e9946e02b200fd1055
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2021-04-21 11:11:20 +0000

    Hoekhandler met pointer functie
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner-common/trunk@977 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/common/HoekHandler.java
+++ src/main/java/nl/numworx/geodefiner/common/HoekHandler.java
@@ -1,9 +1,13 @@
 package nl.numworx.geodefiner.common;
 
+import java.util.Vector;
+
+import fi.euclides.event.TrackerContext;
 import fi.euclides.model.CarryingLine;
 import fi.euclides.model.Destroyable;
 import fi.euclides.model.Label;
 import fi.euclides.model.Lijn;
+import fi.euclides.model.Model;
 import fi.euclides.model.Punt;
 import fi.euclides.model.Ray;
 import fi.euclides.model.Segment;
@@ -11,8 +15,13 @@ import fi.euclides.model.SnijPunt;
 import fi.euclides.model.math.Numbers;
 import fi.euclides.proof.AngleType;
 import fi.euclides.util.DefaultAdapter;
+import fi.euclides.util.Messages;
 
 public class HoekHandler extends fi.euclides.proof.HoekHandler {
+
+	private int state;
+	Punt p0, p1;
+	Lijn l0;
 
 	public HoekHandler(String string) {
 		super(string);
@@ -50,6 +59,106 @@ public class HoekHandler extends fi.euclides.proof.HoekHandler {
 
 		}
 		return (Lijn) d;
+	}
+
+	@Override
+	public void command() {
+		Vector<Destroyable> select = getModel().getSelect();
+		state = select.size();
+		if (state == 0) {
+			setStatus(string);
+			testLijn = true;
+			testPunt = true;
+			getTracker().setPointerHandler(this);
+			return;
+		}
+		if (state == 1) {
+			Destroyable d = select.firstElement();
+			
+			String message;
+			if (d instanceof Lijn) {
+				message = "AddLoodLijnHandler.0";
+				testPunt = false;
+				testLijn = true;
+				l0 = (Lijn) d; p0 = p1 = null;
+			}
+			else if (d instanceof Punt) {
+				message = "AddLijnHandler.1";
+				p0 = (Punt) d; p1 = null; l0 = null;
+				testPunt = true;
+				testLijn = false;
+			}
+			else {
+				message = "Euclides.85";
+				testLijn = true;
+				testPunt = true;
+				getModel().clearSelection();
+				state = 0;
+			}
+			getTracker().setPointerHandler(this);
+			setStatus(Messages.getString(message));
+			return;
+		}
+		if (state == 2 && select.firstElement() instanceof Punt && select.lastElement() instanceof Punt) {
+			p0 = (Punt) select.firstElement();
+			p1 = (Punt) select.lastElement();
+			l0 = null;
+			setStatus(Messages.getString("AddLoodLijnHandler.1"));
+			getTracker().setPointerHandler(this);
+			return; 
+		}
+		state = 0;
+		super.command();
+	}
+
+	@Override
+	public void pointerReleased(Numbers x, Numbers y, TrackerContext context) {
+		pointerDragged(x,y,context);
+		context.setTrack(null);
+		Model m = getModel();
+		Vector<Destroyable> select = context.selection();
+		if (state == 2) {
+			if (select.size() == 1 && select.firstElement() instanceof Punt) {
+				select.insertElementAt(p0, 0);
+				select.insertElementAt(p1, 1);
+				state = 0;
+				super.command();
+				setStatus(string);
+				return;
+			}
+		} else
+		if (state == 1) {
+			if (l0 != null && select.size() == 1 && select.firstElement() instanceof Lijn) {
+				select.insertElementAt(l0, 0);
+				state = 0;
+				super.command();
+				setStatus(string);
+				return;
+			}
+			if (p0 != null && select.size() == 1 && select.firstElement() instanceof Punt) {
+				p1 = (Punt) select.firstElement();
+				state = 2;
+				context.clearSelection();
+				setStatus(Messages.getString("AddLijnHandler.1"));
+				return;
+			}
+		} else 
+		if (state == 0 && select.size() == 1) {
+			Destroyable d = select.firstElement();
+			if (d instanceof Punt) {
+				p0 = (Punt)d; p1 = null; l0 = null; testLijn = false;
+				state = 1;
+				context.clearSelection();
+				setStatus(Messages.getString("AddLoodLijnHandler.1"));
+				return;
+			}
+			if (d instanceof Lijn) {
+				l0 = (Lijn) d; p0=p1=null;
+				state = 1;
+				context.clearSelection();
+				setStatus(Messages.getString("AddLoodLijnHandler.0"));
+			}
+		}
 	}
 
 }
