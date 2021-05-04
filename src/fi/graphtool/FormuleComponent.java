0ec4714fad53aa96ad862537b22ee16c80cfdec2
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-05-04 14:45:18 +0000

    equals fix
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/GraphTool/trunk@26394 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/graphtool/FormuleComponent.java
+++ src/fi/graphtool/FormuleComponent.java
@@ -459,7 +459,7 @@ public class FormuleComponent extends FormuleEditor implements FocusListener, Mo
      	for(int i=0 ; i<domeinStrings.length; i++)
 		{	
      		if(domeinStrings != null && i < domeinStrings.length && i < domeinButtons.length)
-     		{	if(!domeinStrings[i].equals("$f@"))
+     		{	if(!domeinStrings[i][0].equals("$f@"))
     			{	if(randomVars != null)
     				{	try
 						{	domeinStrings[i][0] = FormuleParser.randomizeString(domeinStrings[i][0],randomVars,randomValues);
