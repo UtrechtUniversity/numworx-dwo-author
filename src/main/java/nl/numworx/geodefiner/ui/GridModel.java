0b286309fd982c5066aff752db1b26929d4242a3
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2022-05-04 08:14:52 +0000

    https://numworx.atlassian.net/browse/NUMGEODEF-143
    met instelbare 'snap' in pixels
    
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner/trunk@1053 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/ui/GridModel.java
+++ src/main/java/nl/numworx/geodefiner/ui/GridModel.java
@@ -5,29 +5,34 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
-import nl.numworx.geodefiner.Instance;
 import nl.numworx.geodefiner.Snapper;
 import nl.numworx.geodefiner.common.LineType;
 import nl.numworx.geodefiner.common.UIModel;
 import nl.uu.fi.dwo.interaction.client.json.ObjectMap;
 import fi.euclides.event.Tracker;
 import fi.euclides.model.Destroyable;
-import fi.euclides.model.Lijn;
 import fi.euclides.model.Locus;
 
 public class GridModel extends LineModel {
 	boolean gravity;
+	int snap = Snapper.DEFAULT_SNAP;
 
 	@Override
 	public Map<String, Object> toMap() {
 		Map<String, Object> map = super.toMap();
 		map.put("gravity", gravity);
+		if (snap != Snapper.DEFAULT_SNAP) 
+			map.put("snap", snap);
 		return map;
 	}
 
 	@Override
 	public void fromMap(ObjectMap map) {
 		gravity = map.getBoolean("gravity", false);
+		if (map.containsKey("snap"))
+			snap = map.getInt("snap");
+		else
+			snap = Snapper.DEFAULT_SNAP;
 		super.fromMap(map);
 	}
 
@@ -41,6 +46,7 @@ public class GridModel extends LineModel {
 		super.install(item);
 		Snapper snapper = tracker.adapt(Snapper.class);
 		snapper.setGravity(gravity);
+		snapper.setSnap(snap);
 	}
 	
 	public UIModel<Destroyable, UIEditor> init(Locus item) {
--- src/main/java/nl/numworx/geodefiner/ui/GridPane.java
+++ src/main/java/nl/numworx/geodefiner/ui/GridPane.java
@@ -1,15 +1,21 @@
 package nl.numworx.geodefiner.ui;
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.Box;
 import fi.beans.numworxlf.JCheckBox;
+import fi.beans.numworxlf.JFormattedTextField;
+
 import javax.swing.JLabel;
 
 import fi.euclides.util.Messages;
 
 @SuppressWarnings("serial")
-public class GridPane extends LinePane<GridModel> {
+public class GridPane extends LinePane<GridModel> implements ItemListener {
 
 	JCheckBox gravity;
+	JFormattedTextField snapField;
 	
 	public GridPane(GridModel model) {
 		super(model);
@@ -19,15 +25,31 @@ public class GridPane extends LinePane<GridModel> {
 		Box hbox = Box.createHorizontalBox();
 		hbox.add(new JLabel(Messages.getString("GridPane.1")));
 		hbox.add(gravity);
+		hbox.add(new JLabel(Messages.getString("GridPane.2")));
+		snapField = new JFormattedTextField();
+		snapField.setValue(Double.valueOf(model.snap));
+		snapField.setColumns(4);
+		snapField.setMaximumSize(snapField.getPreferredSize());
+		
+		snapField.setEnabled(model.gravity);
+		hbox.add(snapField);
+		hbox.add(new JLabel("px"));		
 		hbox.add(Box.createGlue());
 		content.add(hbox);
+		gravity.addItemListener(this);
 		setSizes();
 	}
 
 	@Override
 	public void commit() {
 		model.gravity = gravity.isSelected();
+		model.snap = ((Number) snapField.getValue()).intValue();
 		super.commit();
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		snapField.setEnabled(e.getStateChange() == ItemEvent.SELECTED);		
 	}
 
 }
