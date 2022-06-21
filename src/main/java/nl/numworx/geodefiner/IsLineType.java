d70c67344297f0ece8ff42cbb2e5acc66ea4dc71
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2022-06-21 11:16:28 +0000

    refactoring isLinetype
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner/trunk@1069 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/IsLineType.java
+++ src/main/java/nl/numworx/geodefiner/IsLineType.java
@@ -36,18 +36,6 @@ public class IsLineType extends nl.numworx.geodefiner.common.math.IsLineType {
 		return LineType.SOLID;
 	}
 
-	@Override
-	protected LineType getLineTypeB(Destroyable b) {
-		if (b instanceof Label) {
-			Label l = (Label) b;
-			Numbers value = l.value;
-			int n = value.intValue();
-			if (n >= 0 && n < LineType.values().length) {
-				return LineType.values()[n];
-			}
-		}
-		return getLineTypeA(b);
-	}
 
 	public void updateLineType() {
 		test(label);
