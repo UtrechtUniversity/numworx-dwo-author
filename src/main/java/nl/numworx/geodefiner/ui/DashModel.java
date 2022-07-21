68029f5a641a6e76c92959951f0423ed2f7cc800
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2022-07-21 11:31:05 +0000

    DasModel voor gwt
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner/trunk@1115 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/ui/DashModel.java
+++ src/main/java/nl/numworx/geodefiner/ui/DashModel.java
@@ -1,20 +1,15 @@
 package nl.numworx.geodefiner.ui;
 
-import java.awt.LayoutManager;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.inject.Inject;
 import javax.swing.BoxLayout;
 
 import fi.beans.numworxlf.JCheckBox;
-import fi.euclides.event.Tracker;
 import fi.euclides.model.Destroyable;
+import nl.numworx.geodefiner.common.AbstractDashModel;
 import nl.numworx.geodefiner.common.LineType;
 import nl.numworx.geodefiner.common.UIModel;
-import nl.uu.fi.dwo.interaction.client.json.ObjectMap;
 
-public class DashModel implements UIModel<Destroyable, UIEditor> {
+public class DashModel extends AbstractDashModel<UIEditor> implements UIModel<Destroyable, UIEditor> {
 
 	private class DashEditor extends UIEditor {
 
@@ -42,84 +37,12 @@ public class DashModel implements UIModel<Destroyable, UIEditor> {
 
 	}
 
-	private final boolean[] selected;
-	
 	@Inject DashModel() {
-		selected = new boolean[LineType.values().length];
-		allTrue();
-	}
-
-	public boolean isSelected(LineType t) {
-		return selected[t.ordinal()];
-	}
-	
-	public boolean isSelected(int i) {
-		return selected[i];
-	}
-	
-	private void allTrue() {
-		for (int i = 0; i < selected.length; i++) {
-			selected[i] = true;
-		}
-	}
-
-	@Override
-	public UIModel<Destroyable, UIEditor> init(Destroyable item) {
-		return this;
-	}
-
-	@Override
-	public UIModel<Destroyable, UIEditor> init2(Destroyable d) {
-		return this;
-	}
-
-	@Override
-	public void install() {
-	}
-
-	@Override
-	public Map<String, Object> toMap() {
-		Map<String,Object> map = new HashMap<>();
-		for(LineType t: LineType.values()) {
-			if (! selected[t.ordinal()])
-				map.put(t.name(), selected[t.ordinal()]);
-		}
-		if (map.isEmpty()) return null;
-		return map;
-	}
-
-	@Override
-	public void fromMap(ObjectMap value) {
-		if (value == null) {
-			allTrue();
-		} else {
-			for (int i = 0; i < selected.length; i++) {
-				selected[i] = value.getBoolean(LineType.values()[i].name(), true);
-			}
-		}
 	}
 
 	@Override
 	public UIEditor editor() {
 		return new DashEditor();
-	}
-
-	@Override
-	public void setVisible(boolean visible) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public UIModel<Destroyable, UIEditor> set(Tracker tracker) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void install(Destroyable buildPunt) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
