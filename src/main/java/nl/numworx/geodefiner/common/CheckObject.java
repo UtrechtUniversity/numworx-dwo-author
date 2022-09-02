6377cecd50c689c45cbe7e908ae7fece882e5c09
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2022-09-02 12:10:44 +0000

    https://numworx.atlassian.net/browse/NUMGEODEF-201
    checkobject default marge is 0.05
    
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner-common/trunk@1123 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefiner/common/CheckObject.java
+++ src/main/java/nl/numworx/geodefiner/common/CheckObject.java
@@ -32,7 +32,7 @@ public class CheckObject extends Observable implements Observer, Comparable<Chec
 	int present = Label.UNKNOWN;
 	private Destroyable item, cache;
 	String formule = "";
-	Numbers marge = Numbers.createDouble(0.01);
+	Numbers marge = Numbers.createDouble(0.05);
 	private EqualsVisitor eq;
 	private List<Destroyable> depend = Collections.emptyList();
 	
