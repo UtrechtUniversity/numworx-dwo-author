b064d2e2f524d36d2efd8e3775ec01d81ab6fc3c
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-10-18 12:26:01 +0000

    action als scheme voor uri
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/WiskOpdr/trunk@28144 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/wiskopdr/tekstobjects/AddLinkDialog.java
+++ src/fi/wiskopdr/tekstobjects/AddLinkDialog.java
@@ -331,7 +331,7 @@ public class AddLinkDialog extends Dialog implements ActionListener,
         		  URI uri = new URI(urls[0]);
         		  String scheme = uri.getScheme();
         		  if (scheme != null) {
-        		    java.util.List<String> allowed = Arrays.asList("http","https", "goto", "about");
+        		    java.util.List<String> allowed = Arrays.asList("http","https", "goto", "about", "action");
         		    if ( ! allowed.contains(scheme)) {
         		      throw new IllegalArgumentException(scheme);
         		    }
