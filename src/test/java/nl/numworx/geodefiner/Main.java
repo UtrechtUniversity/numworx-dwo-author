fb38052f6bbbef832d9966299e81bf9e279a9153
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2023-04-11 09:38:06 +0000

    fix imports
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/geodefiner/trunk@1206 32ccb885-960d-4b53-a61f-10870b223014

--- src/test/java/nl/numworx/geodefiner/Main.java
+++ src/test/java/nl/numworx/geodefiner/Main.java
@@ -11,7 +11,6 @@ import java.util.Locale;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import org.cbook.cbookif.CBookContext;
@@ -26,7 +25,6 @@ import org.json.simple.parser.ParseException;
 
 import cbookeditor.CBookEditor;
 import fi.wiskopdr.WiskOpdr;
-import fi.wiskopdr.formuleobjects.FormuleParser;
 
 public class Main {
 
