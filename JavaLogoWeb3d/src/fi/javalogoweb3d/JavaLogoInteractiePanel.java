933a82a109ae4c616806bebe8bec63b68d29a751
Velthoven, W.P.G. van (Wim) <w.p.g.vanvelthoven@uu.nl>
2023-05-11 12:49:37 +0200

    opruimen oude beans
    met wiskopdr en dwo-commons

--- JavaLogoWeb3d/src/fi/javalogoweb3d/JavaLogoInteractiePanel.java
+++ JavaLogoWeb3d/src/fi/javalogoweb3d/JavaLogoInteractiePanel.java
@@ -626,7 +626,7 @@ System.out.println("jlip setEditState");
 		
 	}
 
-	@Override
+	//@Override
 	public int geefAsHoogte() {
 		// TODO Auto-generated method stub
 		return 0;
@@ -915,6 +915,12 @@ System.out.println("jlip setEditState");
 	@Override
 	public String getLocalizedCmd(String cmd) {
 		return JavaLogoWeb3d.rb.getString(CBA_PREFIX + cmd);
+	}
+
+	@Override
+	public int[][] getScoreObjectives() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
