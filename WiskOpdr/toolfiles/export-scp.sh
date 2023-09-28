2c1c0a4fea40fc6d4269a0d948838c84f771c6e1
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-09-28 11:18:32 +0200

    een export met scp

---
+++ WiskOpdr/toolfiles/export-scp.sh
@@ -0,0 +1,14 @@
+R=$USER@gemini.science.uu.nl:/science/wwwprojects/FI-Sites
+
+scp ../target/wiskopdr.jar $R/www-dev/dwo/jars/wiskopdr.jar
+scp ../target/wiskopdr.jar.pack.gz $R/www-dev/dwo/jars/wiskopdr.jar.pack.gz
+scp ../target/graphtool.jar $R/www-dev/dwo/jars/graphtool.jar
+scp ../target/graphtool.jar.pack.gz $R/www-dev/dwo/jars/graphtool.jar.pack.gz
+scp ../target/balansfruitapplet.jar $R/www-dev/dwo/jars/balansfruitapplet.jar
+scp ../target/balansfruitapplet.jar.pack.gz $R/www-dev/dwo/jars/balansfruitapplet.jar.pack.gz
+scp ../target/geodefiner.jar $R/www-dev/dwo/jars/geodefiner.jar
+scp ../target/geodefiner.jar.pack.gz $R/www-dev/dwo/jars/geodefiner.jar.pack.gz
+scp ../target/statistiek.jar $R/www-dev/dwo/jars/statistiek.jar
+scp ../target/statistiek.jar.pack.gz $R/www-dev/dwo/jars/statistiek.jar.pack.gz
+scp ../target/mathscratch.jar $R/www-dev/dwo/jars/mathscratch.jar
+scp ../target/mathscratch.jar.pack.gz $R/www-dev/dwo/jars/mathscratch.jar.pack.gz
