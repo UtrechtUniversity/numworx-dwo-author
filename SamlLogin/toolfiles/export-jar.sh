b8e943c64e9069c14919202ae1e51edeacc0c2bf
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-11-10 16:02:02 +0100

    Merge branch 'develop' of git@github.com:UtrechtUniversity/numworx-dwo-author.git into develop

--- SamlLogin/toolfiles/export-jar.sh
+++ SamlLogin/toolfiles/export-jar.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-R=/Volumes/fisme-sites
+R=$USER@gemini.science.uu.nl:/science/wwwprojects/FI-Sites
 
-if ! test -d $R/www-dev
-then
-	echo "MOUNT FISME-SITES"
-	exit 1;
-else
-	echo "fisme-sites mounted"
-fi
 APPLET=samllogin
-cp ../target/$APPLET.jar $R/www-dev/dwo/jars/
-cp ../target/$APPLET.jar $R/www-dev/javaclasses/jars/
+scp ../target/$APPLET.jar $R/www-dev/dwo/jars/
