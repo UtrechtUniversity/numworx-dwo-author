d8a09e7cc57e1545949b7d83626dec29aecbaf55
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-12-13 10:15:38 +0100

    https://numworx.atlassian.net/browse/DWOWISK-232
    ook bij insert en edit

--- WiskOpdr/src/fi/beans/iconan/Iconan.java
+++ WiskOpdr/src/fi/beans/iconan/Iconan.java
@@ -362,7 +362,8 @@ public class Iconan extends JPanel implements ActionListener, FocusListener, Lis
 					xnamemap.put(suffix(selected) + "/w", Integer.parseInt(widthField.getText()));
 					xnamemap.put(suffix(selected) + "/h", Integer.parseInt(heightField.getText()));
 					xnamemap.put(suffix(selected) + "/v", volBreedteCB.isSelected());
-				} else {
+			        WiskOpdr.setLaunchDataChanged();
+			} else {
 					selected = "";
 					al.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "cancel"));
 					return;
@@ -408,6 +409,7 @@ public class Iconan extends JPanel implements ActionListener, FocusListener, Lis
 				namemap.remove(selected + "/t");
 				namemap.remove(selected);
 			}
+			WiskOpdr.setLaunchDataChanged();
 		} else if (e.getSource() == downBtn) {
             String selected = (String) list.getSelectedValue();
             if (selected == null) return;
@@ -480,6 +482,8 @@ public class Iconan extends JPanel implements ActionListener, FocusListener, Lis
 				strategies.remove(selected);
 				imagemap.remove(selected);
 				selectPreview(selected);
+                WiskOpdr.setLaunchDataChanged();
+
 			}
 			return;
 		}
@@ -1260,6 +1264,7 @@ public class Iconan extends JPanel implements ActionListener, FocusListener, Lis
 		namemap.remove(filename + "/h");
 		namemap.remove(filename + "/u");
 		namemap.put(filename + "/f", file.toURI());
+        WiskOpdr.setLaunchDataChanged();
 		return filename;
 	}
   private String retype(String filename, String type) {
@@ -1309,6 +1314,7 @@ public class Iconan extends JPanel implements ActionListener, FocusListener, Lis
 		namemap.remove(filename + "/f");
 // als 'u' niet begint met codebase, bewaar volledige naam
 		namemap.put(filename + "/u", getURI(u));
+        WiskOpdr.setLaunchDataChanged();
 		return filename;
 	}
 
