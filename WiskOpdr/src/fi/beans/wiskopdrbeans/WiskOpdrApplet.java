1cd33bd9a814a5cf38295980761282618437bd83
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2024-06-28 07:38:44 +0200

    uitfaseren java.applet.Applet c.s.

--- WiskOpdr/src/fi/beans/wiskopdrbeans/WiskOpdrApplet.java
+++ WiskOpdr/src/fi/beans/wiskopdrbeans/WiskOpdrApplet.java
@@ -5,6 +5,11 @@ import java.applet.AppletStub;
 public interface WiskOpdrApplet {
 	
 	public InteractiePanel getInteractiePanel();
+	@Deprecated
 	public void setStub(AppletStub stub);
+	
+	public default void setStub(fi.beans.mainframe.AppletStub s) {
+	  setStub ( (AppletStub) s);
+	}
 
 }
