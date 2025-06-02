c9e7008a7ae6e82ee0ccb8400e853a7fc6872c48
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-06-02 14:08:40 +0200

    export jars met scp

--- aimodel/toolfiles/export-jar.sh
+++ aimodel/toolfiles/export-jar.sh
@@ -2,6 +2,6 @@
 R=/Volumes/fisme-sites
 R=$USER@gemini.science.uu.nl:/science/wwwprojects/FI-Sites
 
-APPLET=normaleverdeling
+APPLET=aimodel
 scp ../target/$APPLET.jar $R/www-dev/dwo/jars/
-scp ../target/$APPLET.jar.pack.gz $R/www-dev/dwo/jars/
+#scp ../target/$APPLET.jar.pack.gz $R/www-dev/dwo/jars/
