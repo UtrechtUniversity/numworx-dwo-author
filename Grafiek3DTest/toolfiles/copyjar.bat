e5bf0609be0a16c3a0c631a432df61aac866faaf
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-06-03 07:51:04 +0200

    Grafiek3DTest wordt bundle

--- Grafiek3DTest/toolfiles/copyjar.bat
+++
@@ -1,3 +0,0 @@
-copy ..\output\jar\grafiek3dtest.jar ..\..\DWO\output\jar\grafiek3dtest.jar
-copy ..\output\jar\grafiek3dtest.jar ..\..\DWOApp\war\dwo\jars\grafiek3dtest.jar
-copy ..\output\jar\grafiek3dtest.jar.pack.gz ..\..\DWOApp\war\dwo\jars\grafiek3dtest.jar.pack.gz
--- Grafiek3DTest/toolfiles/jar.bat
+++
@@ -1,2 +0,0 @@
-%JAVA_HOME%\bin\jar.exe cvfm ..\output\jar\grafiek3dtest.jar ..\output\classes\meta-inf\Manifest.mf -C ..\output\classes fi
-
